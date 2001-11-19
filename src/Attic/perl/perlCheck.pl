bcb9d7fd421dc2da282ec5e5bcfcc49f05b609f6
Ilya Goldberg <igg@nih.gov>
2001-11-19 02:49:49 +0000

    Added code to actually install missing or unsupported modules
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@47 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/perl/perlCheck.pl
+++ src/Attic/perl/perlCheck.pl
@@ -2,6 +2,7 @@
 use strict;
 
 my $moduleRepository = 'http://ome1-sorger.mit.edu/packages/perl';
+my $badTestsFatal = 0;
 
 $ENV{PATH} .= ':/usr/local/bin';
 
@@ -39,6 +40,7 @@ my @modules = ({
 	Name => 'DBD::Pg',
 	repositoryFile => 'DBD-Pg-0.95.tar.gz',
 	checkVersion => \&DBD_Pg_VersionOK,
+	installModule => \&DBD_Pg_Install,
 	loadOrder => 7
 	}
 );
@@ -52,13 +54,43 @@ foreach (@modules) {
 #  Version checks:
 sub DBI_VersionOK {
 my $version = shift;
-	return ($version >= 1.15 and $version <= 1.19);
+	return (1) if $version ge 1.15 and $version le 1.19;
+	return (0);
 }
 
 sub DBD_Pg_VersionOK {
 my $version = shift;
-	return ($version = 0.95);
+	return (1) if $version eq 0.95;
+	return (0);
 }
+
+# Special installation subs:
+sub DBD_Pg_Install {
+my $module = shift;
+my (undef,$pgVersion) = split (' ',`pg_config --version`);
+my $installTarBall = $module->{repositoryFile};
+my $installDir;
+	if ($installTarBall =~ /(.*)\.tar\.gz/) {$installDir = $1};
+my $error;
+
+
+	die "Postgres version must be >= 7.1\n" unless $pgVersion ge '7.1';
+
+	my $incDir = `pg_config --includedir`;
+	$incDir =~ s/^\s+//;$incDir =~ s/\s+$//;
+	$ENV{POSTGRES_INCLUDE} = $incDir;
+	my $libDir = `pg_config --libdir`;
+	$libDir =~ s/^\s+//;$libDir =~ s/\s+$//;
+	$ENV{POSTGRES_LIB} = $libDir;
+	
+	if ($^O eq 'darwin') {
+		print "ranlib2 $libDir/libpq.a","\n";
+		die "Couldn't run ranlib on $libDir/libpq.a\n" if system ("ranlib $libDir/libpq.a");
+	}
+	
+	InstallModule ($module);
+}
+
 
 # Should not have to modify below here when specifying new modules
 sub CheckModule {
@@ -67,18 +99,24 @@ my $moduleName = $module->{Name};
 my $repositoryFile = $module->{repositoryFile};
 my $moduleVersion;
 my $checkVersion;
+my $installModule;
 $checkVersion = $module->{checkVersion} if exists $module->{checkVersion}
 	and defined $module->{checkVersion} and $module->{checkVersion};
+$installModule = $module->{installModule} if exists $module->{installModule}
+	and defined $module->{installModule} and $module->{installModule};
+$installModule = \&InstallModule unless defined $installModule;
+
 
 	eval qq /require $moduleName;\$moduleVersion = \$$moduleName/.qq/::VERSION;/;
 	
 	if ($@) {
 		print "$@\n";
-		print "Downloading $moduleRepository/$repositoryFile...\n";
 		DownloadModule ("$moduleRepository/$repositoryFile");
+		&{$installModule}($module);
 	} elsif (defined $checkVersion and not &{$checkVersion}($moduleVersion)) {
 		print "$moduleName version is $moduleVersion - not supported\n";
 		DownloadModule ("$moduleRepository/$repositoryFile");
+		&{$installModule}($module);
 	} else {
 		print "$moduleName version is $moduleVersion - OK\n";
 	}
@@ -92,11 +130,11 @@ my $module = shift;
 my $wget;
 my $error;
 
-	print "Downloading $module...\n";
 	
 	$wget = 'curl';
 	$error = `$wget -V 2>&1 1>/dev/null`;
 	if (not $error) {
+		print "Downloading $module using $wget...\n";
 		$error = system ("$wget -O $module 2>&1 1>/dev/null");
 	}
 	return unless $error;
@@ -104,9 +142,29 @@ my $error;
 	$wget = 'wget';
 	$error = `$wget -V 2>&1 1>/dev/null`;
 	if (not $error) {
+		print "Downloading $module using $wget...\n";
 		$error = system ("$wget -nv $module 2>&1 1>/dev/null");
 	}
 	return unless $error;
 	
 	print STDERR "Could not download $module.\n";
+}
+
+
+sub InstallModule {
+my $module = shift;
+my $installTarBall = $module->{repositoryFile};
+my $installDir;
+	if ($installTarBall =~ /(.*)\.tar\.gz/) {$installDir = $1};
+my $error;
+
+	print "Installing $installTarBall\n";
+	die "Couldn't unpack $installTarBall.\n" if system ("tar -zxvf $installTarBall");
+	chdir $installDir or die "Couldn't change working directory to $installDir.\n";
+
+	die "Couldn't execute perl script 'Makefile.PL'.\n" if system ('perl Makefile.PL');
+	die "Compilation errors - script aborted.\n" if system ('make');
+	die "Test errors - script aborted.\n" if system ('make test') and $badTestsFatal;
+	die "Install errors - script aborted.\n" if system ('make install');
+	chdir '..';
 }
