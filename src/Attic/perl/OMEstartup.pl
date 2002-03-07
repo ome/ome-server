b2a779599b9defcb8b1c232d472f01d15202ff4a
Ilya Goldberg <igg@nih.gov>
2002-03-07 21:10:39 +0000

    Added automatic checking and making of OME's temporary diorectories (owned by 'www').
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@72 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/perl/OMEstartup.pl
+++ src/Attic/perl/OMEstartup.pl
@@ -11,4 +11,37 @@ use OMEpl;
 print STDERR "Executed OMEstartup.pl\n";
 use vars qw ($OME_TEST);
 $OME_TEST = "A wee little test of memory sharing";
+
+my $dir;
+my $apacheUID = getpwnam('www');
+my $apacheGrp = getgrnam('www');
+
+$dir = '/var/tmp/OME';
+if (not -e $dir) {
+	die "Couldn't make directory $dir: $!\n" if system ( "mkdir $dir");
+	chown $apacheUID, $apacheGrp, $dir;
+	chmod 0700, $dir;
+}
+
+$dir = '/var/tmp/OME/lock';
+if (not -e $dir) {
+	die "Couldn't make directory $dir: $!\n" if system ( "mkdir $dir");
+	chown $apacheUID, $apacheGrp, $dir;
+	chmod 0700, $dir;
+}
+
+$dir = '/var/tmp/OME/sessions';
+if (not -e $dir) {
+	die "Couldn't make directory $dir: $!\n" if system ( "mkdir $dir");
+	chown $apacheUID, $apacheGrp, $dir;
+	chmod 0700, $dir;
+}
+
+$dir = '/OME/Datasets';
+if (not -e $dir) {
+	die "Couldn't make directory $dir: $!\n" if system ( "mkdir $dir");
+	chown $apacheUID, $apacheGrp, $dir;
+	chmod 0755, $dir;
+}
+
 1;
