4d7972180bbca6685c0d6cb4426f018c681ad654
Doug Creager <dcreager@alum.mit.edu>
2002-09-25 15:56:10 +0000

    Use case tasks
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@128 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Tasks/ImageTasks.pm
@@ -0,0 +1,71 @@
+# OME/Tasks/ImageTasks.pm
+
+# Copyright (C) 2002 Open Microscopy Environment, MIT
+# Author:  Douglas Creager <dcreager@alum.mit.edu>
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+package OME::Tasks::ImageTasks;
+
+use OME::Session;
+use OME::Image;
+use OME::ImportExport::Importer;
+use IO::File;
+
+
+# importFiles(session,project,filenames)
+# --------------------------------------
+
+sub importFiles {
+    my ($session,$project,$filenames) = @_;
+
+    return unless
+        (defined $session) &&
+        (defined $project) &&
+        (defined $filenames);
+
+    my @images;
+
+    my $lambda = sub {
+        my ($href, $aref) = @_;
+
+        # determine which repository the new image should be placed in
+        my $repository = findRepository($session,$aref);
+        my $image = $session->Factory->createObject("OME::Image");
+        $image->Field("name",$href->{Image.Name});
+        $image->Field("description",$href->{Image.Description});
+        $image->Field("experimenter",$session->User());
+        $image->Field("created",$href->{Image.CreationDate});
+        $image->Field("inserted","now");
+        $image->Field("repository",$repository);
+        $image->Field("sizeX",$href->{Image.SizeX});
+        $image->Field("sizeY",$href->{Image.SizeY});
+        $image->Field("sizeZ",$href->{Image.SizeZ});
+        $image->Field("sizeW",$href->{Image.NumWaves});
+        $image->Field("sizeT",$href->{Image.NumTimes});
+
+        my $path = $image->Field("id").".ome";
+        $image->Field("path",$path);
+
+        my $handle = new IO::File;
+        open $handle, ">" . $repository->Field("path") . $path or
+            die "Error creating repository file";
+
+        #for (my 
+    };
+
+    my $importer = OME::ImportExport::Importer->new($filenames,$project,$lambda);
+}
