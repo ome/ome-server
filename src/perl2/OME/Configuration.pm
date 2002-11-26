bfffe42070f3f1bb077825e1847fb4482f293512
Brian Hughes <bshughes@mit.edu>
2002-11-26 20:38:33 +0000

    Define new configuration table
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@244 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Configuration.pm
@@ -0,0 +1,34 @@
+# OME/Config.pm
+
+# Copyright (C) 2002 Open Microscopy Environment, MIT
+# Author:  Douglas Creager <dcreager@alum.mit.edu>
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+package OME::Configuration;
+
+use strict;
+our $VERSION = '1.0';
+
+use OME::DBObject;
+use base qw(OME::DBObject);
+
+__PACKAGE__->table('configuration');
+__PACKAGE__->columns(Primary => qw(configuration_id));
+__PACKAGE__->columns(All => qw(mac_address input_dir repository_dir ome_root display_settings));
+
+
+1;
