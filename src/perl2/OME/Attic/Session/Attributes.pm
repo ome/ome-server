e746493f6acdfca2699f0ab3ff4c3a9f1b0521ba
Brian Hughes <bshughes@mit.edu>
2002-12-03 20:39:32 +0000

    add one_sessions table to DB
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@282 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Attic/Session/Attributes.pm
@@ -0,0 +1,25 @@
+
+package OME::Session::Attributes;
+
+use strict;
+our $VERSION = '1.0';
+
+use OME::DBObject;
+use base qw(OME::DBObject);
+
+use fields qw(_attributes);
+__PACKAGE__->AccessorNames({
+    project_id     => 'project'
+});
+
+__PACKAGE__->table('ome_sessions');
+__PACKAGE__->sequence('session_seq');
+__PACKAGE__->columns(Primary => qw(session_id));
+__PACKAGE__->columns(Essential => qw(session_key analysis));
+__PACKAGE__->columns(Others => qw(host image_view feature_view display_settings last_access started));
+__PACKAGE__->hasa('OME::Experimenter' => qw(experimenter_id));
+__PACKAGE__->hasa('OME::Project' => qw(project_id));
+
+1;
+
+
