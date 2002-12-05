114edc9e450d691883b93f6cd0cf3c3b44677820
Josiah Johnston <siah@nih.gov>
2002-12-05 20:22:01 +0000

    some functions fleshed out
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@296 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/JavaScript/SVGviewer/scale.js
+++ src/JavaScript/SVGviewer/scale.js
@@ -1,7 +1,7 @@
 /*****
 
 	scale.js
-		external file dependencies: none
+		external file dependencies: widget.js, slider.js, button.js, popupList.js
 		
 		Author: Josiah Johnston
 		email: siah@nih.gov
@@ -15,7 +15,8 @@ var svgns = "http://www.w3.org/2000/svg";
 	class variables
 	
 *****/
-scale.VERSION = 0.1;
+Scale.VERSION = 0.1;
+scaleWidth = 180;
 
 /********************************************************************************************/
 /********************************************************************************************/
@@ -28,14 +29,16 @@ scale.VERSION = 0.1;
 	constructor
 		image = OMEimage
 		
-	untested
+		make instance in viewer to be scale
+		
+	tested
 
 *****/
-function scale(image) {
+function Scale(image, updateBlack, updateWhite) {
 	if(image.Dims != null)
-		this.init(image, default_WBS )
+		this.init(image, updateBlack, updateWhite)
 	else
-		alert("Bad parameters or image not initialized. Class creation of scale.js failed");
+		alert("Bad parameters or image not initialized. Class creation of Scale.js failed");
 }
 
 /*****
@@ -48,10 +51,10 @@ function scale(image) {
 	returns:
 		nothing
 		
-	untests
+	untested
 
 *****/
-scale.prototype.setChannelWavelength = function( channel, waveNum ) {
+Scale.prototype.setChannelWavelength = function( channel, waveNum ) {
 }
 
 /*****
@@ -59,15 +62,86 @@ scale.prototype.setChannelWavelength = function( channel, waveNum ) {
 	buildSVG
 	
 	returns:
-		SVG node containing scale pane
+		SVG chunk describing Scale pane
 	notes:
 		for use in conjuction with multipaneToolBox
 		
-	untested
+	tested
 		
 *****/
-scale.prototype.buildSVG = function() {
-	return svgDocument.createElementNS("<g/>");
+Scale.prototype.buildSVG = function() {
+	if(this.image == null) 
+		return null;
+
+	this.root = svgDocument.createElementNS(svgns, "g");
+
+	this.fluors = new Array();
+	for(var i in this.image.Wavelengths)
+		this.fluors[this.image.Wavelengths[i]['WaveNum']] = this.image.Wavelengths[i]['Fluor'];
+	this.blackSlider = new Slider( 
+		10, 60, scaleWidth, 0, 
+		this.updateBlack,
+		'<rect width="'+scaleWidth+'" height="10" opacity="0"/>',
+		'<rect x="-2" width="4" height="10" fill="black"/>'
+	);
+	this.whiteSlider = new Slider( 
+		10, 70, scaleWidth, 0, 
+		this.updateWhite,
+		'<rect width="'+scaleWidth+'" height="10" opacity="0"/>',
+		'<rect x="-2" width="4" height="10" fill="white"/>'
+	);
+	this.wavePopupList = new popupList(
+		10, 90, this.fluors, null
+	);
+
+	this.root.appendChild( this.whiteSlider.textToSVG(
+'<g transform="translate(10,70)">\
+	<line x2="'+ scaleWidth +'" stroke-width="2" stroke="blue"/>\
+	<line id="geomeanTick" y1="-10" y2="10" stroke-width="2" stroke="blue"/>\
+</g>'
+	));
+	this.geomeanTick = this.root.lastChild.lastChild;
+	
+	this.root.appendChild( this.whiteSlider.textToSVG(
+		'<text x="20" y="2em">Black level: </text>' ));
+	this.blackLabel = this.root.lastChild;
+	this.root.appendChild( this.whiteSlider.textToSVG(
+		'<text x="20" y="3em">White level: </text>' ));
+	this.whiteLabel = this.root.lastChild;
+	this.root.appendChild( this.whiteSlider.textToSVG(
+		'<text x="20" y="4em">Scale: </text>' ));
+	this.scaleLabel = this.root.lastChild;
+
+	this.blackSlider.realize( this.root );
+	this.whiteSlider.realize( this.root );
+	this.wavePopupList.realize( this.root );
+	
+	return this.root;
+}
+
+/*****
+
+	updateScale
+		t = theT
+	
+	purpose:
+		update scale based on info particular to W & T
+
+	untested
+	
+*****/
+
+Scale.prototype.updateGeomean = function(t) {
+	if(this.root == null) return null;
+	if(t != null && t>=0 && t<this.Dims['T'])
+		this.theT = t;
+	var wavenum = this.wavePopupList.getSelection();
+	var min = this.Stats[wavenum][this.theT]['min'];
+	var max = this.Stats[wavenum][this.theT]['max'];
+	var sigma = this.Stats[wavenum][this.theT]['sigma'];
+	var geomeanX = (this.Stats[wavenum][this.theT]['geomean'] - min) / (max - min) * scaleWidth;
+	this.geomeanTick.setAttribute("transform", "translate("+geomeanX+",0)");
+	// set B&W sliderVals to correct positions
 }
 
 /********************************************************************************************/
@@ -82,7 +156,7 @@ scale.prototype.buildSVG = function() {
 	returns:
 		possibly altered WBS that is valid
 		
-	GOES IN OMEIMAGE WHEN DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	GOES IN OMEimage.js WHEN DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		
 	untested
 		
@@ -90,47 +164,6 @@ scale.prototype.buildSVG = function() {
 *****/
 function verifyWBS(WBS) {
 	return WBS;
-}
-
-/*****
-
-	test
-	
-	purpose:
-		to instantiate this object and call all functions to see if they crash
-		
-	untested
-
-*****/
-function test() {
-}
-
-/*****
-
-	updateBlack
-		val = slider value from black slider
-	
-	purpose:
-		do backend response to changing Black scale in GUI
-		
-	untested
-
-*****/
-function updateBlack(val) {
-}
-
-/*****
-
-	updateWhite
-		val = slider value from white slider
-	
-	purpose:
-		do backend response to changing White scale in GUI
-		
-	untested
-
-*****/
-function updateWhite(val) {
 }
 
 /********************************************************************************************/
@@ -144,14 +177,18 @@ function updateWhite(val) {
 	init
 		image = OMEimage
 
-	untested
+	tested
 
 *****/
 
-scale.prototype.init = function(image) {
+Scale.prototype.init = function(image, updateBlack, updateWhite) {
 	this.Dims = image.Dims;
 	this.Stats = image.Stats;
-	
+	this.BS = new Array(this.Dims['W']);
+	this.image = image;
+	this.theT = (this.image.oldT == null ? 0 : this.image.oldT);
+	this.updateBlack = updateBlack;
+	this.updateWhite = updateWhite;
 }
 
 /********************************************************************************************/
