a15e61cce88a825962c0375e496306db6c356218
Josiah Johnston <siah@nih.gov>
2002-12-09 16:23:04 +0000

    added alternate GUI layout
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@320 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/JavaScript/SVGviewer/overlay.js
+++ src/JavaScript/SVGviewer/overlay.js
@@ -30,7 +30,7 @@ Overlay.VERSION = 0.1;
 		
 		make instance in viewer to be scale
 		
-	untested
+	tested
 
 *****/
 function Overlay() {
@@ -45,8 +45,9 @@ function Overlay() {
 		SVG chunk describing Overlay pane
 	notes:
 		for use in conjuction with multipaneToolBox
+		layout #1
 		
-	untested
+	tested
 		
 *****/
 Overlay.prototype.buildSVG = function() {
@@ -69,12 +70,18 @@ Overlay.prototype.buildSVG = function() {
 
 	// set up GUI
 	this.layerPopupList = new popupList(
-		40, 10, this.layerNames, this.updateLayer
+		40, 10, this.layerNames, this.updateLayer, null,
+		skinLibrary["popupListAnchorLightslategray"],
+		skinLibrary["popupListBackgroundLightskyblue"],
+		skinLibrary["popupListHighlightAquamarine"]
 	);
 	this.layerPopupList.setLabel(-2, 12, "Layer:");
 	this.layerPopupList.getLabel().setAttribute("text-anchor", "end");
 	this.colorPopupList = new popupList(
-		60, 30, this.colors, this.changeColor
+		60, 30, this.colors, this.changeColor, null,
+		skinLibrary["popupListAnchorLightslategray"],
+		skinLibrary["popupListBackgroundLightskyblue"],
+		skinLibrary["popupListHighlightAquamarine"]
 	);
 	this.colorPopupList.setLabel(-2, 12, "Color:");
 	this.colorPopupList.getLabel().setAttribute("text-anchor", "end");
@@ -101,7 +108,11 @@ Overlay.prototype.buildSVG = function() {
 	this.markViewButton.setLabel(-7, 10,"Mark current view");
 	this.markViewButton.getLabel().setAttribute("text-anchor", "end");
 	this.markViewPopupList = new popupList(
-		74, 20, this.markedViews, this.changeMarkedView
+		74, 20, this.markedViews, this.changeMarkedView, null,
+		skinLibrary["popupListAnchorLightslategray"],
+		skinLibrary["popupListBackgroundLightskyblue"],
+		skinLibrary["popupListHighlightAquamarine"]
+		
 	);
 	this.markViewPopupList.setLabel(-2, 12, "Marked views");
 	this.markViewPopupList.getLabel().setAttribute("text-anchor", "end");
@@ -150,6 +161,101 @@ Overlay.prototype.buildSVG = function() {
 	return this.root;
 }
 
+/*****
+	
+	buildSVG2
+	
+	returns:
+		SVG chunk describing Overlay pane
+	notes:
+		for use in conjuction with multipaneToolBox
+		#2 layout
+		
+	tested
+		
+*****/
+Overlay.prototype.buildSVG2 = function() {
+	// check for initialization
+	if(this.colors == null) return null;
+
+// phony info
+	this.layerNames = new Array();
+	this.layerNames.push("Nucleus Vector");
+	this.layerNames.push("Nucleus Outline");
+	this.layerNames.push("Nucleus Centroid");
+	this.layerNames.push("Scale");
+	
+	this.markedViews = new Array;
+	this.markedViews.push("Z 12 T 0");
+	this.markedViews.push("Z 5 T 15");
+
+// build SVG
+	this.root = svgDocument.createElementNS(svgns, "g");
+
+	// set up GUI
+	this.layerPopupList = new popupList(
+		40, 10, this.layerNames, this.updateLayer, null,
+		skinLibrary["popupListAnchorLightslategray"],
+		skinLibrary["popupListBackgroundLightskyblue"],
+		skinLibrary["popupListHighlightAquamarine"]
+	);
+	this.layerPopupList.setLabel(-2, 12, "Layer:");
+	this.layerPopupList.getLabel().setAttribute("text-anchor", "end");
+	this.colorPopupList = new popupList(
+		60, 30, this.colors, this.changeColor, null,
+		skinLibrary["popupListAnchorLightslategray"],
+		skinLibrary["popupListBackgroundLightskyblue"],
+		skinLibrary["popupListHighlightAquamarine"]
+	);
+	this.colorPopupList.setLabel(-2, 12, "Color:");
+	this.colorPopupList.getLabel().setAttribute("text-anchor", "end");
+	this.displayButton = new button(
+		120, 30, this.displayLayer,
+		'<text y="12">Off</text>',
+		'<text y="12">On</text>'
+	);
+	
+	
+	this.dynamicControls = svgDocument.createElementNS( svgns, "g" );
+	this.dynamicControls.setAttribute("transform", "translate(20,60)");
+	this.dynamicControls.padding = 5;
+	this.dynamicControls.appendChild( this.displayButton.textToSVG(
+		'<rect x="-' + this.dynamicControls.padding + '" y="-' +
+			this.dynamicControls.padding + '" height="0" width="0" ' +
+			'fill="none" stroke="black" stroke-width="2" opacity="0.7"/>'
+	));
+	this.dynamicControls.border = this.dynamicControls.lastChild;
+	
+	this.allZButton = new button(
+		100, 0, this.allZ
+	);
+	this.allZButton.setLabel(-7, 10,"Show all Z");
+	this.allZButton.getLabel().setAttribute("text-anchor", "end");
+	this.allTButton = new button(
+		100, 20, this.allT
+	);
+	this.allTButton.setLabel(-7, 10,"Show all T");
+	this.allTButton.getLabel().setAttribute("text-anchor", "end");
+
+
+	// place GUI elements in containers
+	this.root.appendChild( this.dynamicControls );
+
+	this.allZButton.realize( this.dynamicControls );
+	this.allTButton.realize( this.dynamicControls );
+	
+	this.dynamicControls.border.setAttribute("width", 180);
+	this.dynamicControls.border.setAttribute("height", 40);
+
+	this.colorPopupList.realize( this.root );
+	this.displayButton.realize( this.root );
+	this.layerPopupList.realize( this.root );
+	
+	// build background
+	
+	return this.root;
+}
+
 /********************************************************************************************/
 /********************************************************************************************/
 /************************** Functions without safety nets ***********************************/
@@ -160,7 +266,7 @@ Overlay.prototype.buildSVG = function() {
 
 	init
 		
-	untested
+	tested
 
 *****/
 
@@ -168,10 +274,16 @@ Overlay.prototype.init = function() {
 	this.updateLayer = null;
 	this.changeColor = null;
 	this.displayLayer = null;
+
+	// for GUI layout 1
 	this.markView = null
 	this.changeMarkedView = null;
 	this.removeMarkedView = null;
 	this.displayMarkedView = null;
+
+	// for GUI layout 2
+	this.allZ = null;
+	this.allT = null;
 	
 	this.makeColors();
 }
@@ -186,7 +298,7 @@ Overlay.prototype.init = function() {
 	returns:
 		list of colors
 		
-	untested
+	tested
 	
 *****/
 
