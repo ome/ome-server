6971aad32dfe39c96674468b2e16bde994e5f808
Josiah Johnston <siah@nih.gov>
2002-12-11 02:27:51 +0000

    GUI layout is finalized
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@328 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/JavaScript/SVGviewer/overlay.js
+++ src/JavaScript/SVGviewer/overlay.js
@@ -45,7 +45,6 @@ function Overlay() {
 		SVG chunk describing Overlay pane
 	notes:
 		for use in conjuction with multipaneToolBox
-		layout #1
 		
 	tested
 		
@@ -56,9 +55,9 @@ Overlay.prototype.buildSVG = function() {
 
 // phony info
 	this.layerNames = new Array();
-	this.layerNames.push("Nucleus Vector");
-	this.layerNames.push("Nucleus Outline");
-	this.layerNames.push("Nucleus Centroid");
+	this.layerNames.push("Vector");
+	this.layerNames.push("Outline");
+	this.layerNames.push("Centroid");
 	this.layerNames.push("Scale");
 	
 	this.markedViews = new Array;
@@ -78,7 +77,7 @@ Overlay.prototype.buildSVG = function() {
 	this.layerPopupList.setLabel(-2, 12, "Layer:");
 	this.layerPopupList.getLabel().setAttribute("text-anchor", "end");
 	this.colorPopupList = new popupList(
-		60, 30, this.colors, this.changeColor, null,
+		90, 35, this.colors, this.changeColor, null,
 		skinLibrary["popupListAnchorLightslategray"],
 		skinLibrary["popupListBackgroundLightskyblue"],
 		skinLibrary["popupListHighlightAquamarine"]
@@ -86,155 +85,30 @@ Overlay.prototype.buildSVG = function() {
 	this.colorPopupList.setLabel(-2, 12, "Color:");
 	this.colorPopupList.getLabel().setAttribute("text-anchor", "end");
 	this.displayButton = new button(
-		120, 30, this.displayLayer,
-		'<text y="12">Off</text>',
-		'<text y="12">On</text>'
+		30, 35, this.displayLayer,
+		'<g>' +
+		'	<rect x="-12" y="-1" width="24" height="18" fill="lightslategray"/>' +
+		'	<text y="12" text-anchor="middle">Off</text>'+
+		'</g>',
+		'<g>' +
+		'	<rect x="-12" y="-1" width="24" height="18" fill="lightslategray"/>' +
+		'	<text y="12" text-anchor="middle">On</text>'+
+		'</g>'
 	);
-	
 	
 	this.dynamicControls = svgDocument.createElementNS( svgns, "g" );
-	this.dynamicControls.setAttribute("transform", "translate(20,60)");
+	this.dynamicControls.setAttribute("transform", "translate(20,70)");
 	this.dynamicControls.padding = 5;
-	this.dynamicControls.appendChild( this.displayButton.textToSVG(
-		'<rect x="-' + this.dynamicControls.padding + '" y="-' +
-			this.dynamicControls.padding + '" height="0" width="0" ' +
-			'fill="none" stroke="black" stroke-width="2" opacity="0.7"/>'
-	));
-	this.dynamicControls.border = this.dynamicControls.lastChild;
-	
-	this.markViewButton = new button(
-		100, 0, this.markView
-	);
-	this.markViewButton.setLabel(-7, 10,"Mark current view");
-	this.markViewButton.getLabel().setAttribute("text-anchor", "end");
-	this.markViewPopupList = new popupList(
-		74, 20, this.markedViews, this.changeMarkedView, null,
-		skinLibrary["popupListAnchorLightslategray"],
-		skinLibrary["popupListBackgroundLightskyblue"],
-		skinLibrary["popupListHighlightAquamarine"]
-		
-	);
-	this.markViewPopupList.setLabel(-2, 12, "Marked views");
-	this.markViewPopupList.getLabel().setAttribute("text-anchor", "end");
-	this.viewDisplayButton = new button(
-		20, 40, this.displayMarkedView,
-		'<g>' +
-		'	<rect x="-2" width="34" height="14" fill="gray" opacity="0.7"/>' +
-		'	<text x="2" y="1em">Hide</text>' +
-		'</g>',
-		'<g>' +
-		'	<rect x="-2" width="34" height="14" fill="gray" opacity="0.7"/>' +
-		'	<text y="1em">Show</text>' +
-		'</g>',
-		'<rect x="-2" width="34" height="14" fill="white"/>'
-
-	);
-	this.removeView = new button(
-		80, 40, this.removeMarkedView,
-		'<g>' +
-		'	<rect x="-2" width="50" height="14" fill="gray" opacity="0.7"/>' +
-		'	<text y="1em">Remove</text>' +
-		'</g>',
-		null,
-		'<rect x="-2" width="50" height="14" fill="white"/>'
-	);
-
-
-
-	// place GUI elements in containers
-	this.root.appendChild( this.dynamicControls );
-
-	this.markViewButton.realize( this.dynamicControls );
-	this.viewDisplayButton.realize( this.dynamicControls );
-	this.removeView.realize( this.dynamicControls );
-	this.markViewPopupList.realize( this.dynamicControls );
-	
-	this.dynamicControls.border.setAttribute("width", 180);
-	this.dynamicControls.border.setAttribute("height", 70);
-
-	this.colorPopupList.realize( this.root );
-	this.displayButton.realize( this.root );
-	this.layerPopupList.realize( this.root );
-	
-	// build background
-	
-	return this.root;
-}
-
-/*****
-	
-	buildSVG2
-	
-	returns:
-		SVG chunk describing Overlay pane
-	notes:
-		for use in conjuction with multipaneToolBox
-		#2 layout
-		
-	tested
-		
-*****/
-Overlay.prototype.buildSVG2 = function() {
-	// check for initialization
-	if(this.colors == null) return null;
-
-// phony info
-	this.layerNames = new Array();
-	this.layerNames.push("Nucleus Vector");
-	this.layerNames.push("Nucleus Outline");
-	this.layerNames.push("Nucleus Centroid");
-	this.layerNames.push("Scale");
-	
-	this.markedViews = new Array;
-	this.markedViews.push("Z 12 T 0");
-	this.markedViews.push("Z 5 T 15");
-
-// build SVG
-	this.root = svgDocument.createElementNS(svgns, "g");
-
-	// set up GUI
-	this.layerPopupList = new popupList(
-		40, 10, this.layerNames, this.updateLayer, null,
-		skinLibrary["popupListAnchorLightslategray"],
-		skinLibrary["popupListBackgroundLightskyblue"],
-		skinLibrary["popupListHighlightAquamarine"]
-	);
-	this.layerPopupList.setLabel(-2, 12, "Layer:");
-	this.layerPopupList.getLabel().setAttribute("text-anchor", "end");
-	this.colorPopupList = new popupList(
-		60, 30, this.colors, this.changeColor, null,
-		skinLibrary["popupListAnchorLightslategray"],
-		skinLibrary["popupListBackgroundLightskyblue"],
-		skinLibrary["popupListHighlightAquamarine"]
-	);
-	this.colorPopupList.setLabel(-2, 12, "Color:");
-	this.colorPopupList.getLabel().setAttribute("text-anchor", "end");
-	this.displayButton = new button(
-		120, 30, this.displayLayer,
-		'<text y="12">Off</text>',
-		'<text y="12">On</text>'
-	);
-	
-	
-	this.dynamicControls = svgDocument.createElementNS( svgns, "g" );
-	this.dynamicControls.setAttribute("transform", "translate(20,60)");
-	this.dynamicControls.padding = 5;
-	this.dynamicControls.appendChild( this.displayButton.textToSVG(
-		'<rect x="-' + this.dynamicControls.padding + '" y="-' +
-			this.dynamicControls.padding + '" height="0" width="0" ' +
-			'fill="none" stroke="black" stroke-width="2" opacity="0.7"/>'
-	));
-	this.dynamicControls.border = this.dynamicControls.lastChild;
 	
 	this.allZButton = new button(
 		100, 0, this.allZ
 	);
-	this.allZButton.setLabel(-7, 10,"Show all Z");
+	this.allZButton.setLabel(-9, 9,"Show all Z");
 	this.allZButton.getLabel().setAttribute("text-anchor", "end");
 	this.allTButton = new button(
 		100, 20, this.allT
 	);
-	this.allTButton.setLabel(-7, 10,"Show all T");
+	this.allTButton.setLabel(-9, 9,"Show all T");
 	this.allTButton.getLabel().setAttribute("text-anchor", "end");
 
 
@@ -244,8 +118,11 @@ Overlay.prototype.buildSVG2 = function() {
 	this.allZButton.realize( this.dynamicControls );
 	this.allTButton.realize( this.dynamicControls );
 	
-	this.dynamicControls.border.setAttribute("width", 180);
-	this.dynamicControls.border.setAttribute("height", 40);
+	// draw some background
+	this.dynamicControls.appendChild( parseXML(
+		'<rect x="30" y="-5" width="80" height="39" fill="none" stroke="black" stroke-width="2" opacity="0.7"/>',
+		svgDocument
+	));
 
 	this.colorPopupList.realize( this.root );
 	this.displayButton.realize( this.root );
@@ -275,13 +152,6 @@ Overlay.prototype.init = function() {
 	this.changeColor = null;
 	this.displayLayer = null;
 
-	// for GUI layout 1
-	this.markView = null
-	this.changeMarkedView = null;
-	this.removeMarkedView = null;
-	this.displayMarkedView = null;
-
-	// for GUI layout 2
 	this.allZ = null;
 	this.allT = null;
 	
