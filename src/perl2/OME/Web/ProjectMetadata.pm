07dbd306ba9f9c9c480c547acb1bb9fa136058d9
Josiah Johnston <siah@nih.gov>
2002-12-31 22:00:19 +0000

    package to view and edit project metadata
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@395 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Web/ProjectMetadata.pm
@@ -0,0 +1,105 @@
+# OME/Web/ProjectMetadata.pm
+
+# Copyright (C) 2002 Open Microscopy Environment, MIT
+# Author:  Douglas Creager <dcreager@alum.mit.edu>
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+package OME::Web::ProjectMetadata;
+
+use strict;
+use vars qw($VERSION @ISA);
+$VERSION = '1.0';
+use CGI;
+use OME::Web;
+@ISA = ("OME::Web");
+
+sub getPageTitle {
+	return "Open Microscopy Environment - Make New Project";
+}
+
+sub getPageBody {
+	my $self = shift;
+	my $cgi = $self->CGI();
+	my $body = "";
+	my $session = $self->Session();
+
+	# figure out what to do: save & print info or just print?
+	if( $cgi->param('Save')) {
+
+		my $project = $session->project();
+		if (!defined $project) {
+			# we got problems
+			$body .= "No project is defined. What's up w/ that?";
+		}
+		else {
+			# change stuff. validate permission?
+			$project->name( $cgi->param('name') );
+			$project->description( $cgi->param('description') );
+
+			$project->writeObject();
+		}
+	}
+	# print info & form
+	$body .= $self->print();
+
+    return ('HTML',$body);
+}
+
+sub print {
+	my $self = shift;
+	my $cgi = $self->CGI();
+	my $project = $self->Session()->project();
+	
+	my $text = '';
+
+	$text .= "\n".$cgi->startform;
+	$text .= "<CENTER>\n	".$cgi->submit (-name=>'Save',-value=>'Save Changes')."\n</CENTER>\n";
+
+	$text .= 
+		$cgi->table(
+			$cgi->Tr( { -valign=>'MIDDLE' },
+				$cgi->td( { -align=>'LEFT' },
+					'ID:' ),
+				$cgi->td( { -align=>'LEFT' },
+					$project->project_id() ) ),
+			$cgi->Tr( { -valign=>'MIDDLE' },
+				$cgi->td( { -align=>'LEFT' },
+					'Name:' ),
+				$cgi->td( { -align=>'LEFT' },
+					$cgi->textfield(-name=>'name', -size=>32, -default=>$project->name()) ) ),
+			$cgi->Tr( { -valign=>'MIDDLE' },
+				$cgi->td( { -align=>'LEFT' },
+					'Description:' ),
+				$cgi->td( { -align=>'LEFT' },
+					$cgi->textarea(-name=>'description', -columns=>32, -rows=>3, -default=>$project->description() ))),
+			$cgi->Tr( { -valign=>'MIDDLE' },
+				$cgi->td( { -align=>'LEFT' },
+					'Owner:' ),
+				$cgi->td( { -align=>'LEFT' },
+					$project->owner()->firstname()." ".$project->owner()->lastname()." <a href='mailto:".$project->owner()->email()."'>".$project->owner()->email()."</a>" ) ),
+			$cgi->Tr( { -valign=>'MIDDLE' },
+				$cgi->td( { -align=>'LEFT' },
+					'Group:' ),
+				$cgi->td( { -align=>'LEFT' },
+					$project->group()->name() ) )
+		);
+			
+	$text .= $cgi->endform."\n";
+	return $text;
+}
+
+1;
