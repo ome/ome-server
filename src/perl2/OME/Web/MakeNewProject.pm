3725b894be99d9a3ddc490077ef6347ac67508c1
Josiah Johnston <siah@nih.gov>
2002-12-31 17:26:19 +0000

    UI to create a project
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@386 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Web/MakeNewProject.pm
@@ -0,0 +1,101 @@
+# OME/Web/MakeNewProject.pm
+
+# Copyright (C) 2002 Open Microscopy Environment, MIT
+# Author:  Douglas Creager <dcreager@alum.mit.edu>
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+package OME::Web::MakeNewProject;
+
+use strict;
+use vars qw($VERSION @ISA);
+$VERSION = '1.0';
+use CGI;
+use OME::Web;
+@ISA = ("OME::Web");
+
+sub getPageTitle {
+	return "Open Microscopy Environment - Make New Project";
+}
+
+sub getPageBody {
+	my $self = shift;
+	my $cgi = $self->CGI();
+	my $body = "";
+	my $session = $self->Session();
+
+	# figure out what to do: create a project or print an input form
+	if( $cgi->param('CreateProject')) {
+	# try to make a project, print status message, include some mechanism
+	# 	to redirect to import images if this is a first time login
+
+		my $user = $session->User();
+		my $data = {name => $cgi->param('name'),
+			description => $cgi->param('description'),
+			owner_id => $user->ID(),
+			group_id => $user->group()->ID()};
+		my $project = $session->Factory()->newObject("OME::Project", $data);
+		if (!defined $project) {
+			$body .= " Failed to create new project $cgi->param('name').\n";
+		}
+		else {
+			$project->writeObject();
+			# is this the first project? if so, we need to redirect to
+			# import images
+			my $redirect = (not defined $session->project() ? 1 : undef);
+			$session->project($project);
+			$session->writeObject();
+			$body .= "Project Creation successfull.";
+			$body .= "Click ".$cgi->a({href=>'/JavaScript/DirTree/index.htm'},'here')." to continue on to import images."
+				if defined $redirect;
+		}
+
+	
+	} else {
+	# print an input form
+		$body .= $self->print_form();
+	}
+
+    return ('HTML',$body);
+}
+
+sub print_form {
+	my $self = shift;
+	my $cgi = $self->CGI();
+	
+	my $text = '';
+
+	$text .= "\n".$cgi->startform;
+	$text .= "<CENTER>\n	".$cgi->submit (-name=>'CreateProject',-value=>'Create Project')."\n</CENTER>\n";
+
+	$text .= 
+		$cgi->table(
+			$cgi->Tr( { -valign=>'MIDDLE' },
+				$cgi->td( { -align=>'LEFT' },
+					'Name:' ),
+				$cgi->td( { -align=>'LEFT' },
+					$cgi->textfield(-name=>'name', -size=>32) ) ),
+			$cgi->Tr( { -valign=>'MIDDLE' },
+				$cgi->td( { -align=>'LEFT' },
+					'Description:' ),
+				$cgi->td( { -align=>'LEFT' },
+					$cgi->textfield(-name=>'description', -size=>101) ) ) );
+			
+	$text .= $cgi->endform."\n";
+	return $text;
+}
+
+1;
