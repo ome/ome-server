bc98ad574a1b233c174739eb732551d2de2080cd
Josiah Johnston <siah@nih.gov>
2003-01-14 22:55:15 +0000

    traps all exceptions and displays exit messages. also other messages are more descriptive now.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@422 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/serve.pl
+++ src/perl2/serve.pl
@@ -4,28 +4,37 @@ use strict;
 use vars qw($VERSION);
 $VERSION = '1.0';
 use CGI;
-use OME::Web;
 
 
 my $CGI = CGI->new();
 my $pageClass = $CGI->url_param("Page");
 
 if ($pageClass) {
-    my $page;
-    {
+	my $page;
 	eval "use $pageClass";
-	$page = $pageClass->new(CGI => $CGI);
-    }
-    if ($@) {
-	print STDERR "Error loading package - $@\n";
-	print $CGI->header(-type => 'text/html',-status => '404 File not found');
-    } elsif (!$page->isa("OME::Web")) {
-	print STDERR "Package not OME::Web\n";
-	print $CGI->header(-type => 'text/html',-status => '404 File not found');
-    } else {
-	$page->serve();
-    }
+	if ($@) {
+		print STDERR "Error loading package - $@\n";
+		print $CGI->header(-type => 'text/html',-status => '404 File not found');
+	}
+
+	eval {
+		$page = $pageClass->new(CGI => $CGI);
+		if (!$page) {
+			print STDERR "Error calling package constructor -\n";
+			print $CGI->header(-type => 'text/html',-status => '404 File not found');
+		} elsif (! (ref($page) =~ /^OME::Web::/) ) {
+			print STDERR "Package ".ref($page)." does not inherit from OME::Web\n";
+			print $CGI->header(-type => 'text/html',-status => '404 File not found');
+		} else {
+			$page->serve();
+		}
+	};
+	
+	if ($@) {
+		print $CGI->header(-type => 'text/html');
+		print "<pre>Error serving $pageClass:\n$@</pre>";
+	}
 } else {
-    print STDERR "Class not specified\n";
-    print $CGI->header(-type => 'text/html',-status => '404 File not found');
+	print STDERR "Class not specified\n";
+	print $CGI->header(-type => 'text/html',-status => '404 File not found');
 }
