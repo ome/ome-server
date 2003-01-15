815c888a270cdd44c29b92e8d9e17a233c681ed5
Josiah Johnston <siah@nih.gov>
2003-01-15 00:52:11 +0000

    Previously this would only display info. it had no active capabilities. now it does.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@438 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/ProjectDataset.pm
+++ src/perl2/OME/Web/ProjectDataset.pm
@@ -22,19 +22,20 @@
 
 =head1 Package OME::Web::ProjectDataset
 
-Description: Generate HTML describing & linking to datasets belonging to
-a the project specified in session.
+Description: Generate HTML to describe & control datasets belonging to
+the project specified in session.
 
 =cut
 
 package OME::Web::ProjectDataset;
 
 use strict;
-use vars qw($VERSION @ISA);
+use vars qw($VERSION);
 $VERSION = '1.0';
 use CGI;
-use OME::Web;
-@ISA = ("OME::Web");
+use OME::Web::Validation;
+use OME::Dataset;
+use base qw{ OME::Web };
 
 sub getPageTitle {
 	return "Open Microscopy Environment - Datasets in this project";
@@ -45,13 +46,74 @@ sub getPageBody {
 	my $cgi = $self->CGI();
 	my $body = "";
 	my $session = $self->Session();
+	my $project = $session->project();
 	
-	# do we need to do anything?
-	# ... check for switch datasets in cgi params, switch, update title bar, display message
+	# This is not meant to take the place of OME::Web::Validation->isRedirectNecessary
+	# It is explicit variable validity verification. Validation may eventually have other checks as well. This only needs these checks.
+	if( not defined $project ) {
+		$body .= OME::Web::Validation->ReloadHomeScript();
+		return ("HTML",$body);
+	}
+
+	# cgi parameters to remove & switch datasets show up as ID=Remove and ID=Select
+	# I need to reverse name, value pairs in the cgi hash to see if Remove or Select is in there
+	my @names = $cgi->param();
+	my %revArgs = map { $cgi->param($_) => $_ } @names;
+
+# FIXME: Some validation is needed for these.
+	# Àdo we need to remove a dataset?
+	if( exists $revArgs{Remove} ) {
+		my $dataset = $session->Factory()->loadObject("OME::Dataset",$revArgs{Remove});
+		if( not defined $dataset ) {
+			die "Could not load dataset ( ID = '".$revArgs{Remove}."' ). It has not been removed.<br>";
+		}
+		else {
+			my $name = $dataset->name();
+			# call a function in project to remove this dataset. don't forget to error check for session's dataset & save everything
+	
+			# this will add a script to reload OME::Home if it's necessary
+			$body .= OME::Web::Validation->ReloadHomeScript();
+#			$body .= "Dataset '$name' has been deleted.<br>";
+			$body .= "I haven't implemented this feature yet.<br>";
+		}
+	}
+	# Àdo we need to switch to another dataset?
+	if( exists $revArgs{Select} ) {
+		my $dataset = $session->Factory()->loadObject("OME::Dataset",$revArgs{Select})
+			or die "Unable to load dataset ( ID = ".$revArgs{Select}." ). Action cancelled<br>";
+		if( $project->doesDatasetBelong( $dataset ) ) {
+		# does this dataset belong to the current project?
+			$session->dataset($dataset);
+			$session->writeObject();
+	
+			# this will add a script to reload OME::Home if it's necessary
+			$body .= OME::Web::Validation->ReloadHomeScript();
+			$body .= "Operation successful. Current dataset is: ".$session->dataset()->name()."<br>";
+
+			# update titlebar
+			$body .= "<script>top.title.location.href = top.title.location.href;</script>";
+		} else {
+			die "Dataset '".$dataset->name."' does not belong to the current project.<br>";
+		}
+	}
+	# Àdo we need to add a dataset to the project?
+	if( defined $cgi->param('addDataset') ) {
+		my $dataset = $project->addDatasetID( $cgi->param('addDatasetID') )
+			or die ref $self." died when trying to add dataset (".$cgi->param('addDatasetID');
+		$session->dataset($dataset);
+		$session->writeObject();
+		$project->writeObject();
+		
+		$body .= "Dataset '".$dataset->name()."' successfully added to this project and set to current dataset.<br>";
+		# this will add a script to reload OME::Home if it's necessary
+		$body .= OME::Web::Validation->ReloadHomeScript();
+
+		# update titlebar
+		$body .= "<script>top.title.location.href = top.title.location.href;</script>";
+	}
 
 	# display datasets that user owns 
 	$body .= $self->print_form();
-	
 
     return ('HTML',$body);
 }
@@ -61,6 +123,17 @@ sub print_form {
 	my $session = $self->Session();
 	my $project = $session->project();
 	my @projectDatasets = $project->datasets();
+	my $user    = $session->User()
+		or die ref ($self)."->print_form() say: There is no user defined for this session.";
+	my @groupDatasets = OME::Dataset->search( group_id => $user->group()->group_id() );
+	my %datasetList;
+	foreach (@groupDatasets) {
+	print STDERR "\n".$project->doesDatasetBelong($_)." ".$_->ID();
+		if (not $project->doesDatasetBelong($_)) {
+			$datasetList{$_->ID()} = $_->name();
+		}
+	}
+
 	my $cgi = $self->CGI();
 	my $text = '';
 	my ($tableRows);
@@ -75,11 +148,14 @@ sub print_form {
 				$cgi->td( { -align=>'LEFT' },
 					( $_->locked() == 0 ? 'Unlocked' : 'Locked' ) ),
 				$cgi->td( { -align=>'LEFT' },
-					'<font color=red>[button goes here]</font>'),
+					$cgi->submit( { -name  => $_->dataset_id() ,
+					                -value => 'Remove' } ) ),
 				$cgi->td( { -align=>'LEFT' },
-					'<font color=red>[button goes here]</font>'));
+					$cgi->submit( { -name  => $_->dataset_id() ,
+					                -value => 'Select' } ) ) );
 	}
 	
+	$text .= $cgi->startform;
 	$text .=
 		$cgi->table( { -border=>1 },
 			$cgi->Tr( { -valign=>'MIDDLE' },
@@ -93,7 +169,18 @@ sub print_form {
 					'<b>Make this the current dataset</b>' ) ),
 			$tableRows );
 	
-	$text .= '<p><font color=red>[Something to add datasets to this projects should go here.]</font><br>Which datasets should the user be able to add? Ones in this user\'s group that aren\'t already in this project? Should they go in a popup? the popup could get might big.</p>';
+	$text .= "<br>".
+		$cgi->popup_menu (
+			-name => 'addDatasetID',
+			-values => [keys %datasetList],
+			-labels => \%datasetList
+		).$cgi->submit (
+			-name=>'addDataset',
+			-value=>'add Dataset to this project');
+
+
+	$text .= '<p>Which datasets should the user be able to add? Currently it holds every dataset in the users group. I think it should limit it further to ones that aren\'t already in this project. Someone needs to do that. Should they go in a popup? the popup could get might big.</p>';
+	$text .= $cgi->endform;
 	$text .= '<br>What else would you like to do with these? Think about it. <a href="mailto:igg@nih.gov,bshughes@mit.edu,dcreager@mit.edu,siah@nih.gov,a_falconi_jobs@hotmail.com">email</a> the developers.';
 	
 	return $text;
