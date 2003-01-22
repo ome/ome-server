d0e78bad5d742aaa94dc5918a04c9104928f7b0a
Josiah Johnston <siah@nih.gov>
2003-01-22 20:26:09 +0000

    removed class variable Session
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@464 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME.pm
+++ src/perl2/OME.pm
@@ -1,13 +1,7 @@
 
 package OME;
 use strict;
-our $VERSION = '2.0';
-
-use base qw(Class::Data::Inheritable);
-# currently the 'Session' class variable is set by OME::SessionManager and erased by OME::Web::Logout
-# Web, DBobject, and Factory access it in their Session accessor methods
-__PACKAGE__->mk_classdata('Session');
-
+our $VERSION = '2.0'
 
 ########################################### main pod documentation begin ##
 # Below is the stub of documentation for your module. You better edit it!
