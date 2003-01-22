13732c151cce88fbbed761f65c6eec2998481315
Josiah Johnston <siah@nih.gov>
2003-01-22 20:06:05 +0000

    stores database connection info that previously was in SessionManager
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@454 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/DBConnection.pm
@@ -0,0 +1,36 @@
+# OME::DBConnection
+
+# Copyright (C) 2002 Open Microscopy Environment, MIT
+# Author:  Douglas Creager <dcreager@alum.mit.edu>
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+package OME::DBConnection;
+our $VERSION = '1.00';
+
+use strict;
+
+use base qw(Class::Accessor Class::Data::Inheritable);
+
+__PACKAGE__->mk_classdata('DataSource');
+__PACKAGE__->mk_classdata('DBUser');
+__PACKAGE__->mk_classdata('DBPassword');
+
+__PACKAGE__->DataSource("dbi:Pg:dbname=ome");
+__PACKAGE__->DBUser(undef);
+__PACKAGE__->DBPassword(undef);
+
+1;
