230f21eb07c2f9e77747692e6bfd90525d7c679f
Josiah Johnston <siah@nih.gov>
2003-02-05 16:29:37 +0000

    Usage check before login now
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@482 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tests/ImportModule.pl
+++ src/perl2/OME/Tests/ImportModule.pl
@@ -2,11 +2,13 @@ use strict;
 use OME::Tasks::ProgramImport;
 use OME::SessionManager;
 
-my $session = OME::SessionManager->TTYlogin();
 if (scalar(@ARGV) != 1) {
     print "Usage:  ImportModule <path to XML spec>\n\n";
     exit -1;
 }
+
+my $session = OME::SessionManager->TTYlogin();
+
 
 OME::Tasks::ProgramImport->importXMLFile( $session, $ARGV[0] );
 
