34f63a43c8c98ce69fea88a05e79ae9d3c45365b
Ilya Goldberg <igg@nih.gov>
2003-02-26 03:15:28 +0000

    Added XML dependencies + a little refactoring
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@550 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/perl/perlCheck.pl
+++ src/Attic/perl/perlCheck.pl
@@ -4,7 +4,7 @@ use Config;
 use Cwd;
 my $cwd = getcwd;
 my $libDir = $Config{installprivlib};
-
+use File::Spec;
 
 my $moduleRepository = 'http://openmicroscopy.org/packages/perl';
 my $DEFAULT_badTestsFatal = 0;
@@ -100,7 +100,7 @@ my @modules = ({
 	},{
 	Name => 'Date::Simple',
 	repositoryFile => 'Date-Simple-2.04.tar.gz',
-        },{
+	},{
 	Name => 'Class::DBI',
 	repositoryFile => 'Class-DBI-0.90.tar.gz',
 	checkVersion => \&Class_DBI_VersionOK,
@@ -111,6 +111,25 @@ my @modules = ({
 	Name => 'Image::Magick',
 	repositoryFile => 'ImageMagick-5.3.6-OSX.tar.gz',
 	installModule => \&ImageMagickInstall
+	},{
+	Name => 'XML::NamespaceSupport',
+	repositoryFile => 'XML-NamespaceSupport-1.08.tar.gz'
+	},{
+	Name => 'XML::Sax',
+	repositoryFile => 'XML-SAX-0.12.tar.gz',
+	getVersion => \&XML_SAX_getVersion
+	},{
+	Name => 'libxml2',
+	getVersion => \&libXMLgetVersion,
+	checkVersion => \&libXML_VersionOK,
+	repositoryFile => '../source/libxml2-2.4.30.tar.gz',
+	installModule => \&LibXMLInstall,
+	},{
+	Name => 'XML::LibXML::Common',
+	repositoryFile => 'XML-LibXML-Common-0.12.tar.gz'
+	},{
+	Name => 'XML::LibXML',
+	repositoryFile => 'XML-LibXML-1.53.tar.gz',
 	}
 );
 
@@ -137,15 +156,20 @@ foreach (@modules) {
 # chdir back to the OME perl directory
 chdir ('..') or die "Couldn't change working directory to '..': $!\n";
 
+#IGG 2/25/2003: removed this old OME 1.0 stuff.  There's now a proper Makefile.PL for OME 2.0
 # Make symlinks between the private perl module directory and OME modules:
 # Where to install private libs: $Config{installprivlib}
-my @OMEmodules = ('OMEpl.pm','OMEDataset.pm','OMEDataset','OMEfeature.pm','OMEwebLogin.pm');
-foreach (@OMEmodules) {
-	if (-l $libDir.'/'.$_) {
-		unlink ("$libDir/$_") or die "Could't delete '$libDir/$_': $!\n";
-	}
-	symlink ("$cwd/$_", "$libDir/$_") or die "Could't make a symbolic link to '$cwd/$_' from '$libDir/$_':  $!";
-}
+#my @OMEmodules = ('OMEpl.pm','OMEDataset.pm','OMEDataset','OMEfeature.pm','OMEwebLogin.pm');
+#foreach (@OMEmodules) {
+#	if (-l $libDir.'/'.$_) {
+#		unlink ("$libDir/$_") or die "Could't delete '$libDir/$_': $!\n";
+#	}
+#	symlink ("$cwd/$_", "$libDir/$_") or die "Could't make a symbolic link to '$cwd/$_' from '$libDir/$_':  $!";
+#}
+
+print "\nInstallation of OME Perl Module dependencies is complete.\n";
+print   "---------------------------------------------------------\n";
+
 ######################
 
 
@@ -174,9 +198,19 @@ my $version = shift;
 
 sub Class_DBI_VersionOK {
 my $version = shift;
-	return (1) if $version > 0.90;
+	return (1) if $version >= 0.90;
 	return (0);
 }
+
+sub libXML_VersionOK {
+my $version = shift;
+	my ($major,$minor,$rev) = split ('\.',$version);
+	$version = $major.'.'.$minor;
+
+	return (0) if not defined $version or $version < 2.4;
+	return (1);
+}
+
 
 
 ############################
@@ -185,8 +219,7 @@ sub DBD_Pg_Install {
 my $module = shift;
 my (undef,$pgVersion) = split (' ',`pg_config --version`);
 my $installTarBall = $module->{repositoryFile};
-my $installDir;
-	if ($installTarBall =~ /(.*)\.tar\.gz/) {$installDir = $1};
+my $installDir = $module->{installDir};
 my $error;
 
 
@@ -214,8 +247,7 @@ my $badTestsFatal;
 $badTestsFatal = $module->{badTestsFatal} if exists $module->{badTestsFatal};
 $badTestsFatal = $DEFAULT_badTestsFatal unless defined $badTestsFatal;
 my $installTarBall = $module->{repositoryFile};
-my $installDir;
-	if ($installTarBall =~ /(.*)\.tar\.gz/) {$installDir = $1};
+my $installDir = $module->{installDir};
 my $error;
 my @configFlags = (
 	'--enable-shared',
@@ -250,6 +282,64 @@ my @configFlags = (
 }
 
 
+sub LibXMLInstall {
+my $module = shift;
+my $installTarBall = $module->{repositoryFile};
+my $installDir = $module->{installDir};
+my $error;
+
+
+	print "\nInstalling $installDir\n";
+	chdir $installDir or die "Couldn't change working directory to $installDir.\n";
+
+	die "Couldn't execute configure script.\n" if system ('./configure') != 0;
+	die "Compilation errors - script aborted.\n" if system ('make') != 0;
+	die "Install errors - script aborted.\n" if system ('make install') != 0;
+	my $libXMLVersion = `xml2-config --version`;
+	chomp ($libXMLVersion);
+
+	die "Installation error:  libxml2 seems to have installed OK, but 'xml2-config' looks funny ('$libXMLVersion')\n" 
+		unless defined $libXMLVersion and length ($libXMLVersion ) > 1;
+	chdir '..';
+	
+}
+
+
+
+##################################################################
+# Special GetVersion subs:
+
+#XML::SAX v0.12 doesn't report a VERSION even if it loads in perl 5.6.0
+#XML::SAX won't load this way in perl 5.6.1
+#XML::SAX::ParserFactory loads OK and reports proper VERSION string in both cases.
+sub XML_SAX_getVersion {
+my $moduleName = shift->{Name};
+my $moduleVersion;
+my $loadModule = "use XML::SAX::ParserFactory; ".'$moduleVersion = $XML::SAX::ParserFactory::VERSION;';
+
+	eval ($loadModule);
+	print "Eval errors: $@\n" if $@;
+
+	$moduleVersion = '' if not $@ and not defined $moduleVersion;
+
+	return $moduleVersion;
+}
+
+# get the version of libxml2
+sub libXMLgetVersion {
+my $module = shift;
+my $libXMLVersion = `xml2-config --version`;
+
+	chomp ($libXMLVersion);
+	my ($major,$minor,$rev) = split ('\.',$libXMLVersion);
+	$libXMLVersion = $major.'.'.$minor;
+	
+	return ($libXMLVersion);
+	
+}
+
+
+
 ##################################################################
 # Should not have to modify below here when specifying new modules
 sub CheckModule {
@@ -257,25 +347,31 @@ my $module = $_;
 my $moduleName = $module->{Name};
 my $repositoryFile = $module->{repositoryFile};
 my $moduleVersion;
+my $getVersion;
 my $checkVersion;
 my $installModule;
-$checkVersion = $module->{checkVersion} if exists $module->{checkVersion}
-	and defined $module->{checkVersion} and $module->{checkVersion};
-$installModule = $module->{installModule} if exists $module->{installModule}
-	and defined $module->{installModule} and $module->{installModule};
-$installModule = \&InstallModule unless defined $installModule;
+
+	$getVersion = $module->{getVersion} if exists $module->{getVersion}
+		and defined $module->{getVersion} and $module->{getVersion};
+	$getVersion = \&GetModuleVersion unless defined $getVersion;
+
+	$checkVersion = $module->{checkVersion} if exists $module->{checkVersion}
+		and defined $module->{checkVersion} and $module->{checkVersion};
+	$installModule = $module->{installModule} if exists $module->{installModule}
+		and defined $module->{installModule} and $module->{installModule};
+	$installModule = \&InstallModule unless defined $installModule;
 
 
 	
-	$moduleVersion = GetModuleVersion ($moduleName);
+	$moduleVersion = &{$getVersion} ($module);
 
-	if (not (defined $moduleVersion and $moduleVersion)) {
+	if (not defined $moduleVersion) {
 		print "Couldn't find a functioning $moduleName\n";
-		DownloadModule ($repositoryFile);
+		$module->{installDir} = DownloadModule ($repositoryFile);
 		&{$installModule}($module);
 	} elsif (defined $checkVersion and not &{$checkVersion}($moduleVersion)) {
 		print "$moduleName version is $moduleVersion - not supported\n";
-		DownloadModule ($repositoryFile);
+		$module->{installDir} = DownloadModule ($repositoryFile);
 		&{$installModule}($module);
 	} else {
 		print "$moduleName version is $moduleVersion - OK\n";
@@ -284,11 +380,16 @@ $installModule = \&InstallModule unless defined $installModule;
 }
 
 sub GetModuleVersion {
-my $moduleName = shift;
+my $moduleName = shift->{Name};
 my $moduleVersion;
-my $command = "perl -e 'require $moduleName;print \$$moduleName"."::VERSION'";
+#my $command = "perl -e 'use $moduleName;print \$$moduleName"."::VERSION'";
+my $eval = "use $moduleName; ".'$moduleVersion = $'.$moduleName.'::VERSION;';
+
+	eval ($eval);
+	print "Eval errors: $@\n" if $@;
 	
-	$moduleVersion = `$command`;
+	$moduleVersion = '' if not $@ and not defined $moduleVersion;
+	
 	return $moduleVersion;
 }
 
@@ -301,6 +402,9 @@ my $wget;
 my $error;
 my $moduleURL = "$moduleRepository/$installTarBall";
 my $installDir;
+
+	(undef,undef,$installTarBall) = File::Spec->splitpath( $installTarBall );
+
 	if ($installTarBall =~ /(.*)\.tar\.gz/) {$installDir = $1};
 
 	if (not -e $installDir) {
@@ -332,6 +436,7 @@ my $installDir;
 
 	chdir $installDir or die "Couldn't change working directory to $installDir.\n";
 	chdir '..';
+	return ($installDir);
 }
 
 
@@ -341,8 +446,7 @@ my $installTarBall = $module->{repositoryFile};
 my $badTestsFatal;
 $badTestsFatal = $module->{badTestsFatal} if exists $module->{badTestsFatal};
 $badTestsFatal = $DEFAULT_badTestsFatal unless defined $badTestsFatal;
-my $installDir;
-	if ($installTarBall =~ /(.*)\.tar\.gz/) {$installDir = $1};
+my $installDir = $module->{installDir};
 my $error;
 
 	print "\nInstalling $installDir\n";
