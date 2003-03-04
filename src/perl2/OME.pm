0611bff978ec1cdcaa6b73a2363123cd51b0fe0f
Doug Creager <dcreager@alum.mit.edu>
2003-03-04 05:26:12 +0000

    Documentation
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@570 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME.pm
+++ src/perl2/OME.pm
@@ -1,3 +1,21 @@
+# OME.pm
+
+# Copyright (C) 2002 Open Microscopy Environment, MIT
+# Author:  Ilya Goldberg <igg@nih.gov>
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package OME;
 use strict;
@@ -11,10 +29,10 @@ our $VERSION = '2.0';
 
 OME - The Open Microscopy Environment
 
-More information about the Perl OME API can be obtained by looking at documentation for
-the insividual classes:
+More information about the Perl OME API can be obtained by looking at
+documentation for the individual classes:
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 	Ilya Goldberg, Doug Creager, Brian Hughes, Josiah Johnston
 	igg@nih,gov, dcreager@alum.mit.edu, bshughes@mit.edu, siah@nih.gov
