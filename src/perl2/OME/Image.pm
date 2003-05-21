72bd56368eff2cf5ae201370252b45450d7e5bde
Doug Creager <dcreager@alum.mit.edu>
2003-05-21 20:27:43 +0000

    Fixed typo in experimenter()
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@888 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Image.pm
+++ src/perl2/OME/Image.pm
@@ -62,7 +62,7 @@ sub experimenter {
         my $attribute = shift;
         die "Owner must be an Experimenter"
           unless $attribute->attribute_type()->name() eq "Experimenter";
-        return $self->owner_id($attribute->id());
+        return $self->experimenter_id($attribute->id());
     } else {
         return $self->Session()->Factory()->loadAttribute("Experimenter",
                                                           $self->experimenter_id());
