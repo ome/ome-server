8925cb59f1f0385def29beceed10a4a610087544
Ilya Goldberg <igg@nih.gov>
2003-05-22 15:52:28 +0000

    Added debug logging switch based on ENV{OME_DEBUG} via logconfig
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@899 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tests/UberImport.pl
+++ src/perl2/OME/Tests/UberImport.pl
@@ -2,13 +2,35 @@ use strict;
 use OME::Tasks::OMEImport;
 use OME::SessionManager;
 use XML::LibXML;
-
+use Log::Agent;
 if( ! $ARGV[0] ) {
 	print "Usage is:\n\t perl UberImport.pl [file1] [file2] ...\n";
 	exit -1;
 }
+if ($ENV{OME_DEBUG} > 0) {
+#	print 'Debugging output enabled';
+#	logconfig(
+#		-driver    => Log::Agent::Driver::File->make(
+#			-prefix      => '$0',
+#			-showpid     => 1,
+#			-channels    => {
+#	#			 'error'  => '/OME/Logs/OME.err',
+#	#			 'output' => '/OME/Logs/OME.out',
+#				 'debug'  => "$0.dbg",
+#			}
+#		),
+#	# for now, debug output is on.
+#		-level    => 'debug'
+#	);
+	
+	logconfig(
+		-prefix      => "$0",
+		-level    => 'debug'
+	);
+}
 
 my $session = OME::SessionManager->TTYlogin();
+$session->DBH()->trace(3);
 my $OMEImporter = OME::Tasks::OMEImport->new( session => $session, debug => 1 );
 
 map( $OMEImporter->importFile( $_ ), @ARGV );
