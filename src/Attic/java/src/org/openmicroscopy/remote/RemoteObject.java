38f38f1d0b08ed788de7c3d00e1f1b1ea99f2619
Doug Creager <dcreager@alum.mit.edu>
2003-05-23 00:36:19 +0000

    Return values of accessors are cached to reduce RPC overhead
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@911 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/remote/RemoteObject.java
+++ src/Attic/java/src/org/openmicroscopy/remote/RemoteObject.java
@@ -48,6 +48,8 @@ public class RemoteObject
     public RemoteObject() { this.reference = null; }
     public RemoteObject(String reference) { this.reference = reference; }
 
+    protected Map elementCache = new HashMap();
+
     protected void finalize()
     {
         //System.err.println("finalize "+getClass()+"."+reference);
@@ -91,10 +93,29 @@ public class RemoteObject
         }
     }
 
+    private Object getCachedElement(String element)
+    {
+        if (elementCache.containsKey(element))
+        {
+            //System.err.println("--- cache "+getClass()+" "+element);
+            return elementCache.get(element);
+        } else {
+            //System.err.println("--- NO CACHE "+getClass()+" "+element);
+            Object o = caller.dispatch(this,element);
+            elementCache.put(element,o);
+            return o;
+        }
+    }
+
+    private void saveElement(String element, Object value)
+    {
+        elementCache.put(element,value);
+        caller.dispatch(this,element,value);
+    }
 
     protected boolean getBooleanElement(String element)
     {
-        Object o = caller.dispatch(this,element);
+        Object o = getCachedElement(element);
         if (o instanceof String)
         {
             String s = (String) o;
@@ -106,115 +127,140 @@ public class RemoteObject
                 s.equalsIgnoreCase("1");
         } else if (o instanceof Boolean) {
             return ((Boolean) o).booleanValue();
+        } else if (o == null) {
+            return false;
         } else {
+            if (o == null) o = "null";
             throw new RemoteException(element+": expect boolean, got "+o.getClass());
         }
     }
     protected void setBooleanElement(String element, boolean value)
-    { caller.dispatch(this,element,new Boolean(value)); }
+    { saveElement(element,new Boolean(value)); }
 
     protected int getIntElement(String element)
     {
-        Object o = caller.dispatch(this,element);
+        Object o = getCachedElement(element);
         if (o instanceof String)
         {
             String s = (String) o;
             return Integer.parseInt(s);
         } else if (o instanceof Integer) {
             return ((Integer) o).intValue();
+        } else if (o == null) {
+            return 0;
         } else {
+            if (o == null) o = "null";
             throw new RemoteException(element+": expect int, got "+o.getClass());
         }
     }
     protected void setIntElement(String element, int value)
-    { caller.dispatch(this,element,new Integer(value)); }
+    { saveElement(element,new Integer(value)); }
 
     protected long getLongElement(String element)
     {
         // XML-RPC's only integer type is Integer
-        Object o = caller.dispatch(this,element);
+        Object o = getCachedElement(element);
         if (o instanceof String)
         {
             String s = (String) o;
             return Long.parseLong(s);
         } else if (o instanceof Integer) {
             return (long) ((Integer) o).intValue();
+        } else if (o == null) {
+            return 0L;
         } else {
+            if (o == null) o = "null";
             throw new RemoteException(element+": expect long, got "+o.getClass());
         }
     }
     protected void setLongElement(String element, long value)
-    { caller.dispatch(this,element,new Integer((int) value)); }
+    { saveElement(element,new Integer((int) value)); }
 
     protected float getFloatElement(String element)
     {
         // XML-RPC's only floating-point type is Double
-        Object o = caller.dispatch(this,element);
+        Object o = getCachedElement(element);
         if (o instanceof String)
         {
             String s = (String) o;
             return Float.parseFloat(s);
         } else if (o instanceof Double) {
             return (float) ((Double) o).doubleValue();
+        } else if (o == null) {
+            return 0.0F;
         } else {
+            if (o == null) o = "null";
             throw new RemoteException(element+": expect float, got "+o.getClass());
         }
     }
     protected void setFloatElement(String element, float value)
-    { caller.dispatch(this,element,new Double(value)); }
+    { saveElement(element,new Double(value)); }
 
     protected double getDoubleElement(String element)
     {
-        Object o = caller.dispatch(this,element);
+        Object o = getCachedElement(element);
         if (o instanceof String)
         {
             String s = (String) o;
             return Double.parseDouble(s);
         } else if (o instanceof Double) {
             return ((Double) o).doubleValue();
+        } else if (o == null) {
+            return 0.0D;
         } else {
+            if (o == null) o = "null";
             throw new RemoteException(element+": expect boolean, got "+o.getClass());
         }
     }
     protected void setDoubleElement(String element, double value)
-    { caller.dispatch(this,element,new Double(value)); }
+    { saveElement(element,new Double(value)); }
 
     protected String getStringElement(String element)
     {
-        Object o = caller.dispatch(this,element);
+        Object o = getCachedElement(element);
         if (o instanceof String)
         {
             return (String) o;
+        } else if (o == null) {
+            return null;
         } else {
+            if (o == null) o = "null";
             throw new RemoteException(element+": expect String, got "+o.getClass());
         }
     }
     protected void setStringElement(String element, String value)
-    { caller.dispatch(this,element,value); }
+    { saveElement(element,value); }
 
     protected Object getObjectElement(String element)
-    { return caller.dispatch(this,element); }
+    { return getCachedElement(element); }
     protected void setObjectElement(String element, Object value)
-    { caller.dispatch(this,element,value); }
+    { saveElement(element,value); }
 
     protected RemoteObject getRemoteElement(Class clazz,
                                             String element)
     {
-        Object o = caller.dispatch(this,element);
+        Object o = getCachedElement(element);
         if (o instanceof String)
         {
-            return instantiate(clazz,(String) o);
+            RemoteObject ro =  instantiate(clazz,(String) o);
+            elementCache.put(element,ro);
+            return ro;
+        } else if (o instanceof RemoteObject) {
+            return (RemoteObject) o;
+        } else if (o == null) {
+            return null;
         } else {
+            if (o == null) o = "null";
             throw new RemoteException(element+": expect String (ref "+clazz+"), got "+o.getClass());
         }
     }
     protected void setRemoteElement(String element, Object value)
-    { caller.dispatch(this,element,value); }
+    { saveElement(element,value); }
 
     protected List getRemoteListElement(Class clazz,
                                         String element)
     {
-        Object o = caller.dispatch(this,element);
+        Object o = getCachedElement(element);
         if (o instanceof List)
         {
             List refList = (List) o;
@@ -223,15 +269,31 @@ public class RemoteObject
             while (i.hasNext())
                 objList.add(instantiate(clazz,(String) i.next()));
             return objList;
+        } else if (o == null) {
+            return null;
         } else {
+            if (o == null) o = "null";
             throw new RemoteException(element+": expect List (of "+clazz+"), got "+o.getClass());
         }
     }
 
     protected Attribute getAttributeElement(String element)
-    { return (Attribute)
-          instantiate(RemoteAttribute.class,
-                      caller.dispatch(this,element)); }
+    {
+        Object o = getCachedElement(element);
+        if (o instanceof String)
+        {
+            Attribute a = (Attribute) instantiate(RemoteAttribute.class,(String) o);
+            elementCache.put(element,a);
+            return a;
+        } else if (o instanceof Attribute) {
+            return (Attribute) o;
+        } else if (o == null) {
+            return null;
+        } else {
+            if (o == null) o = "null";
+            throw new RemoteException(element+": expect String (Attribute), got "+o.getClass());
+        }
+    }
     protected void setAttributeElement(String element, Attribute value)
-    { caller.dispatch(this,element,value); }
+    { saveElement(element,value); }
 }
