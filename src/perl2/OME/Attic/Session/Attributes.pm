8f66da85faaa5771375b53401319d529f5191688
Josiah Johnston <siah@nih.gov>
2003-06-24 21:53:29 +0000

    removed commented out line
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1231 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Attic/Session/Attributes.pm
+++ src/perl2/OME/Attic/Session/Attributes.pm
@@ -17,7 +17,6 @@ __PACKAGE__->sequence('session_seq');
 __PACKAGE__->columns(Primary => qw(session_id));
 __PACKAGE__->columns(Essential => qw(session_key module_execution));
 __PACKAGE__->columns(Others => qw(host image_view feature_view display_settings last_access started experimenter_id project_id));
-#__PACKAGE__->hasa('OME::Experimenter' => qw(experimenter_id));
 __PACKAGE__->hasa('OME::Project' => qw(project_id));
 
 1;
