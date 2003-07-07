81e596c55f0aba69c3056d67ce8b16ab0df21a34
Chris Allan <callan@lifesci.dundee.ac.uk>
2003-07-07 14:06:04 +0000

    Commit before migration to omed.
    New:
    - CLI flags and logic (see perl StandaloneServe.pl -h) for more details
    - Daemonization (default)
    - Removal of icky environment variable checking
    
    Bug fixes:
    - undef hash key fix in dispatch_with()
    - Comments and various logic fixes
    
    
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1396 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Remote/StandaloneServer.pl
+++ src/perl2/OME/Remote/StandaloneServer.pl
@@ -1,9 +1,14 @@
-#!/usr/bin/perl -w 
+#!/usr/bin/perl
 
+use warnings;
+use strict;
 use SOAP::Lite;
-#SOAP::Trace->import('all');
 use XMLRPC::Lite;
+use Getopt::Long;
+use POSIX;
 
+# We'd rather not have tracing right now :)
+#SOAP::Trace->import('all');
 
 use OME::Remote::Dispatcher;
 my @dispatchObjects =
@@ -14,39 +19,126 @@ use XMLRPC::Transport::HTTP;
 $SOAP::Constants::DO_NOT_USE_XML_PARSER = 1;
 $XMLRPC::Constants::DO_NOT_USE_XML_PARSER = 1;
 
+#*********
+#********* GLOBALS AND DEFINES
+#*********
+
+# Command line options
+my ($show_calls, $show_results, $show_caching, $verbose, $help, $debug);
+
+# Command line defaults
+my $port = 8002;
+my $transport = "soap";
+
+#*********
+#********* LOCAL SUBROUTINES
+#*********
+
+sub usage {
+    my $usage = <<USAGE;
+OME Standalone server, which implements the OME Remote API via XMLRPC or SOAP.
+
+Usage:
+  $0 [options]
+
+Options:
+  -s, --show-calls		Show the remote framework calls
+  -e, --show-results		Show the result of remote framework calls
+  -c, --show-caching		Show caching being done by the server
+  -p, --port			Listening port
+
+  -d, --debug			Do not release the controlling terminal (follow
+				output from the daemon [sets --verbose])
+  -v, --verbose			Verbose output (same as -s,-e,-c)
+  -h, --help			This message
+
+Transport options:
+  -t, --transport <xmlrpc,soap>	Use either an XMLRPC or SOAP transport
+
+Report bugs to <ome-devel\@mit.edu>.
+USAGE
+
+	print STDOUT $usage;
+	exit (0);
+}
+
+#*********
+#********* START OF CODE
+#*********
+
+
+# CLI flag logic
+
+GetOptions ("s|show-calls", \$show_calls,	# Show call generation
+	    "d|show-results", \$show_results,	# Show call results
+	    "n|show-caching", \$show_caching,	# Show server caching
+	    "p|port=s", \$port,			# Listening port
+	    "d|debug", \$debug,			# Debug mode
+	    "v|verbose", \$verbose,		# Verbose operation
+	    "t|transport=s", \$transport,	# Transport
+	    "h|help", \$help			# Show usage
+	    );
+
+if ($help) { usage () }
+
+if ((lc($transport) ne "xmlrpc") && (lc($transport) ne "soap") ) {
+    print "Unknown transport: $transport\n";
+    usage ();
+}
+
+if ($debug) { $verbose = 1 }
+if ($verbose) { $show_calls = 1; $show_results = 1; $show_caching = 1; }
+if ($show_calls) { $OME::Remote::Dispatcher::SHOW_CALLS = 1 }
+if ($show_results) { $OME::Remote::Dispatcher::SHOW_RESULTS = 1 } 
+if ($show_caching) { $OME::Remote::Dispatcher::SHOW_CACHING = 1 }
+
+# Fork once so that the parent can exit unless we're debugging
+unless ($debug) {
+    # Okay we're not debugging lets fork
+    my $pid = fork;
+    exit if $pid;
+    die "Couldn't fork. $!" unless defined ($pid);
+
+    # Start a new session so we loose our controling terminal
+    POSIX::setsid () or die "Can't start a new session. $!";
+}
+
+# Ignore SIGPIPE
 $SIG{PIPE} = 'IGNORE';
 
-print "Using dispatch objects\n  ",join("\n  ",@dispatchObjects),"\n";
+# Let everyone know which Dispatcher we're using
+if ($debug) {  # FIXME: This is brain-dead, should use a proper output chooser
+    print "Using dispatch objects...\n  \\_ ", join("\n  \\_ ",@dispatchObjects), "\n";
+}
 
-$OME::Remote::Dispatcher::SHOW_CALLS = $ENV{OME_SHOW_CALLS}
-  if exists $ENV{OME_SHOW_CALLS};
-$OME::Remote::Dispatcher::SHOW_RESULTS = $ENV{OME_SHOW_RESULTS}
-  if exists $ENV{OME_SHOW_RESULTS};
-$OME::Remote::Dispatcher::SHOW_CACHING = $ENV{OME_SHOW_CACHING}
-  if exists $ENV{OME_SHOW_CACHING};
+my ($transport_class, $style, $daemon);
 
-my $port = $ARGV[0] || 8002;
+if (lc($transport) eq "xmlrpc") {
+    $transport_class = "XMLRPC::Transport::HTTP::Daemon";
 
-my ($transportClass, $style, $daemon);
-
-if ((defined $ARGV[1]) && (uc($ARGV[1]) eq "XMLRPC")) {
-    $transportClass = "XMLRPC::Transport::HTTP::Daemon";
-    $daemon = $transportClass
+    # Dispatch all method calls to the dispatcher
+    # all is "" in dispatch_with()
+    $daemon = $transport_class
       ->new(LocalPort => $port)
-      ->dispatch_to('OME::Remote::Dispatcher')
-      ->dispatch_with({undef,'OME::Remote::Dispatcher'});
+      ->dispatch_to('OME::Remote::Dispatcher')		
+      ->dispatch_with({"", 'OME::Remote::Dispatcher'});
     $style = "XML-RPC";
-} else {
-    $transportClass = "SOAP::Transport::HTTP::Daemon";
-    $daemon = $transportClass
+} elsif (lc($transport) eq "soap") {
+    $transport_class = "SOAP::Transport::HTTP::Daemon";
+
+    # Just dispatch the method calls to @dispatchObjects
+    $daemon = $transport_class
       ->new(LocalPort => $port)
       ->dispatch_to(@dispatchObjects);
     $style = "SOAP";
+} else { 
+    die "Woah! Unknown transport type.";
 }
 
 #$daemon->on_action(sub { print "A***(",join(',',@_),")\n"; });
 #$daemon->on_dispatch(sub { print "D***(",join(',',@_),")\n"; });
 
-
-print "Connect to $style server at ", $daemon->url(), ".\n";
+if ($debug) {  # FIXME: See previous if ($debug)
+    print "Connect to $style server at ", $daemon->url(), ".\n";
+}
 $daemon->handle();
