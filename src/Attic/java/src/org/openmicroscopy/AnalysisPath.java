b6357b3110c19e2223a7c9810618b081ab45a7fe
Doug Creager <dcreager@alum.mit.edu>
2003-07-18 15:35:56 +0000

    Added Javadocs
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1450 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/AnalysisPath.java
+++ src/Attic/java/src/org/openmicroscopy/AnalysisPath.java
@@ -24,27 +24,100 @@ package org.openmicroscopy;
 import java.util.List;
 import java.util.Iterator;
 
+/**
+ * <p>Represents a single, linear data path through an analysis chain.
+ * Each chain has one <i>data path</i> for each linear path from a
+ * root node to a leaf node.  (A root node contains no inputs; a leaf
+ * node contains no outputs.  Since analysis chains are acyclic, there
+ * must be at least one of each in any chain.)</p>
+ *
+ * @author Douglas Creager
+ * @version 2.0
+ * @since OME2.0
+ */
+
 public interface AnalysisPath
     extends OMEObject
 {
+    /** 
+     * Returns the number of nodes along this analysis path.
+     * @return the number of nodes along this analysis path
+     */
     public int getPathLength();
+
+    /**
+     * Sets the number of nodes along this analysis path.
+     * @param length the number of nodes along this analysis path
+     */
     public void setPathLength(int length);
 
+    /**
+     * Returns the analysis chain that this path belongs to.
+     * @return the analysis chain that this path belongs to.
+     */
     public Chain getChain();
+
+    /**
+     * Sets the analysis chain that this path belongs to.
+     * @param chain the analysis chain that this path belongs to.
+     */
     public void setChain(Chain chain);
 
+    /**
+     * Returns a list of the nodes in this path.
+     * @return a {@link List} of {@link AnalysisPath.Node Nodes}
+     */
     public List getPathNodes();
+
+    /**
+     * Returns an iterator of the nodes in this path.
+     * @return an {@link Iterator} of {@link AnalysisPath.Node Nodes}
+     */
     public Iterator iteratePathNodes();
+
+    /**
+     * <p>Represents each element in an analysis path.  It corresponds
+     * to one of the nodes in the analysis chain.</p>
+     *
+     * @author Douglas Creager
+     * @version 2.0
+     * @since OME2.0
+     */
 
     public interface Node
         extends OMEObject
     {
+        /**
+         * Returns the position along the analysis path of this entry.
+         * The position is indexed from 1.
+         * @return the position along the analyis path of this entry
+         */
         public int getPathOrder();
+
+        /**
+         * Sets the position along the analysis path of this entry.
+         * The position is indexed from 1.
+         * @param order the position along the analyis path of this
+         * entry
+         */
         public void setPathOrder(int order);
 
+        /**
+         * Returns the analysis path that this entry belongs to.
+         * @return the analysis path that this entry belongs to.
+         */
         public AnalysisPath getAnalysisPath();
 
+        /**
+         * Returns the chain node that this entry correspons to.
+         * @return the chain node that this entry correspons to.
+         */
         public Chain.Node getChainNode();
+
+        /**
+         * Sets the chain node that this entry correspons to.
+         * @param node the chain node that this entry correspons to.
+         */
         public void setChainNode(Chain.Node node);
     }
 }
