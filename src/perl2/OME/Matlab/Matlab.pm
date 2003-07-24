8686f41f542e95dbdec40036b435a650d3b3bd16
Doug Creager <dcreager@alum.mit.edu>
2003-07-24 00:25:27 +0000

    Added copyright notice; fixed up export tags; sets up enum constants from Matlab libraries; removed boilerplate POD docs
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1478 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Matlab/Matlab.pm
+++ src/perl2/OME/Matlab/Matlab.pm
@@ -1,3 +1,22 @@
+# OME/Matlab.pm
+
+# Copyright (C) 2003 Open Microscopy Environment, MIT
+# Author:  Douglas Creager <dcreager@alum.mit.edu>
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 package OME::Matlab;
 
 require 5.005_62;
@@ -9,66 +28,68 @@ require DynaLoader;
 
 our @ISA = qw(Exporter DynaLoader);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
+our %EXPORT_TAGS =
+  (complexities => [qw(
+                       $mxREAL $mxCOMPLEX
+                      )],
+   classes      => [qw(
+                       $mxUNKNOWN_CLASS $mxCELL_CLASS $mxSTRUCT_CLASS
+                       $mxOBJECT_CLASS $mxCHAR_CLASS $mxLOGICAL_CLASS
+                       $mxDOUBLE_CLASS $mxSINGLE_CLASS $mxINT8_CLASS
+                       $mxUINT8_CLASS $mxINT16_CLASS $mxUINT16_CLASS
+                       $mxINT32_CLASS $mxUINT32_CLASS $mxINT64_CLASS
+                       $mxUINT64_CLASS $mxFUNCTION_CLASS
+                      )],
+  );
 
-# This allows declaration	use Matlab ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
+$EXPORT_TAGS{constants} = [
+                           @{$EXPORT_TAGS{'complexities'}},
+                           @{$EXPORT_TAGS{'classes'}}
+                          ];
 
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+$EXPORT_TAGS{all} = [
+                     @{$EXPORT_TAGS{'complexities'}},
+                     @{$EXPORT_TAGS{'classes'}}
+                    ];
 
-our @EXPORT = qw(
-	
-);
+our @EXPORT_OK = (@{$EXPORT_TAGS{'all'}});
+
+our @EXPORT = (@{$EXPORT_TAGS{'constants'}});
+
 our $VERSION = '0.01';
 
-our $mxREAL;
-our $mxCOMPLEX;
+our ($mxREAL,$mxCOMPLEX);
+our ($mxUNKNOWN_CLASS,$mxCELL_CLASS,$mxSTRUCT_CLASS,$mxOBJECT_CLASS,
+     $mxCHAR_CLASS,$mxLOGICAL_CLASS,$mxDOUBLE_CLASS,$mxSINGLE_CLASS,
+     $mxINT8_CLASS,$mxUINT8_CLASS,$mxINT16_CLASS,$mxUINT16_CLASS,
+     $mxINT32_CLASS,$mxUINT32_CLASS,$mxINT64_CLASS,$mxUINT64_CLASS,
+     $mxFUNCTION_CLASS);
 
 bootstrap OME::Matlab $VERSION;
 
 $mxREAL = __mxREAL();
 $mxCOMPLEX = __mxCOMPLEX();
 
+$mxUNKNOWN_CLASS = __mxUNKNOWN_CLASS();
+$mxCELL_CLASS = __mxCELL_CLASS();
+$mxSTRUCT_CLASS = __mxSTRUCT_CLASS();
+$mxOBJECT_CLASS = __mxOBJECT_CLASS();
+$mxCHAR_CLASS = __mxCHAR_CLASS();
+$mxLOGICAL_CLASS = __mxLOGICAL_CLASS();
+$mxDOUBLE_CLASS = __mxDOUBLE_CLASS();
+$mxSINGLE_CLASS = __mxSINGLE_CLASS();
+$mxINT8_CLASS = __mxINT8_CLASS();
+$mxUINT8_CLASS = __mxUINT8_CLASS();
+$mxINT16_CLASS = __mxINT16_CLASS();
+$mxUINT16_CLASS = __mxUINT16_CLASS();
+$mxINT32_CLASS = __mxINT32_CLASS();
+$mxUINT32_CLASS = __mxUINT32_CLASS();
+$mxINT64_CLASS = __mxINT64_CLASS();
+$mxUINT64_CLASS = __mxUINT64_CLASS();
+$mxFUNCTION_CLASS = __mxFUNCTION_CLASS();
+
 # Preloaded methods go here.
 
 1;
+
 __END__
-# Below is stub documentation for your module. You better edit it!
-
-=head1 NAME
-
-Matlab - Perl extension for blah blah blah
-
-=head1 SYNOPSIS
-
-  use Matlab;
-  blah blah blah
-
-=head1 DESCRIPTION
-
-Stub documentation for Matlab, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
-=head1 AUTHOR
-
-A. U. Thor, a.u.thor@a.galaxy.far.far.away
-
-=head1 SEE ALSO
-
-perl(1).
-
-=cut
