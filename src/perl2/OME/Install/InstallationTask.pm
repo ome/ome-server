366664b00426c3c1614a2a5322fbed2ae192614f
Andrea Falconi <a.falconi@dundee.ac.uk>
2003-08-01 15:09:08 +0000

    Initial commit.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1547 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Install/InstallationTask.pm
@@ -0,0 +1,41 @@
+# OME/Intall/IntallationTask.pm
+
+# Copyright (C) 2003 Open Microscopy Environment
+# Author:
+#
+#	 This library is free software; you can redistribute it and/or
+#	 modify it under the terms of the GNU Lesser General Public
+#	 License as published by the Free Software Foundation; either
+#	 version 2.1 of the License, or (at your option) any later version.
+#
+#	 This library is distributed in the hope that it will be useful,
+#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	 Lesser General Public License for more details.
+#
+#	 You should have received a copy of the GNU Lesser General Public
+#	 License along with this library; if not, write to the Free Software
+#	 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+package OME::Install::InstallationTask;
+
+use strict;
+use vars qw($VERSION);
+$VERSION = 2.000_000;
+
+
+# Interface common to all installation tasks.
+
+# Every installation task will have to implement this method to carry out its
+# job.
+sub execute {
+    return undef;
+}
+
+# Every installation task will have to implement this to undo the work that has
+# been done at time this method is invoked.
+sub rollback {
+    return undef;
+}
+
