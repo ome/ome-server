f28b04285702cda6860629818ebfa0170b9c1b80
Doug Creager <dcreager@alum.mit.edu>
2003-08-07 17:01:31 +0000

    Not really an installation task yet, but this seemed like a good place to put the list of DBObject classes for the super-cool automated database generator
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1583 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Install/CoreDatabaseTablesTask.pm
@@ -0,0 +1,59 @@
+# OME/Install/CoreDatabaseTablesTask.pm
+
+# Copyright (C) 2003 Open Microscopy Environment
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package OME::Install::CoreDatabaseTablesTask;
+
+# Hey there Chris, I just created this class to have a place to put
+# the list of DBObjects that can be used to instantiate the database.
+# --doug
+
+# $coreClasses = ([$package_to_require,$class_to_instantiate], ... )
+
+# Each class to instantiate is listed as a pair: the package that
+# should be used in the "require" statement, and the class to actually
+# instantiate in the database.  If the first argument is undef, no
+# require statement is executed.  This is just to deal with the fact
+# that some files declare multiple DBObject subclasses; only the
+# package corresponding to the filename should be "required".
+
+my @coreClasses =
+  (
+   ['OME::LookupTable',       'OME::LookupTable'],
+   [undef,                    'OME::LookupTable::Entry'],
+   ['OME::DataTable',         'OME::DataTable'],
+   [undef,                    'OME::DataTable::Column'],
+   ['OME::SemanticType',      'OME::SemanticType'],
+   [undef,                    'OME::SemanticType::Element'],
+   [undef,                    'OME::SemanticType::BootstrapExperimenter'],
+   [undef,                    'OME::SemanticType::BootstrapGroup'],
+   [undef,                    'OME::SemanticType::BootstrapRepository'],
+   ['OME::Dataset',           'OME::Dataset'],
+   ['OME::Project',           'OME::Project'],
+   ['OME::Project',           'OME::Project::DatasetMap'],
+   ['OME::Image',             'OME::Image'],
+   ['OME::Image',             'OME::Image::DatasetMap'],
+   ['OME::Image',             'OME::Image::ImageFilesXYZWT'],
+   ['OME::Session',           'OME::Session'],
+   ['OME::ViewerPreferences', 'OME::ViewerPreferences'],
+   ['OME::Module::Category',  'OME::Module::Category'],
+   # Make sure this next one is last
+   ['OME::Configuration',     'OME::Configuration'],
+  );
+
+
+1;
