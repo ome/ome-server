2399d04888433c3f5a9a688d7d8e3c4e8fc38a70
Doug Creager <dcreager@alum.mit.edu>
2003-08-14 17:34:26 +0000

    Creates some samples attributes to play with
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1658 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/matlab/setup_testdata.m
@@ -0,0 +1,24 @@
+global ome_Pixels
+
+repository.id = 3;
+repository.Path = '/OME/repository/';
+
+ome_Pixels(1).id = 4;
+ome_Pixels(1).SizeX = 640;
+ome_Pixels(1).SizeY = 512;
+ome_Pixels(1).SizeZ = 1;
+ome_Pixels(1).SizeC = 1;
+ome_Pixels(1).SizeT = 1;
+ome_Pixels(1).BitsPerPixel = 8;
+ome_Pixels(1).Repository = repository;
+ome_Pixels(1).Path = '4-n2_plate18_P24_w1.ori';
+
+ome_Pixels(2).id = 9;
+ome_Pixels(2).SizeX = 1024;
+ome_Pixels(2).SizeY = 1024;
+ome_Pixels(2).SizeZ = 24;
+ome_Pixels(2).SizeC = 2;
+ome_Pixels(2).SizeT = 1;
+ome_Pixels(2).BitsPerPixel = 16;
+ome_Pixels(2).Repository = repository;
+ome_Pixels(2).Path = '9-210502-0min-NFKB-4.ori';
