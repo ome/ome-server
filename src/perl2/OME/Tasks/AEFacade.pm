272d333e0b5786ee2152be8b6bb16105686edb0b
Jean-Marie Burel <j.burel@dundee.ac.uk>
2003-08-21 12:47:05 +0000

    checked if an analysis view has been run with the same Inputs
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1733 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tasks/AEFacade.pm
+++ src/perl2/OME/Tasks/AEFacade.pm
@@ -1,13 +1,7 @@
 # OME/Tasks/AEFacade.pm
 
-#-------------------------------------------------------------------------------
-#
 # Copyright (C) 2003 Open Microscopy Environment
-#       Massachusetts Institute of Technology,
-#       National Institutes of Health,
-#       University of Dundee
-#
-#
+# Author:  JM Burel <jburel@dundee.ac.uk>
 #
 #    This library is free software; you can redistribute it and/or
 #    modify it under the terms of the GNU Lesser General Public
@@ -22,24 +16,12 @@
 #    You should have received a copy of the GNU Lesser General Public
 #    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-#-------------------------------------------------------------------------------
-
-
-
-
-#-------------------------------------------------------------------------------
-#
-# Written by:    JM Burel <jburel@dundee.ac.uk>
-#
-#-------------------------------------------------------------------------------
 
 
 package OME::Tasks::AEFacade;
 
 
-use OME;
-our $VERSION = $OME::VERSION;
+our $VERSION = 2.000_000;
 
 =pod
 
@@ -97,20 +79,65 @@ sub executeView{
 	my ($dataset,$nameView,$string,$nameFunction,$refh,$attributeType)=@_;
 	my %user_inputs=();
 	my @attributes=();
+	my $message;
 	my $engine = OME::Analysis::AnalysisEngine->new();
 	my $cmanager = OME::Tasks::ChainManager->new($session);
-	my $view=$self->{session}->Factory()->findObject("OME::AnalysisChain",
+	my $factory = $self->{session}->Factory();
+	my $view=$factory->findObject("OME::AnalysisChain",
 							name=>$nameView);
-	print STDERR "chain===".$view->analysis_chain_id()."\n";
-	my $node=$cmanager->getNode($view,$nameFunction);
-print STDERR "nodeID===".$node->analysis_chain()->id()."\n";
 
+	my $module=$cmanager->findModule($nameFunction);
+	my @moduleExecution=$factory->findObjects("OME::ModuleExecution", 
+								'module_id' => $module->id(),
+								'dataset_id' => $dataset->id()
+								 );
+
+	
+
+	my $node=$cmanager->getNode($view,$nameFunction);		# table analysis_chains
+	# inputs parameters passed to chain. formal_inputs table.
 	my $formal_input=$cmanager->getFormalInput($view,$node,$string);
-
+  	# Check userInputs	
+	my @att=$factory->findAttributes($attributeType);		
+	
+	
+	#######################################################
+	if (scalar (@moduleExecution) >0){
+		foreach my $a (@att){
+			my $module_execution=$a->module_execution();	
+			if (defined $a->module_execution()){
+				my $me_id=$a->module_execution()->id();
+				foreach my $me (@moduleExecution){
+					my @list_input=();
+					my @num=();
+					my $result=$factory->findObject("OME::ModuleExecution::ActualInput", 
+								'module_execution_id' => $me->id(),
+								'input_module_execution_id' => $me_id,
+								'formal_input_id' => $formal_input->id()
+								 );
+					if (defined $result){
+					    my $h=$a->getDataHash();
+					    foreach my $k (keys %$h){
+					      push (@num,$k);
+					      if ("${$h}{$k}" eq "${$refh}{$k}"){
+						  push (@list_input,$k); 
+						}
+				          }
+					    if (scalar (@num) == scalar (@list_input)){
+		 			      $message="This analysis has already been run against this dataset with the same input";
+		  			      return (undef,$message);
+					    }
+					}# defined
+								
+				} #foreach $me
+			}
+		}
+	}
 	my $attribute = $session->Factory()->newAttribute($attributeType,undef,undef,$refh);
       push (@attributes,$attribute);
 
 	$user_inputs{$node->id()}->{$formal_input->id()} =\@attributes;
+
 	$engine->executeAnalysisView($session,$view,\%user_inputs,$dataset);
 	return ;
 
