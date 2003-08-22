39aea6228033154641b091449120cbfa6b1fdfa1
Jean-Marie Burel <j.burel@dundee.ac.uk>
2003-08-22 15:49:31 +0000

    replaced module name "Stack Statistics" by "Fast Stack Statistics"
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1748 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tasks/Thumbnails.pm
+++ src/perl2/OME/Tasks/Thumbnails.pm
@@ -401,16 +401,16 @@ sub getImageStats{
 	my ($factory,$image)=@_;
   	# new version
   	my $pixels = $image->DefaultPixels();
-  	my $stackStats = $factory->findObject( "OME::Module", name => 'Stack statistics' )
-		or die "Stack statistics must be installed for this viewer to work!\n";
+  	my $stackStats = $factory->findObject( "OME::Module", name => 'Fast Stack statistics' )
+		or die "Fast Stack statistics must be installed for this viewer to work!\n";
 	my $pixelsFI = $factory->findObject( "OME::Module::FormalInput",
 		module_id => $stackStats->id(),
 		name       => 'Pixels' )
-		or die "Cannot find 'Pixels' formal input for Program 'Stack Statistics'.\n";
+		or die "Cannot find 'Pixels' formal input for Program 'Fast Stack Statistics'.\n";
 	my $actualInput = $factory->findObject( "OME::ModuleExecution::ActualInput",
 		formal_input_id   => $pixelsFI->id(),
 		input_module_execution_id => $pixels->module_execution()->id() )
-		or die "Stack Statistics has not been run on the Pixels to be displayed.\n";
+		or die "Fast Stack Statistics has not been run on the Pixels to be displayed.\n";
 	my $stackStatsAnalysisID = $actualInput->module_execution()->id();
 
 	my @mins   = grep( $_->module_execution()->id() eq $stackStatsAnalysisID,
