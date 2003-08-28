adeb4275f94cf5aaffccd50d811e6bf4532d922a
Chris Allan <callan@lifesci.dundee.ac.uk>
2003-08-28 10:41:40 +0000

    Merging in of the bootstrap stuff that Doug had previously written.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1783 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Install/CoreDatabaseTablesTask.pm
+++ src/perl2/OME/Install/CoreDatabaseTablesTask.pm
@@ -1,4 +1,5 @@
 # OME/Install/CoreDatabaseTablesTask.pm
+# Builds the core database tables of OME
 
 #-------------------------------------------------------------------------------
 #
@@ -25,20 +26,46 @@
 #
 #-------------------------------------------------------------------------------
 
-
-
-
-#-------------------------------------------------------------------------------
-#
-# Written by:  
-#-------------------------------------------------------------------------------
-
-
 package OME::Install::CoreDatabaseTablesTask;
 
-# Hey there Chris, I just created this class to have a place to put
-# the list of DBObjects that can be used to instantiate the database.
-# --doug
+#*********
+#********* INCLUDES
+#*********
+
+use strict;
+use warnings;
+use Carp;
+use English;
+use Cwd;
+use Term::ANSIColor qw(:constants);
+use File::Basename;
+
+use OME::Install::Util;
+use OME::Install::Environment;
+use OME::Install::Terminal;
+use base qw(OME::Install::InstallationTask);
+
+#*********
+#********* GLOBALS AND DEFINES
+#*********
+
+# Global logfile filehandle
+my $LOGFILE;
+
+# Installation home
+my $INSTALL_HOME;
+
+# Our basedirs and user which we grab from the environment
+my ($OME_BASE_DIR, $OME_TMP_DIR, $OME_USER, $OME_UID);
+
+# Installation Packages
+use OME::Install::Util;
+use OME::Install::Environment;
+use OME::Install::Terminal;
+use base qw(OME::Install::InstallationTask);
+
+# Packages needed from OME for the bootstrap
+use OME::Database::Delegate;
 
 # $coreClasses = ([$package_to_require,$class_to_instantiate], ... )
 
@@ -88,6 +115,158 @@ our @coreClasses =
    # Make sure this next one is last
    ['OME::Configuration',     'OME::Configuration'],
   );
+
+#*********
+#********* LOCAL SUBROUTINES
+#*********
+
+# Create a postgres superuser SQL: CREATE USER foo CREATEUSER
+sub create_superuser {
+    my ($username, $logfile) = @_;
+    my $pg_uid = getpwnam ("postgres") or croak "Unable to retrieve PostgreSQL user UID";
+    my $output;
+
+    # Make sure we're not croaking on a silly logfile print
+    $logfile = *STDERR unless ref ($logfile) eq 'GLOB';
+
+    # Drop our UID to Postgres
+    $EUID = $pg_uid;
+
+    # Make sure we can see the Postgres command
+    which ("createuser") or croak "Couldn't execute createuser (a postgres utility)! Is createuser in your \$PATH?\n";
+    # Create the user using the command line tools
+    $output = `createuser -d -a $username 2>&1`;
+
+    # Back to UID 0
+    $EUID = 0;
+
+    # Log and return success
+    if (($output =~ /already exists/) or ($output =~ /^CREATE/)) {
+	print $logfile "CREATION OF USER $username SUCCESSFUL -- OUTPUT: \"$output\"\n";
+	return 1;
+    }
+
+    # Log and return failure
+    print $logfile "CREATION OF USER $username FAILED -- OUTPUT: \"$output\"\n";
+    return 0;
+}
+
+sub create_database {
+    my $platform = shift;
+    my $dbh;
+    my $sql;
+
+    $dbh = DBI->connect("dbi:Pg:dbname=template1")
+      or croak "Error: $dbh->errstr()";
+
+    $sql = <<SQL;
+SELECT oid FROM pg_database WHERE lower(datname) = lower(?)
+SQL
+
+    my $find_database = $dbh->prepare($sql);
+    my ($db_oid) = $dbh->selectrow_array($find_database,{},'ome');
+
+    # This will be NULL if the database does not exist
+    if (defined $db_oid) {
+        $dbh->disconnect();
+        return 1;
+    }
+
+    $dbh->do('CREATE DATABASE ome')
+      or die $dbh->errstr();
+    $dbh->disconnect();
+
+    # Debian does this by default using Debconf, everything else needs it.
+    # Atleast as far as I know. (Chris)
+    if ($platform ne "DEBIAN") {
+        print "Adding PL-PGSQL language...\n";
+        my $CMD_OUT = `createlang plpgsql ome 2>&1`;
+        die $CMD_OUT if $? != 0;
+	print BOLD "[Done.]", RESET, "\n";
+    }
+
+    print "Fixing OID/INTEGER compatability bug...\n";
+    $dbh = DBI->connect("dbi:Pg:dbname=ome")
+      or die $dbh->errstr();
+    $sql = <<SQL;
+CREATE FUNCTION OID(INT8) RETURNS OID AS '
+declare
+  i8 alias for \$1;
+begin
+  return int4(i8);
+end;'
+LANGUAGE 'plpgsql';
+SQL
+    $dbh->do($sql) or die ($dbh->errstr);
+    $dbh->disconnect();
+    print BOLD "[Done.]", RESET, "\n";
+
+    return 0;
+}
+
+
+#*********
+#********* START OF CODE
+#*********
+
+sub execute {
+    my $retval;
+
+    print_header "Database Bootstrap";
+
+    # Our OME::Install::Environment
+    my $environment = initialize OME::Install::Environment;
+
+    # Populate globals
+    $OME_BASE_DIR = $environment->base_dir()
+	or croak "Unable to retrieve OME_BASE_DIR!";
+    $OME_TMP_DIR = $environment->tmp_dir()
+	or croak "Unable to retrieve OME_TMP_DIR!";
+    $OME_USER = $environment->user()
+	or croak "Unable to retrieve OME_USER!";
+    $OME_UID = getpwnam ($OME_USER)
+	or croak "Unable to retrive OME_USER UID!";
+
+    # Set our installation home
+    $INSTALL_HOME = $OME_TMP_DIR."/install";
+    
+    print "(All verbose information logged in $INSTALL_HOME/CoreDatabaseTablesTask.log)\n\n";
+
+    # Get our logfile and open it for reading
+    open ($LOGFILE, ">", "$INSTALL_HOME/CoreDatabaseTablesTask.log")
+	or croak "Unable to open logfile \"$INSTALL_HOME/CoreDatabaseTablesTask.log\", $!";
+
+    #*********
+    #********* Create our super-user and bootstrap the DB
+    #*********
+
+    # Make sure our OME_USER is also a Postgres user
+    print "Creating PostgreSQL SUPERUSER ";
+    $retval = create_superuser ($OME_USER, $LOGFILE);
+    
+    print BOLD, "[FAILURE]", RESET, ".\n"
+        and croak "Unable to create PostgreSQL superuser, see CoreDatabaseTablesTask.log for details."
+        unless $retval;
+    print BOLD, "[SUCCESS]", RESET, ".\n";
+
+    # Drop our UID to the OME_USER
+    $EUID = $OME_UID;
+
+    # Create our database
+    my $exists = create_database ("DEBIAN");
+    
+    # Back to UID 0
+    $EUID = 0;
+
+    croak "Database already exists!" if $exists;
+
+    return 1;
+}
+
+sub rollback {
+
+    return 1;
+}
 
 
 1;
