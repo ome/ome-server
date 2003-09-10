11691a17b575a651e2d8ef08caff14392f6a8753
Harry Hochheiser <hsh@nih.gov>
2003-09-10 19:38:27 +0000

    revised to be a superclass for PParamLink and PModuleLink. revised selection to use link targets at ends of links.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1924 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/piccolo/PLink.java
+++ src/Attic/java/src/org/openmicroscopy/vis/piccolo/PLink.java
@@ -1,5 +1,5 @@
 /*
- * org.openmicroscopy.vis.piccolo.Link
+ * org.openmicroscopy.vis.piccolo.PLink
  *
  *------------------------------------------------------------------------------
  *
@@ -41,12 +41,10 @@
 package org.openmicroscopy.vis.piccolo;
 
 import edu.umd.cs.piccolo.nodes.PPath;
-import edu.umd.cs.piccolo.PNode;
 import java.lang.Math;
 import java.awt.BasicStroke;
 import java.awt.geom.Point2D;
 import java.awt.Color;
-//import java.awt.geom.CubicCurve2D;
 
 
 
@@ -60,15 +58,15 @@ import java.awt.Color;
  * @since OME2.0
  */
 
-public class PLink extends  PPath implements PNodeEventListener {
+public abstract class PLink extends  PPath implements PNodeEventListener {
 	
 	public static final BasicStroke LINK_STROKE=
 		new BasicStroke(2,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND);
 
 	Point2D point = new Point2D.Float();
 	
-	private static final int HIGHLIGHT_SIZE=8;
-	private static final int HIGHLIGHT_RADIUS=HIGHLIGHT_SIZE/2;
+	protected static final int HIGHLIGHT_SIZE=8;
+	protected static final int HIGHLIGHT_RADIUS=HIGHLIGHT_SIZE/2;
 	
 	public static final Color DEFAULT_COLOR=Color.black;
 	public static final Color HIGHLIGHT_COLOR=Color.WHITE;
@@ -77,24 +75,20 @@ public class PLink extends  PPath implements PNodeEventListener {
 	public static final float END_BULB_RADIUS = END_BULB/2;
 	
 	
-	// note that "start" and "end" refer to where the _drawing_
-	// started and ended, not to the direction of the link. 
-	// thus, start might be an input and end an output (even 
-	// though links go output-input) or vice-versa.
-	private PFormalParameter start = null;
-	private PFormalParameter end = null;
 		
 	// xs and ys for line
-	float xstart,ystart;
-	float xend,yend;
+	protected float xstart,ystart;
+	protected float xend,yend;
 	
 
-	private PPath arrow;
+	protected PPath arrow;
 	
 	// nodes used for selection indicators
-	private PPath select1;
-	private PPath select2;
+	protected PPath select1;
+	protected PPath select2;
 
+    // the link layer that we're part of.
+	protected PLinkLayer  linkLayer = null;
 		
 	public PLink() {		
 		super();
@@ -102,22 +96,9 @@ public class PLink extends  PPath implements PNodeEventListener {
 		setPaint(DEFAULT_COLOR);
 		buildArrow();
 	}
-	public PLink(PFormalInput in,PFormalOutput out) {
-		this.start = in;
-		this.end = out;
-		buildArrow();
-		start.addNodeEventListener(this);
-		end.addNodeEventListener(this);
-		start.setLinkedTo(end,this);
-		end.setLinkedTo(start,this);
-		setStartPoint();
-		setEndPoint();
-		setStroke(LINK_STROKE);
-		setPaint(DEFAULT_COLOR);
 		
-	}
 	
-	private void buildArrow() {
+	protected void buildArrow() {
 		arrow = new PPath();
 		arrow.moveTo(0,0);
 		float left = -12;
@@ -131,72 +112,21 @@ public class PLink extends  PPath implements PNodeEventListener {
 		addChild(arrow);
 	}
 	
-	public void setStartParam(PFormalParameter start) {
-	
-		this.start = start;
-		start.addNodeEventListener(this);
-		setStartPoint();
-	}
-	
-	public void setEndParam(PFormalParameter end) {
-		this.end = end;
-		if (start != null) {
-			end.setLinkedTo(start,this);  
-			start.setLinkedTo(end,this);
-		}
-		end.addNodeEventListener(this);
-		setEndPoint();
-	}
-
-	private void setStartPoint() {
-		getEndPointCoords(start);
-		setStartCoords((float)point.getX(),(float) point.getY());
-	}
-	
-	private void setEndPoint() {
-		getEndPointCoords(end);
-		setEndCoords((float) point.getX(),(float) point.getY());
-	}
-		
-	private void getEndPointCoords(PFormalParameter p) {
-		Point2D paramCirclePoint = p.getCircleCenter();
-		point.setLocation(paramCirclePoint);
-		//p.getLocator().locatePoint(point);
-		//p.localToGlobal(point);
-	}
 	
 	public void setStartCoords(float x,float y) {
 		xstart = x;
 		ystart = y;
 	}
 
-	
 	public void setEndCoords(float x,float y) {
 		xend = x;
 		yend = y;
 		setLine();
 	}
 	
-	private void setLine() {
-		double theta;
-		reset();
-		moveTo(xstart,ystart);
-		lineTo(xend,yend);
-//		CubicCurve2D.Float curve  = 
-//			new CubicCurve2D.Float(xstart,ystart,xstart+20,ystart-20,xend-20,yend+20,xend,yend);
-//		setPathTo(curve);
-			
-		if ( start instanceof PFormalInput) {
-			theta = getAngle(xend,yend,xstart,ystart);
-			drawLinkEnd(xstart,ystart,theta);
-		}
-		else {
-			theta = getAngle(xstart,ystart,xend,yend);
-			drawLinkEnd(xend,yend,theta);
-		}
-	}
+	protected abstract void setLine();
 
-	private double getAngle(float xs,float ys,float xe,float ye) {
+	protected double getAngle(float xs,float ys,float xe,float ye) {
 		double arctan = (double) (ye-ys)/(xe-xs);
 		double angle = Math.atan(arctan);
 		if (xe < xs)
@@ -204,34 +134,28 @@ public class PLink extends  PPath implements PNodeEventListener {
 		return angle;
 	}
 	
-	private void drawLinkEnd(float x,float y,double theta) {
-	
-		//arrow.setOffset(0,0);
+	protected void drawLinkEnd(float x,float y,double theta) {
 		arrow.setRotation(theta);
 		arrow.setOffset(x,y); 
 	}
 	
 	
-	public void nodeChanged(PNodeEvent e) {
-		PNode node =e.getNode();
-		if (!(node instanceof PFormalParameter))
-			return;
-		if (node == start) {
-			setStartPoint();
-			setLine();
-		}
-		else if (node == end) 
-			setEndPoint(); // setLine() call is implied.
-	}		
+	public abstract void nodeChanged(PNodeEvent e); 
 	
 	public void remove() {
 		removeFromParent();
-		start.clearLinkedTo(end);
-		end.clearLinkedTo(start);
 	}
 
+	public abstract PLinkTarget getStartLinkTarget();
+	
+	public abstract PLinkTarget getEndLinkTarget();
+
 	public void setSelected(boolean v) {
-		if (v == true) {
+		PLinkTarget startTarget = getStartLinkTarget();
+		PLinkTarget endTarget = getEndLinkTarget();
+		startTarget.setSelected(v);
+		endTarget.setSelected(v);
+		 /*if (v == true) {
 			PPath select1 =PPath.createEllipse(xstart-END_BULB_RADIUS,
 				ystart-HIGHLIGHT_RADIUS,HIGHLIGHT_SIZE,HIGHLIGHT_SIZE);
 			addChild(select1);
@@ -242,8 +166,6 @@ public class PLink extends  PPath implements PNodeEventListener {
 			select2.setStrokePaint(DEFAULT_COLOR);
 			select1.setPaint(HIGHLIGHT_COLOR);
 			select2.setPaint(HIGHLIGHT_COLOR);
-			//setStrokePaint(HIGHLIGHT_COLOR);
-			//setPaint(HIGHLIGHT_COLOR);
 			moveToFront();
 		}
 		else {
@@ -252,22 +174,11 @@ public class PLink extends  PPath implements PNodeEventListener {
 			if (select2 != null)
 				removeChild(select2);
 			select1=select2=null;
-		}
+		} */
 		repaint();
 	}
 	
-	public PFormalInput getInput() {
-		if (start instanceof PFormalInput)
-			return (PFormalInput) start;
-		else
-			return (PFormalInput) end;
+	public void setLinkLayer(PLinkLayer linkLayer) {
+		this.linkLayer = linkLayer;
 	}
-	
-	public PFormalOutput getOutput() {
-		if (start instanceof PFormalOutput)
-				return (PFormalOutput) start;
-			else
-				return (PFormalOutput) end;
-	}
-	
 }
