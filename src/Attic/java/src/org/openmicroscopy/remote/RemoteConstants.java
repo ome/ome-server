927481a255d9b56fd43752e4e745eb05769be43f
Doug Creager <dcreager@alum.mit.edu>
2003-09-11 20:23:12 +0000

    Moved common constants to a single class
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1946 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/Attic/java/src/org/openmicroscopy/remote/RemoteConstants.java
@@ -0,0 +1,61 @@
+/*
+ * org.openmicroscopy.remote.RemoteConstants
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Douglas Creager <dcreager@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+package org.openmicroscopy.remote;
+
+/**
+ * Contains some useful constants for the OME Remote Client interface.
+ * @author Douglas Creager
+ * @version 2.1
+ * @since OME2.1
+ */
+
+public interface RemoteConstants
+{
+    /**
+     * This is the Remote object reference which represents a null
+     * value.  (We need this since at least one RPC protocol does not
+     * support null values directly.)
+     */
+    public static final String  NULL_REFERENCE = ">>OBJ:NULL";
+
+}
