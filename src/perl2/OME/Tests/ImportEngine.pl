6f46069c7c692730ce905267d72398de51669003
Ilya Goldberg <igg@nih.gov>
2003-09-12 21:55:02 +0000

    Ooops - reverted to version without benchmark timer
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@1963 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tests/ImportEngine.pl
+++ src/perl2/OME/Tests/ImportEngine.pl
@@ -4,13 +4,11 @@ use OME::SessionManager;
 use OME::Session;
 use OME::Factory;
 use OME::ImportEngine::ImportEngine;
-#OME::DBObject->Caching(1);
 
 my $manager = OME::SessionManager->new();
 my $session = $manager->TTYlogin();
 my $factory = $session->Factory();
 
-$session->BenchmarkTimer->start ('Total');
 if( scalar @ARGV eq 0 ) {
 	print STDERR "Incorrect usage. Usage is\n\t ImporEngine.pl file1 file2 file3 ...\n";
 	exit -1;
@@ -20,7 +18,3 @@ print "Importing @ARGV\n";
 OME::ImportEngine::ImportEngine->
   importFiles(session => $session,
               \@ARGV);
-$session->BenchmarkTimer->stop ('Total');
-
-$session->BenchmarkTimer->report(); 
-
