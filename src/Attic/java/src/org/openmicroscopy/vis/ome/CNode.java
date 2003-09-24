5c9efe40bb6942180610751a55795440398f1573
Harry Hochheiser <hsh@nih.gov>
2003-09-24 22:34:29 +0000

    subclass of RemoteChain.Node that maintains state for drawing
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2083 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/Attic/java/src/org/openmicroscopy/vis/ome/CNode.java
@@ -0,0 +1,69 @@
+/*
+ * org.openmicroscopy.vis.chains.ome.CNode
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Harry Hochheiser <hsh@nih.gov>
+ *
+ *------------------------------------------------------------------------------
+ */
+ 
+ package org.openmicroscopy.vis.ome;
+ 
+ import org.openmicroscopy.remote.RemoteChain;
+ import org.openmicroscopy.remote.RemoteObjectCache;
+ 
+ public class CNode extends RemoteChain.Node {
+	
+	static {
+		RemoteObjectCache.addClass("OME::AnalysisChain::Node",CNode.class);
+	}
+	private int layer = -1;
+	
+ 	public CNode() {
+ 		super();
+ 		System.err.println("creating a cnode");
+ 	}
+ 	
+	public void setLayer(int layer) {
+		this.layer = layer;
+	}
+	
+	public int getLayer() {
+		return layer;
+	}
+	
+	public boolean hasLayer() {
+		return (!(layer == -1));
+	} 
+ }
+ 
