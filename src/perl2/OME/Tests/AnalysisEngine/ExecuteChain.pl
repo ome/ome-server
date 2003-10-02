cdf22d5702ad23e81a1a8695e1236c74e4363a54
Josiah Johnston <siah@nih.gov>
2003-10-02 15:06:02 +0000

    fixed usage message: s/view/analysis chain/g
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2166 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tests/AnalysisEngine/ExecuteChain.pl
+++ src/perl2/OME/Tests/AnalysisEngine/ExecuteChain.pl
@@ -50,12 +50,12 @@ undef &Class::DBI::min;
 undef &Class::DBI::max;
 use strict 'refs';
 
-print "\nOME Test Case - Execute view\n";
+print "\nOME Test Case - Execute Analysis Chain\n";
 print "----------------------------\n";
 
 if (scalar(@ARGV) < 2) {
-    print "Usage:  ExecuteView <view id> <dataset id> <flags>\n";
-    print "known flags are: ReuseResults, DebugDefault, DebugTiming. Flag usage is [flag]=[0 or 1] (i.e. ReuseResults=0)\n\n";
+    print "Usage:  ExecuteView <analysis chain id> <dataset id> <flags>\n";
+    print "known flags are: ReuseResults, DebugDefault, DebugTiming, Cached. Flag usage is [flag]=[0 or 1] (i.e. ReuseResults=0)\n\n";
 # flags are listed in AnalysisEngine, so I say known flags cuz the flags there might change independently of this usage note.
 
     exit -1;
