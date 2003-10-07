42ba2232dae096092c190040d25f008e25a0dcc5
Harry Hochheiser <hsh@nih.gov>
2003-10-07 20:32:42 +0000

    subclass of link to handled links with dummy nodes
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2206 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/Attic/java/src/org/openmicroscopy/vis/ome/CLayoutLink.java
@@ -0,0 +1,83 @@
+/*
+ * org.openmicroscopy.vis.chains.ome.CLayoutLink
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Harry Hochheiser <hsh@nih.gov>
+ *
+ *------------------------------------------------------------------------------
+ */
+ 
+ package org.openmicroscopy.vis.ome;
+ 
+import org.openmicroscopy.remote.RemoteChain.Link;
+import org.openmicroscopy.Chain.Node;
+ /*
+	* This is mostly a placeholder that will allow me to distinguish between
+	* real nodes in a graph (CNode objects) and dummy nodes that will
+	* be created to aid in drawing. 
+	*/
+
+ public class CLayoutLink extends Link {
+	
+	private CNode toNode;
+	private CNode fromNode;
+	
+	public CLayoutLink() {
+		super();
+	}
+	
+	public CLayoutLink(CNode fromNode,CNode toNode) {
+		this.fromNode = fromNode;
+		this.toNode = toNode;
+	}
+ 	
+	public Node getToNode() {
+		return toNode;
+	}
+	
+	public Node getFromNode() {
+		return fromNode;
+	}
+	
+	// we want this to throw an exception if we're ever setting it
+	// to anything that can't be cast to be a CNode.
+	public void setToNode(Node node) {
+		toNode = (CNode) node;
+	}
+	
+	public void setFromNode(Node node) {
+		fromNode = (CNode) node;
+	}
+}
+
+ 
+ 
