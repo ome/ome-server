91732c852af4cf28f3a61cb848e436937ecbac32
Josiah Johnston <siah@nih.gov>
2003-10-15 21:47:31 +0000

    move FormalInput and FormalOutput into separate files
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2263 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Module.pm
+++ src/perl2/OME/Module.pm
@@ -44,6 +44,9 @@ our $VERSION = $OME::VERSION;
 use OME::DBObject;
 use base qw(OME::DBObject);
 
+use OME::Module::FormalOutput;
+use OME::Module::FormalInput;
+
 __PACKAGE__->newClass();
 __PACKAGE__->setDefaultTable('modules');
 __PACKAGE__->setSequence('module_seq');
@@ -93,116 +96,6 @@ __PACKAGE__->hasMany('analyses','OME::ModuleExecution' => 'module');
 
 
 
-package OME::Module::FormalInput;
-
-use strict;
-use OME;
-our $VERSION = $OME::VERSION;
-
-use OME::DBObject;
-use base qw(OME::DBObject);
-
-__PACKAGE__->newClass();
-__PACKAGE__->setDefaultTable('formal_inputs');
-__PACKAGE__->setSequence('formal_input_seq');
-__PACKAGE__->addPrimaryKey('formal_input_id');
-__PACKAGE__->addColumn(module_id => 'module_id');
-__PACKAGE__->addColumn(module => 'module_id','OME::Module',
-                       {
-                        SQLType => 'integer',
-                        NotNull => 1,
-                        Indexed => 1,
-                        ForeignKey => 'modules',
-                       });
-__PACKAGE__->addColumn(name => 'name',
-                       {
-                        SQLType => 'varchar(64)',
-                        NotNull => 1,
-                        Indexed => 1,
-                       });
-__PACKAGE__->addColumn(description => 'description',{SQLType => 'text'});
-__PACKAGE__->addColumn(optional => 'optional',
-                       {
-                        SQLType => 'boolean',
-                        Default => 'false',
-                       });
-__PACKAGE__->addColumn(list => 'list',
-                       {
-                        SQLType => 'boolean',
-                        Default => 'true',
-                       });
-__PACKAGE__->addColumn(semantic_type_id => 'semantic_type_id');
-__PACKAGE__->addColumn(semantic_type => 'semantic_type_id',
-                       'OME::SemanticType',
-                       {
-                        SQLType => 'integer',
-                        NotNull => 1,
-                        Indexed => 1,
-                        ForeignKey => 'semantic_types',
-                       });
-__PACKAGE__->addColumn(lookup_table_id => 'lookup_table_id');
-__PACKAGE__->addColumn(lookup_table => 'lookup_table_id',
-                       'OME::LookupTable',
-                       {
-                        SQLType => 'integer',
-                        ForeignKey => 'lookup_tables',
-                       });
-__PACKAGE__->addColumn(user_defined => 'user_defined',
-                       {
-                        SQLType => 'boolean',
-                        Default => 'false',
-                       });
-
-__PACKAGE__->hasMany('actual_inputs','OME::ModuleExecution::ActualInput' =>
-                     'formal_input');
-
-package OME::Module::FormalOutput;
-
-use strict;
-use OME;
-our $VERSION = $OME::VERSION;
-
-use OME::DBObject;
-use base qw(OME::DBObject);
-
-__PACKAGE__->newClass();
-__PACKAGE__->setDefaultTable('formal_outputs');
-__PACKAGE__->setSequence('formal_output_seq');
-__PACKAGE__->addPrimaryKey('formal_output_id');
-__PACKAGE__->addColumn(module_id => 'module_id');
-__PACKAGE__->addColumn(module => 'module_id','OME::Module',
-                       {
-                        SQLType => 'integer',
-                        NotNull => 1,
-                        Indexed => 1,
-                        ForeignKey => 'modules',
-                       });
-__PACKAGE__->addColumn(name => 'name',
-                       {
-                        SQLType => 'varchar(64)',
-                        NotNull => 1,
-                        Indexed => 1,
-                       });
-__PACKAGE__->addColumn(description => 'description',{SQLType => 'text'});
-__PACKAGE__->addColumn(optional => 'optional',
-                       {
-                        SQLType => 'boolean',
-                        Default => 'false',
-                       });
-__PACKAGE__->addColumn(list => 'list',
-                       {
-                        SQLType => 'boolean',
-                        Default => 'true',
-                       });
-__PACKAGE__->addColumn(semantic_type_id => 'semantic_type_id');
-__PACKAGE__->addColumn(semantic_type => 'semantic_type_id',
-                       'OME::SemanticType',
-                       {
-                        SQLType => 'integer',
-                        Indexed => 1,
-                        ForeignKey => 'semantic_types',
-                       });
-__PACKAGE__->addColumn(feature_tag => 'feature_tag',{SQLType => 'varchar(128)'});
 
 1;
 
