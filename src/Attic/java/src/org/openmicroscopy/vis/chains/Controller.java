0112413cf47e8c3be722c28fe203043d9d8d5d96
Harry Hochheiser <hsh@nih.gov>
2003-10-20 20:33:30 +0000

    made library.dispose() call only work if library is not null
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2305 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/chains/Controller.java
+++ src/Attic/java/src/org/openmicroscopy/vis/chains/Controller.java
@@ -205,7 +205,8 @@ public class Controller implements ApplicationController {
 		mainFrame.setLoggedIn(false,connection);
 		removeCanvasFrames();
 		// remove library
-		library.dispose();
+		if (library !=null)
+			library.dispose();
 		chainCanvasCount = 0;
 	}
 	
