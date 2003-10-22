f3617e087b4c851b03410b8b4aea65ed18219d4f
Harry Hochheiser <hsh@nih.gov>
2003-10-22 17:21:18 +0000

    changed BORDER to 40
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2338 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/piccolo/PConstants.java
+++ src/Attic/java/src/org/openmicroscopy/vis/piccolo/PConstants.java
@@ -49,7 +49,7 @@ public class PConstants {
 
 	// for a buffered node, we want to get its bounds with some empty space
 	// on each side.  this value gives the amount of buffer space.
-	public static final int BORDER=20;	
+	public static final int BORDER=40;	
 	
 	// magnification at which point we should do our semantic zooming.
 	public static final double SCALE_THRESHOLD=0.5;
