e7b43850f7ac7e83c24b046b21bdd0fb74512184
Chris Allan <callan@lifesci.dundee.ac.uk>
2003-10-24 18:16:14 +0000

    - Added library searching and detecting support for: 	- libpng 	- libjpeg 	- libgd 	- libssl - Fixed up libtiff detection
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2365 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Install/LibraryTask.pm
+++ src/perl2/OME/Install/LibraryTask.pm
@@ -69,116 +69,199 @@ my ($OME_UID, $OME_GID);
 my $INSTALL_HOME;
 
 # The libraries we're managing
-my @libraries = (
-    {
-	name => 'zlib',
-	get_library_version => q{
-	    #include "zlib.h"
-	    int main () {
-		printf ("%s", ZLIB_VERSION);
+my @libraries = ( {
+		name => 'libssl',
+		get_library_version => q{
+			#include "openssl/opensslv.h"
+			int main () {
+				printf ("%s", SHLIB_VERSION_NUMBER);
+				return (0);
+			}
+		},
+		valid_versions => ['ge 0.9'],
+	}, {
+		name => 'libpng',
+		get_library_version => q(
+	    	#include "png.h"
+	    	int main () {
+			printf ("%d.%d.%d",
+		        	(PNG_LIBPNG_VER / 10000),
+					(PNG_LIBPNG_VER % 10000 / 100),
+					(PNG_LIBPNG_VER % 100));
 
-		return (0);
-	    }
-	},
-	valid_versions => ['ge "1.1.3"'],
-	repository_file => "$REPOSITORY/zlib-1.1.4.tar.gz",
-    },{
-	name => 'libxml2',
-	get_library_version => sub {
-	    my $xml2_config = "xml2-config";
+			return (0);
+	    	}
+		),
+		valid_versions => ['ge 1.0'],
+    }, {
+		name => 'libjpeg',
+		get_library_version => q{
+			#include "stdio.h"
+	    	#include "jpeglib.h"
+	    	int main () {
+				printf ("%d", JPEG_LIB_VERSION);
 
-	    $xml2_config = whereis ("xml2-config") unless which ("$xml2_config");
+				return (0);
+	    	}
+		},
+		valid_versions => ['ge 60'],
+    }, {
+		name => 'libtiff',
+		# XXX Unfortunately, this only works with new libtiff's... *sigh*
+		#get_library_version => q(
+		# 	#include "tiffvers.h"
+		#		/* Semi-ganked from GAIM (http://gaim.sourceforge.net/) cvs rc line parser
+		#		 * src/gaimrc.c -- parse_line()
+		#		 * Thanks to ChipX86 for the great idea, this version stub rocks.
+		#	 */
+		# 	int main () {
+		#		char * c;
+		#			c = (char *) calloc (strlen (TIFFLIB_VERSION_STR) + 1, sizeof(char));
+		#			strcpy (c, TIFFLIB_VERSION_STR);
+		#                                                                        
+		#			while (*c != '\n') {
+		#	   			if ((*c == '.') || ((*c >= 48) && (*c <= 57))) {
+		#					printf ("%c", *c);
+		#	   			}
+		#	   		c++;
+		#			}
+		#
+		#		return (0);
+		#		}
+		#	),
+		get_library_version => q(
+			#include "tiff.h"
+			/* For the moment, with backwards compatability in mind we can't do
+			 * anything better than check if the lib is installed and working.
+			 */
+			int main () {
+				printf ("%s", "N/A");
 
-	    my $version = `$xml2_config --version`;
-	    chomp $version;
-
-	    return $? == 0 ? $version : undef;
-	},
-	valid_versions => ['ge 2.5.7'],
-	repository_file => "$REPOSITORY/libxml2-2.5.7.tar.gz",
-    },{
-	name => 'libxslt',
-	get_library_version => sub {
-	    my $xslt_config = "xslt-config";
-
-	    $xslt_config = whereis ("xslt-config") unless which ("$xslt_config");
-
-	    my $version = `$xslt_config --version`;
-	    chomp $version;
-
-	    return $? == 0 ? $version : undef;
-	},
-	repository_file => "$REPOSITORY/libxslt-1.0.30.tar.gz",
-	valid_versions => ['ge "1.0.30"'],
-    },{
-	name => 'bzlib',
-	get_library_version =>
-	    # There's really no cross platform way to do this other than
-	    # grep'ing for comment data so we'll just assume we've got a
-	    # halfway decent version as long as we can compile a stub C
-	    # app with bzlib support. Not to mention it only includes the
-	    # "main" version not the full one, as far as I can tell there's
-	    # absolutely no way to decern version 1.0 from 1.0.2 in the source.
-	    q(
-	    #include "bzlib.h"
-	    int main () {
-		printf ("%s", "N/A");
-		return (0);
-	    }
-	),
-	configure_library => sub {
-	    my ($path, $logfile) = @_;
+				return (0);
+			}
+		),
+		configure_library => sub {
+			# Since libtiff has an interactive configure script we need to
+	    	# implement a custom configure_library () subroutine that allows
+	    	# for an interactive install
 	    
-	    $logfile = *STDERR unless ref ($logfile) eq 'GLOB';
-	    print $logfile "BZLIB DOESN'T NEED TO BE CONFIGURED -- SKIPPING CONFIGURATION\n";
+	    	my ($path, $logfile) = @_;
+	    	my $iwd = getcwd;  # Initial working directory
 
-	    return 1;
-	},
-	repository_file => "$REPOSITORY/bzip2-1.0.2.tar.gz",
-	#installModule => \&bzlibInstall,
-    },{
-	name => 'libtiff',
-	get_library_version =>
-	# Semi-ganked from GAIM (http://gaim.sourceforge.net/) cvs rc line parser
-	# src/gaimrc.c -- parse_line ()
-	# Thanks to ChipX86 for the great idea, this version stub rocks.
-	    q(
-	    #include "tiffvers.h"
-	    int main () {
-		char * c;
-		c = (char *) calloc (strlen (TIFFLIB_VERSION_STR) + 1, sizeof(char));
-		strcpy (c, TIFFLIB_VERSION_STR);
-                                                                                
-		while (*c != '\n') {
-		    if ((*c == '.') || ((*c >= 48) && (*c <= 57))) {
-			printf ("%c", *c);
-		    }
-		    c++;
-		}
+	    	$logfile = *STDERR unless ref ($logfile) eq 'GLOB';
 
-	    return (0);
-	    }
-	),
-	configure_library => sub {
-	    # Since libtiff has an interactive configure script we need to
-	    # implement a custom configure_library () subroutine that allows
-	    # for an interactive install
+	    	chdir ($path) or croak "Unable to chdir into \"$path\". $!";
+
+	    	system ("./configure 2>&1");
+
+	    	chdir ($iwd) or croak "Unable to chdir back into \"$iwd\", $!";
+
+	    	return 1;
+		},
+		valid_versions => ['ge "3.5.7"'],
+		repository_file => "$REPOSITORY/tiff-v3.5.7-OSX-LZW.tar.gz",
+    }, {
+		name => 'libgd',
+		pre_install => sub {
+			my $library = shift;
+			my $gdlib_config = 'gdlib-config';
+
+			my $version_sub = sub {
+	    		my $version = `gdlib-config --version`;
+	    		chomp $version;
+				return $? == 0 ? $version : undef;
+			};
+
+			$library->{get_library_version} = $version_sub if which($gdlib_config);
+		},
+		get_library_version => q(
+	    	#include "gd.h"
+			/* There's no crossplatform way to do this for version 1 so
+		 	 * we'll assume the lib is okay if we can link against it.
+		 	 * This will get overwritten with a sub if we've got version 2.
+		 	 */
+	    	int main () {
+			printf ("%s", "N/A");
+
+			return (0);
+			}
+	    ),
+	}, {
+		name => 'zlib',
+		get_library_version => q(
+	    	#include "zlib.h"
+	    	int main () {
+			printf ("%s", ZLIB_VERSION);
+
+			return (0);
+	    	}
+		),
+		valid_versions => ['ge "1.1.3"'],
+		repository_file => "$REPOSITORY/zlib-1.1.4.tar.gz",
+    }, {
+		name => 'libxml2',
+		get_library_version => sub {
+	    	my $xml2_config = "xml2-config";
+			my $version;
+
+	    	$xml2_config = whereis ("xml2-config") unless which ("$xml2_config");
+
+			if ($xml2_config) {
+	    		$version = `$xml2_config --version`;
+	    		chomp $version;
+			} else {
+				return undef;
+			}
+
+	    	return $? == 0 ? $version : undef;
+		},
+		valid_versions => ['ge 2.4.20'],
+		repository_file => "$REPOSITORY/libxml2-2.5.7.tar.gz",
+    }, {
+		name => 'libxslt',
+		get_library_version => sub {
+	    	my $xslt_config = "xslt-config";
+			my $version;
+
+	    	$xslt_config = whereis ("xslt-config") unless which ("$xslt_config");
+
+			if ($xslt_config) {
+	    		$version = `$xslt_config --version`;
+	    		chomp $version;
+			} else {
+				return undef;
+			}
+
+	    	return $? == 0 ? $version : undef;
+		},
+		repository_file => "$REPOSITORY/libxslt-1.0.30.tar.gz",
+		valid_versions => ['ge "1.0.30"'],
+    }, {
+		name => 'bzlib',
+		get_library_version => q(
+			#include "stdio.h"
+	    	#include "bzlib.h"
+			/* There's really no cross platform way to do this other than
+	    	 * grep'ing for comment data so we'll just assume we've got a
+	    	 * halfway decent version as long as we can compile a stub C
+	    	 * app with bzlib support. Not to mention it only includes the
+	    	 * "main" version not the full one, as far as I can tell there's
+	    	 * absolutely no way to decern version 1.0 from 1.0.2 in the source.
+			 */
+	    	int main () {
+				printf ("%s", "N/A");
+				return (0);
+	    	}
+		),
+		configure_library => sub {
+	    	my ($path, $logfile) = @_;
 	    
-	    my ($path, $logfile) = @_;
-	    my $iwd = getcwd;  # Initial working directory
+	    	$logfile = *STDERR unless ref ($logfile) eq 'GLOB';
+	    	print $logfile "BZLIB DOESN'T NEED TO BE CONFIGURED -- SKIPPING CONFIGURATION\n";
 
-	    $logfile = *STDERR unless ref ($logfile) eq 'GLOB';
-
-	    chdir ($path) or croak "Unable to chdir into \"$path\". $!";
-
-	    system ("./configure 2>&1");
-
-	    chdir ($iwd) or croak "Unable to chdir back into \"$iwd\", $!";
-
-	    return 1;
-	},
-	valid_versions => ['ge "3.5.7"'],
-	repository_file => "$REPOSITORY/tiff-v3.5.7-OSX-LZW.tar.gz",
+	    	return 1;
+		},
+		repository_file => "$REPOSITORY/bzip2-1.0.2.tar.gz",
     }
 );
 
@@ -324,7 +407,7 @@ sub execute {
 	my @error;
 
 	# Pre-install
-	&{$library->{pre_install}} if exists $library->{pre_install};
+	&{$library->{pre_install}}($library) if exists $library->{pre_install};
 
 	# Exceptions
 	if (exists $library->{exception} and &{$library->{exception}}) {
@@ -341,12 +424,13 @@ sub execute {
 	    my $source_file = $binary.".c";
 	    my $CC = "gcc";
 
-	    open (my $CHECK_C, ">", $source_file);
-	    print $CHECK_C ($library->{get_library_version}, "\n"); 
+	    open (my $CHECK_C, ">", $source_file)
+			or croak "Unable to create version check source file for $library->{name} $!";
+		print $CHECK_C ($library->{get_library_version}, "\n"); 
 	    close ($CHECK_C);
 
 	    $CC = whereis ("compiler") unless which ("$CC");
-	    @error = `$CC $source_file -o $binary 2>&1`;
+		@error = `$CC $source_file -o $binary 2>&1`;
 
 	    if ($? == 0) {
 		$library->{version} = `$binary 2>&1`;
@@ -398,7 +482,7 @@ sub execute {
     unless ($environment->flag ("LIB_CHECK")) {
 	print_header ("Core Binary Setup");
     
-	print "(All verbose information logged in $OME_TMP_DIR/$LOGFILE_NAME)\n\n";
+	print "(All verbose information logged in $INSTALL_HOME/$LOGFILE_NAME)\n\n";
 
 	my $retval = 0;
 
