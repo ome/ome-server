2854fe582dc825bdf6421d80b47973fd9f31b73d
Harry Hochheiser <hsh@nih.gov>
2003-10-27 17:11:32 +0000

    revised to use Bsplines between points, instead of straight lines
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2378 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/piccolo/PLink.java
+++ src/Attic/java/src/org/openmicroscopy/vis/piccolo/PLink.java
@@ -42,13 +42,17 @@ package org.openmicroscopy.vis.piccolo;
 
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.PNode;
-import java.lang.Math;
+import edu.umd.cs.piccolo.util.PPaintContext;
 import java.awt.BasicStroke;
 import java.awt.geom.Point2D;
 import java.awt.Color;
 import java.util.ArrayList;
-
-
+import java.awt.Graphics2D;
+import java.awt.geom.QuadCurve2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.GeneralPath;
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
 
 /** 
  * A Piccolo node for links between inputs and outputs. Link extends
@@ -63,7 +67,7 @@ import java.util.ArrayList;
 public abstract class PLink extends  PPath implements PNodeEventListener {
 	
 	public static final BasicStroke LINK_STROKE=
-		new BasicStroke(2,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND);
+		new BasicStroke(1,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND);
 
 	Point2D point = new Point2D.Float();
 	
@@ -80,6 +84,8 @@ public abstract class PLink extends  PPath implements PNodeEventListener {
 	
 	protected ArrayList points  = new ArrayList();
 	protected PPath arrow;
+	
+	protected Point2D pts[];
 	
 	
 	protected int pointCount = 0;	
@@ -99,6 +105,7 @@ public abstract class PLink extends  PPath implements PNodeEventListener {
 		buildArrow();
 		addChild(targets);
 		targets.setVisible(false);
+		setPickable(true);
 	}
 		
 	
@@ -141,6 +148,8 @@ public abstract class PLink extends  PPath implements PNodeEventListener {
 		setLine();
 	}
 	
+	protected abstract void setLine();
+	
 	public void setPoint(int index,Point2D pt) {
 		//System.err.println("setting point # "+index+", # of points is "+points.size());
 		if (points.size() <= index) {
@@ -148,16 +157,141 @@ public abstract class PLink extends  PPath implements PNodeEventListener {
 		}
 		else
 			points.set(index,pt);
+		updateBounds();
 	}
 	
-	protected void setLine() {
-		Point2D dummy[] = new Point2D[0];
-		Point2D pts[] = (Point2D []) points.toArray(dummy);
-		setPathToPolyline(pts);	
-	}
 
+	public void paint(PPaintContext aPaintContext) {
+		Graphics2D g = aPaintContext.getGraphics();
+		
+		g.setStroke(LINK_STROKE);
+		g.setPaint(DEFAULT_COLOR);
+		
+		Shape p = getLinkShape();
+		if (p != null)
+			g.draw(p);
+	}
+	
+	private Shape getLinkShape() {
+		Shape s=null;
+		Line2D line  = new Line2D.Double();
+		QuadCurve2D quad = new QuadCurve2D.Double();
+				
+		Point2D pts[] = (Point2D []) points.toArray(new Point2D[0]);
+		int size = pts.length;
+		if (size == 2) {
+			System.err.println("painting link with line - 2 pts.");
+			System.err.println("pt1 : "+pts[0].getX()+","+pts[0].getY());
+			System.err.println("pt2 : "+pts[1].getX()+","+pts[1].getY());
+			line.setLine(pts[0],pts[1]);
+			s= line;	
+		}
+		else if (size == 3) {
+			System.err.println("link with quad  - 3pts");
+			quad.setCurve(pts[0],pts[1],pts[2]);
+			s= quad;
+					}
+		else if (size > 3){
+			System.err.println("spline...");
+			s = drawBSpline(pts);	
+		}
+		return s;
+	}
+	
+	
+	// Based on sample code from "Computer Graphics for Java Programmers"
+	// http://home.planet.nl/~ammerall/grjava.html.
+	// I'm not aware of any restrictions on the use of this example code	
+	
+	protected GeneralPath drawBSpline(Point2D[] pts) {
+		int m = 50, n = pts.length;
+		
+		float p0x,p0y,p1x,p1y,p2x,p2y,p3x,p3y;
+		float x,y;
+		
+		GeneralPath p = new GeneralPath();
+		p.moveTo((float)pts[0].getX(),(float)pts[0].getY());
+		n = pts.length;
+		for (int i=1; i<n-2; i++) { 
+			  
+			  p3x=(float) ((-pts[i-1].getX()+3*(pts[i].getX()-pts[i+1].getX())+
+			  	pts[i+2].getX())/6); 
+			  p3y=(float) ((-pts[i-1].getY()+3*(pts[i].getY()-pts[i+1].getY())+
+			  	pts[i+2].getY())/6);
+			  
+			  p2x=(float) ((pts[i-1].getX()-2*pts[i].getX()+pts[i+1].getX())/2);
+			  p2y=(float) ((pts[i-1].getY()-2*pts[i].getY()+pts[i+1].getY())/2);
+			  p1x=(float) ((pts[i+1].getX()-pts[i-1].getX())/2);
+			  p1y=(float) ((pts[i+1].getY()-pts[i-1].getY())/2);
+		      p0x=(float) ((pts[i-1].getX()+4*pts[i].getX()+pts[i+1].getX())/6);
+		      p0y=(float) ((pts[i-1].getY()+4*pts[i].getY()+pts[i+1].getY())/6);
+		      
+			  for (float t=0; t<=1; t+=0.02) {  
+				x = ((p3x*t+p2x)*t+p1x)*t+p0x;
+				y = ((p3y*t+p2y)*t+p1y)*t+p0y;
+				p.lineTo(x,y);
+			}
+		}	
+		p.lineTo((float)pts[n-1].getX(),(float)pts[n-1].getY());
+		return p;
+	}
+	
+	protected GeneralPath drawBezier(Point2D[] pts) {
+		int m = 50, n = pts.length;
+		double  p0x, p1x, p2x, p3x, p0y, p1y, p2y, p3y, x, y;
+		
+		GeneralPath p = new GeneralPath();
+		p.moveTo((float)pts[0].getX(),(float)pts[0].getY());
+		n = pts.length;
+		for (int i=1; i<n-2; i+=4) { 
+			  
+			p0x=pts[i-1].getX();
+			p0y=pts[i-1].getY();
+			
+			p1x=-3*pts[i-1].getX()+3*pts[i].getX();
+			p1y=-3*pts[i-1].getY()+3*pts[i].getY();
+			
+			p2x = 3*pts[i-1].getX()-6*pts[i].getX()+3*pts[i+1].getX();
+			p2y = 3*pts[i-1].getY()-6*pts[i].getY()+3*pts[i+1].getY();
+			
+			p3x=-pts[i-1].getX()+3*(pts[i].getX()-pts[i+1].getX())+
+				pts[i+2].getX(); 
+						  
+			p3y=-pts[i-1].getY()+3*(pts[i].getY()-pts[i+1].getY())+
+							pts[i+2].getY(); 
+			
+			for (float t=0; t<=1; t+=0.02) {  
+				x = ((p3x*t+p2x)*t+p1x)*t+p0x;
+				y = ((p3y*t+p2y)*t+p1y)*t+p0y;
+				p.lineTo((float)x,(float)y);
+			}
+		}	
+		p.lineTo((float)pts[n-1].getX(),(float)pts[n-1].getY());
+		return p;
+	}
+	
+	protected void updateBounds() {
+		Shape s = getLinkShape();
+		if (s != null) {
+			Rectangle2D b = LINK_STROKE.createStrokedShape(s).getBounds2D();
+			System.err.println("updating bounds of curve..");
+			System.err.println(b.getX()+","+b.getY()+", width="+b.getWidth()
+				+", height = "+b.getHeight());
+			super.setBounds(b.getX(), b.getY(), b.getWidth(), b.getHeight());
+		}
+	}
+	
+	
+	public boolean setBounds(double x,double y,double width,double height) {
+		return false;
+	}
+	
+	public boolean intersects(Rectangle2D aBounds) {
+		return getLinkShape().intersects(aBounds);
+	}
+		
 	/*protected double getAngle(float xs,float ys,float xe,float ye) {
-		double angle = 0;
+		double angle = 0;de
 		double arctan = 0;
 		
 		if (xe !=xs) {
@@ -177,13 +311,13 @@ public abstract class PLink extends  PPath implements PNodeEventListener {
 	} */
  	
 	protected void drawLinkEnd(float x,float y) {
-		//System.err.println("setting arrow to be at "+x+","+y+", theta="+theta);
+		System.err.println("setting arrow to be at "+x+","+y);
 		Point2D pt = globalToLocal(new Point2D.Float(x,y));
 		//arrow.setRotation(theta);
 		arrow.setOffset(pt.getX(),pt.getY());
 		invalidateFullBounds(); 
 		
-		repaint();
+		repaint(); 
 	}
 	
 	
@@ -198,6 +332,7 @@ public abstract class PLink extends  PPath implements PNodeEventListener {
 	public abstract PLinkTarget getEndLinkTarget();
 
 	public void setSelected(boolean v) {
+		System.err.println(" setting a link to be selected..");
 		PLinkTarget startTarget = getStartLinkTarget();
 		PLinkTarget endTarget = getEndLinkTarget();
 		startTarget.setSelected(v);
