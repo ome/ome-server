c4777e4a8ca23b40715f62bab5eb83368554dfff
Josiah Johnston <siah@nih.gov>
2003-10-28 01:44:40 +0000

    added liscense, added export history flag
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2395 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tests/ExportSession.pl
+++ src/perl2/OME/Tests/ExportSession.pl
@@ -1,3 +1,40 @@
+# OME/Tests/ExportSession.pl
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Ilya Goldberg <igg@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+
 use strict;
 use OME::Tasks::OMEExport;
 use OME::SessionManager;
@@ -99,7 +136,7 @@ for (my $i=0; $i < @ARGV; $i++) {
 }
 
 logdbg "debug", 'Building DOM';
-$OMEExporter->buildDOM (\@exportObjects, ResolveAllRefs => 1, ExportSTDs => $ExportSTDs);
+$OMEExporter->buildDOM (\@exportObjects, ResolveAllRefs => 1, ExportSTDs => $ExportSTDs, ExportHistory => 1);
 
 if (defined $file) {
 	logdbg "debug", "Exporting to $file";
