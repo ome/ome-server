6c8029b7edc83f61dd222de2f75e2eb28637bc23
Harry Hochheiser <hsh@nih.gov>
2003-11-11 20:54:04 +0000

    pulled management of layering information into separate Layering class
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2484 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/ome/CChain.java
+++ src/Attic/java/src/org/openmicroscopy/vis/ome/CChain.java
@@ -56,7 +56,6 @@ import java.util.List;
 	
 	private static final int CROSSING_ITERATIONS=5;
 	private static final double DELTA=0.1;
-	private Vector layers = new Vector();
 	private boolean orderChanged = false;
 	private Layering layering  = new Layering();
 	
@@ -113,11 +112,10 @@ import java.util.List;
 		boolean ok= true;
 		int nodeCount = nodes.size();
 
-		CNode node;
+		CNode node=null;
 		CNode succ;
 		
 		do {
-			Vector layer = new Vector();
 			Iterator iter = nodes.iterator();
 			while (iter.hasNext()) {
 				ok = true;
@@ -146,19 +144,12 @@ import java.util.List;
 				if (ok == true) {
 					//no unassigned successors, so assign this node to
 					// the current layer
-					layer.addElement(node);
 					node.setLayer(currentLayer);
+					layering.addToLayer(currentLayer,node);
 					numAssigned++;
 				}
 			}
-			
-			if (layer.isEmpty()) {
-				System.err.println("Cycle removal failed?");
-			} else {
-				// Switch to next (upper) layer
-				currentLayer++;
-				layers.addElement(layer);
-			}
+			currentLayer++;	
 		} while (numAssigned < nodeCount);
 	}
 	
@@ -166,24 +157,25 @@ import java.util.List;
 	// make the layout proper - insert additional nodes on paths 
 	// that skip  levels .. ie., paths between levels l_i and l_j where j>i+1...
 	private void makeProper() {
-		int count = layers.size();
+		int count = layering.getLayerCount();
 		// for top (source) level and every level except for the last 2
 		// (any outlinks from level 1 already go to only level 0, by
 		// definition.
-	//	System.err.println("# of layers is "+count);
 		for (int i = count-1; i>1; i--)
 			makeProperLayer(i);
 	}
 	
 	private void makeProperLayer(int i) {
 		CNode node;
-	//	System.err.println("working on layer "+i);
-		Vector layer = (Vector) layers.get(i);
-		Iterator iter = layer.iterator();
-		while (iter.hasNext()) {
-			node = (CNode) iter.next();
-			makeProperNode(node,i);		
+		//	System.err.println("working on layer "+i);
+		try {
+			Iterator iter = layering.layerIterator(i);
+			while (iter.hasNext()) {
+				node = (CNode) iter.next();
+				makeProperNode(node,i);		
+			}
 		}
+		catch (Exception e) { }
 	}
 	
 	private void makeProperNode(CNode node,int i) {
@@ -219,12 +211,14 @@ import java.util.List;
 		else {
 			// create new node.
 			CLayoutNode dummy = new CLayoutNode();
+			
+			CLink semanticLink = link.getSemanticLink();
 			// make this node point to "to"
-			CLayoutLink dummyOutLink = new CLayoutLink(dummy,to);
+			CLayoutLink dummyOutLink = new CLayoutLink(semanticLink,dummy,to);
 			dummy.addSuccLink(dummyOutLink);
 			
 			// make node point to new node
-			CLayoutLink newOutLink = new CLayoutLink(node,dummy);
+			CLayoutLink newOutLink = new CLayoutLink(semanticLink,node,dummy);
 			
 			dummy.addPredLink(newOutLink);
 			
@@ -240,32 +234,39 @@ import java.util.List;
 			to.addPredLink(dummyOutLink);
 			
 			// add dummy to next layer.
-			Vector nextLayer = (Vector)layers.get(i-1);
-			nextLayer.add(dummy);			
+			layering.addToLayer(i-1,dummy);
+			
+			// adjust the semantic link to put dummy in between "from" and "to".
+			// invariant is that "from" is directly before "to", so just put it
+			//after "from"
+			semanticLink.addIntermediate(node,dummy);
 		}
 	}
 	
 	private void reduceCrossings() {
-		Iterator iter = layers.iterator();
-		
-		if (!iter.hasNext())
-			return; // no layers
-			
 		// first entry in layers is bottom layer (1 or zero)
-		assignPosFromLayer((Vector) iter.next());
+		
+		assignPosFromLayer(0);
+		int count=1;
 		
 		for (int i =0;  i < CROSSING_ITERATIONS; i++) {
 			orderChanged = false;
 			
 			// reduce successor crossings
-			while (iter.hasNext()) {
+			/*while (iter.hasNext()) { 11/10/03 hsh
 				Vector layer = (Vector) iter.next();
 				crossingReduction(layer,false);
+			}*/
+			for (count = 1; count < layering.getLayerCount(); count++) {
+				crossingReduction(count,false);
 			}
 			
-			for (int curLayer = layers.size()-2; curLayer >=0; curLayer--) {
-				Vector layer  = (Vector) layers.elementAt(curLayer);
-				crossingReduction(layer,false);
+			//for (int curLayer = layers.size()-2; curLayer >=0; curLayer--) {
+			for (int curLayer = layering.getLayerCount()-2; 
+				curLayer>=0;curLayer--) {
+				//Vector layer  = (Vector) layers.elementAt(curLayer);
+				//crossingReduction(layer,false); 11/10/03 hsh
+				crossingReduction(curLayer,true);
 			}
 			//stop if no changes
 			if (!orderChanged)
@@ -273,29 +274,36 @@ import java.util.List;
 		}
 	}
 	
-	private void crossingReduction(Vector layer,boolean pred) {
-		Iterator iter = layer.iterator();
-		CNode node;
-		Collection adjs;
-		double baryCenter=0.0;
+	private void crossingReduction(int layerNumber,boolean pred) {
+		try {
+			// Iterator iter = layer.iterator(); 11/10/03 hsh
+		//	System.err.println("crossing reduction - layer "+layerNumber);
+			Iterator iter = layering.layerIterator(layerNumber);
+			CNode node;
+			Collection adjs;
+			double baryCenter=0.0;
 		
-		while (iter.hasNext()) {
-			node = (CNode) iter.next();
-			if (pred == true)
-				adjs = node.getPredecessors();
-			else
-				adjs = node.getSuccessors();
-			if (adjs.size()>0)
-				baryCenter = calcBaryCenter(layer,adjs);
-			else
-				baryCenter = 0.0;
-			node.setPosInLayer(baryCenter); 
+			while (iter.hasNext()) {
+				node = (CNode) iter.next();
+				if (pred == true)
+					adjs = node.getPredecessors();
+				else	
+					adjs = node.getSuccessors();
+				if (adjs.size()>0)
+					baryCenter = calcBaryCenter(adjs);
+				else
+					baryCenter = 0.0;
+				node.setPosInLayer(baryCenter); 
+			}
+			sortLayerByPos(layerNumber);
+			assignPosFromLayer(layerNumber);
 		}
-		sortLayerByPos(layer);
-		assignPosFromLayer(layer);
+		catch(Exception e) {
+			System.err.println("exception caught!"); 
+		}
 	}
 	
-	private double calcBaryCenter(Vector layer,Collection adjs) {
+	private double calcBaryCenter(Collection adjs) {
 		double center=0.0;
 		int deg = adjs.size();
 		int total =0;
@@ -309,69 +317,56 @@ import java.util.List;
 	}
 	
 	
-	private void sortLayerByPos(Vector layer) {
-		int n = layer.size();
-		for (int i = 1; i < n; i++) {
-			CNode node = (CNode) layer.elementAt(i);
-			for (int j = i-1; j >=0; j--) {
-				CNode prev = (CNode) layer.elementAt(j);
-				if (prev.getPosInLayer() >= node.getPosInLayer()) {
-					layer.setElementAt(prev,j+1);
-					layer.setElementAt(node,j);
-					orderChanged = true;
+	private void sortLayerByPos(int layerNumber) {
+		try {
+			int n = layering.getLayerSize(layerNumber);
+		
+			for (int i = 1; i < n; i++) {
+				CNode node = layering.getNode(layerNumber,i);
+				for (int j = i-1; j >=0; j--) {
+					CNode prev = layering.getNode(layerNumber,j);
+					
+					if (prev.getPosInLayer() >= node.getPosInLayer()) {
+						layering.setNode(layerNumber,j+1,prev);
+						layering.setNode(layerNumber,j,node);
+						orderChanged = true;
+					}
 				}
 			}
 		}
+		catch(Exception e) {
+		}
 	}
 	
-	private void assignPosFromLayer(Vector layer) {
-		Iterator iter = layer.iterator();
+	private void assignPosFromLayer(int layerNumber) {
+		Iterator iter = layering.layerIterator(layerNumber);
 		double pos = 0.0;
 		
-		while (iter.hasNext()) {
-			CNode node = (CNode) iter.next();
-			node.setPosInLayer(pos);
-			pos +=1.0;
+		try {
+			//System.err.println("assigning position from layer "+layerNumber);
+			while (iter.hasNext()) {
+				CNode node = (CNode) iter.next();
+				node.setPosInLayer(pos);
+				pos +=1.0;
+			}
+		} 
+		catch(Exception e) {
+			System.err.println("exception!");
 		}
 	}
 	
-	public int getLayerCount() {
-		return layers.size();
+	public Layering getLayering() {
+		return layering;
 	}
 	
-	private Vector getLayer(int i) {
-		if (i < layers.size())
-			return (Vector) layers.elementAt(i);
-		else
-			return (Vector) null;
-	}
-
-	public int getLayerSize(int i) {
-		Vector v = getLayer(i);
-		if (v != null) {
-			return v.size();
-		}
-		else 
-			return -1;
-	}
-	
-	public CNode getLayerNode(int i, int j) {
-		CNode node = null;
-		Vector v = getLayer(i);
-		if (v != null) {
-			node = (CNode) v.elementAt(j);
-		}
-		return node;
-	}
 	
 	// stub code...
 	private void dumpLayers() {
 		System.err.println("Chain is "+getName());
-		int count = layers.size();
+		int count = layering.getLayerCount();
 		for (int i =0; i < count; i++) {
 			System.err.println("Layer "+i);
-			Vector layer = (Vector) layers.get(i);
-			Iterator iter = layer.iterator();
+			Iterator iter = layering.layerIterator(i);
 			while (iter.hasNext()) {
 				CNode node = (CNode) iter.next();
 				if (node instanceof CLayoutNode)
@@ -388,15 +383,65 @@ import java.util.List;
 			} 
 		}
 	}
-}
+ 
 
-class Layering {
-	private Vector layers = new Vector();
+	public class Layering {
+		// the vector that holds the abstract nodes
+		private Vector layers = new Vector();
 	
-	Layering() {
-	}
+		// vector that holds the piccolo instatations of the nodes
+		private Vector nodes = new Vector();
+		// and a vector for their x positions
+		private Vector xpositions = new Vector();
+		
+		Layering() {
+		}
 	
-	private void addLayer(Vector layer) {
-		layers.addElement(layer);		
-	}
-}
+		private void addLayer(Vector layer) {
+			layers.addElement(layer);		
+		}
+	
+		public void addToLayer(int layerNumber,CNode node) {
+			if (layerNumber > layers.size()-1) { // if we haven't created this layer yet
+				for (int i = layers.size(); i <= layerNumber; i++) {
+					Vector  v = new Vector();
+					layers.add(v);
+				}
+			}
+			Vector v = (Vector) layers.elementAt(layerNumber);
+			v.add(node);
+		}
+	
+		public int getLayerCount() {
+			return layers.size();
+		}
+
+		private Vector getLayer(int i) {
+			if (i < layers.size()) {
+				Vector v = (Vector) layers.elementAt(i);
+				return v;
+			}
+			else
+				return null;
+		}
+	
+		public Iterator layerIterator(int i) {
+			return getLayer(i).iterator();
+		}
+	
+		public int getLayerSize(int layerNumber) {
+			return getLayer(layerNumber).size();
+		}
+	
+		public CNode getNode(int layerNumber,int n) {
+			Vector v = getLayer(layerNumber);
+			CNode node = (CNode) v.elementAt(n);
+			return node;
+		}
+	
+		public void setNode(int layerNumber,int n,CNode node) {
+			Vector v = getLayer(layerNumber);
+			v.setElementAt(node,n);
+		}
+	} 
+ }
