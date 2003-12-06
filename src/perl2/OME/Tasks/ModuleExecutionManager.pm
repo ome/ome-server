45060fb6b641d5001a4f623b66254554a364865c
Doug Creager <dcreager@alum.mit.edu>
2003-12-06 02:34:00 +0000

    Handles new columns in MEX.  Allows multiple input MEXes per actual input.  Added createVirtualMEX method.  Added createNEX method.  Modified getAttributesForMEX to handle virtual MEXes.  Modified getInputTag to calculate tag for MEXes which haven't been created yet.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2674 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tasks/ModuleExecutionManager.pm
+++ src/perl2/OME/Tasks/ModuleExecutionManager.pm
@@ -91,7 +91,13 @@ use strict;
 use OME;
 our $VERSION = $OME::VERSION;
 
+use Carp;
+
 use OME::Session;
+use OME::Module;
+use OME::ModuleExecution;
+use OME::SemanticType;
+use OME::AnalysisChainExecution;
 
 =head1 METHODS
 
@@ -101,7 +107,8 @@ them commit any database transactions.
 =head2 createMEX
 
 	my $mex = OME::Tasks::ModuleExecutionManager->
-	    createMEX($module,$dependence,$target);
+	    createMEX($module,$dependence,$target,
+	              $iterator_tag,$new_feature_tag);
 
 Creates a new module execution of the given module.  It is marked as
 having the given dependence and target.  The $dependence parameter
@@ -117,26 +124,40 @@ defined until all of the actual inputs are specified.  For another,
 it's often unnecessary, and slows down this method.  To perform this
 check, call the B<***asYetUnwritten***> method.
 
+If the $iterator_tag or $new_feature_tag parameters are given, then
+they provide values for the MEX fields of the same name.  If either is
+undefined, that value is copied from the default_iterator or
+new_feature_tag column, respectively, of the module.
+
 =cut
 
 sub createMEX {
     my $class = shift;
-    my ($module,$dependence,$target) = @_;
-    my $session = OME::Session->instance();
-    my $factory = $session->Factory();
+    my ($module,$dependence,$target,$iterator_tag,$new_feature_tag) = @_;
+    my $factory = OME::Session->instance()->Factory();
+
+    Carp::cluck "Undefined module" unless defined $module;
 
     my $dataset = ($dependence eq 'D')? $target: undef;
     my $image = ($dependence eq 'I')? $target: undef;
 
+    # If either the iterator tag or new feature tag isn't specified,
+    # get its value from the module.
+
+    $iterator_tag ||= $module->default_iterator();
+    $new_feature_tag ||= $module->new_feature_tag();
+
     my $mex = $factory->
       newObject("OME::ModuleExecution",
                 {
-                 module     => $module,
-                 dependence => $dependence,
-                 dataset    => $dataset,
-                 image      => $image,
-                 timestamp  => 'now',
-                 status     => 'UNFINISHED',
+                 module          => $module,
+                 dependence      => $dependence,
+                 dataset         => $dataset,
+                 image           => $image,
+                 iterator_tag    => $iterator_tag,
+                 new_feature_tag => $new_feature_tag,
+                 timestamp       => 'now',
+                 status          => 'UNFINISHED',
                 });
 
     return $mex;
@@ -160,8 +181,7 @@ execution are used as input.
 sub addActualInput {
     my $class = shift;
     my ($output_mex,$input_mex,$input_name) = @_;
-    my $session = OME::Session->instance();
-    my $factory = $session->Factory();
+    my $factory = OME::Session->instance()->Factory();
 
     my $input_module = $input_mex->module();
 
@@ -183,8 +203,9 @@ sub addActualInput {
     my $actual_input = $factory->
       findObject("OME::ModuleExecution::ActualInput",
                  {
-                  module_execution => $input_mex,
-                  formal_input     => $input,
+                  module_execution       => $input_mex,
+                  formal_input           => $input,
+                  input_module_execution => $output_mex,
                  });
     die "MEX ".$input_mex->id()." (".$input_module->name().") already has an actual input for input $input_name"
       if defined $actual_input;
@@ -200,37 +221,195 @@ sub addActualInput {
     return $actual_input;
 }
 
+=head2 createVirtualMEX
+
+=cut
+
+sub createVirtualMEX {
+    my ($self,$attributes) = @_;
+    my $factory = OME::Session->instance()->Factory();
+
+    my %used_mexes;
+    my ($dependence,$target,$module,$iterator_tag,$new_feature_tag);
+
+    foreach my $attribute (@$attributes) {
+        my $mex = $attribute->module_execution();
+        $used_mexes{$mex->id()} = $mex;
+
+        my $mex_dependence = $mex->dependence();
+        die "Cannot create a virtual MEX -- dependence mismatch"
+          if (defined $dependence) && ($dependence ne $mex_dependence);
+        $dependence = $mex_dependence;
+
+        if ($dependence ne 'G') {
+            my $mex_target =
+              ($dependence eq 'D')? $mex->dataset(): $mex->image();
+            die "Cannot create a virtual MEX -- target mismatch"
+              if (defined $target) && (defined $mex_target)
+                && ($target->id() != $mex_target->id());
+            $target = $mex_target;
+        }
+
+        my $mex_module = $mex->module();
+        die "Cannot create a virtual MEX -- module mismatch"
+          if (defined $module) && (defined $mex_module)
+          && ($module->id() != $mex_module->id());
+        $module = $mex_module;
+
+        my $mex_iterator_tag = $mex->iterator_tag();
+        die "Cannot create a virtual MEX -- iterator tag mismatch"
+          if (defined $iterator_tag)
+          && ($iterator_tag ne $mex_iterator_tag);
+        $iterator_tag = $mex_iterator_tag;
+
+        my $mex_new_feature_tag = $mex->new_feature_tag();
+        die "Cannot create a virtual MEX -- new feature tag mismatch"
+          if (defined $new_feature_tag)
+          && ($new_feature_tag ne $mex_new_feature_tag);
+        $new_feature_tag = $mex_new_feature_tag;
+    }
+
+    # At this point, the %used_mexes hash should be populated with all
+    # of the MEX's used to create the input attributes.  If there's
+    # only one such MEX, then there is no need to create a virtual MEX.
+    # We should just return the existing MEX.
+
+    if (scalar(keys %used_mexes) == 1) {
+        my ($id, $mex) = each %used_mexes;
+        return $mex;
+    }
+
+    # Otherwise, we need to create the virtual MEX.
+    my $mex = $self->createMEX($module,$dependence,$target,
+                               $iterator_tag,$new_feature_tag);
+
+    foreach my $attribute (@$attributes) {
+        my $map = $factory->
+          maybeNewObject('OME::ModuleExecution::VirtualMEXMap',
+                         {
+                          module_execution => $mex,
+                          attribute        => $attribute,
+                         });
+    }
+
+    $mex->virtual_mex('t');
+    $mex->status('FINISHED');
+    $mex->storeObject();
+
+    return $mex;
+}
+
+=head2 createNEX
+
+	my $nex = OME::Tasks::ModuleExecutionManager->
+	    createNEX($mex,$chain_execution,$node);
+
+Creates a new node execution.  Node executions record which module
+execution was used to satisfy a node during the execution of an
+analysis chain.  The chain execution and node can be undefined, which
+signifies that this is a "universal execution".  Universal executions
+are basically a hack to make the import modules work correctly -- if a
+universal execution exists for a module, it will B<I<always>> be used
+to satisfy that module.
+
+=cut
+
+sub createNEX {
+    my $class = shift;
+    my ($mex,$chex,$node) = @_;
+    my $factory = OME::Session->instance()->Factory();
+
+    die "Chain execution and node must either both be specified or neither"
+      if (defined $chex && !defined $node)
+      || (defined $node && !defined $chex);
+
+    my $nex = $factory->
+      newObject("OME::AnalysisChainExecution::NodeExecution",
+                {
+                 module_execution         => $mex,
+                 analysis_chain_execution => $chex,
+                 analysis_chain_node      => $node,
+                });
+}
+
 =head2 getAttributesForMEX
 
-	my @attributes = OME::Tasks::ModuleExecutionManager->
+	my $attributes = OME::Tasks::ModuleExecutionManager->
 	    getAttributesForMEX($mex,$semantic_type,[\%extra_criteria]);
 
-Returns the attributes of a given semantic type which were created by
-a given module execution.  The final parameter, if specified, should
-be a hashref of extra criteria for the search.  They will be passed
-directly into the factory.
+Returns an array reference of the attributes of a given semantic type
+which were created by a given module execution.  The final parameter,
+if specified, should be a hashref of extra criteria for the search.
+They will be passed directly into the factory.
 
 =cut
 
 sub getAttributesForMEX {
     my $class = shift;
-    my ($mex,$semantic_type,$criteria) = @_;
-    $criteria->{module_execution} = $mex;
+    my ($mexes,$semantic_type,$criteria) = @_;
+    my $factory = OME::Session->instance()->Factory();
 
-    return OME::Session->instance()->Factory()->
-      findAttributes($semantic_type,$criteria);
+    $mexes = (ref($mexes) eq 'ARRAY')? $mexes: [$mexes];
+
+    my @regular_mexes;
+    my @virtual_mexes;
+
+    foreach my $mex (@$mexes) {
+        if ($mex->virtual_mex()) {
+            push @virtual_mexes, $mex;
+        } else {
+            push @regular_mexes, $mex;
+        }
+    }
+
+    my @attributes;
+
+    if (scalar(@regular_mexes) > 0) {
+        $criteria->{module_execution} = ['in',\@regular_mexes];
+
+        push @attributes, $factory->
+          findAttributes($semantic_type,$criteria);
+    }
+
+    if (scalar(@virtual_mexes) > 0) {
+        my @maps = $factory->
+          findObjects('OME::ModuleExecution::VirtualMEXMap',
+                      { module_execution => ['in',\@virtual_mexes] });
+        my @attr_ids = map { $_->attribute_id() } @maps;
+
+        $criteria->{module_execution} = ['in',\@attr_ids];
+
+        push @attributes, $factory->
+          findAttributes($semantic_type,$criteria);
+    }
+
+    return \@attributes;
 }
 
 =head2 getInputTag
 
 	my $tag = OME::Tasks::ModuleExecutionManager->getInputTag($mex);
+	my $tag = OME::Tasks::ModuleExecutionManager->
+	    getInputTag($module,$dependence,$target,
+	                $iterator_tag,$new_feature_tag,
+	                \%actual_inputs);
 
-Returns the input tag of an existing module execution.  This input tag
-is not well-defined until all of the actual inputs for the module
-execution have been recorded.  Calling code should not make any
-assumptions about the output of this method, except that it will be a
-string, and that two module executions with the same input tag are
-eligible candidates for attribute reuse.
+The first form returns the input tag of an existing module execution.
+This input tag is not well-defined until all of the actual inputs for
+the module execution have been recorded.
+
+The second form returns the input tag of a module execution that does
+not exist in the database yet.  All of the data necessary to create a
+module execution (the module and actual inputs) must be presented to
+this routine instead of the MEX itself.  The %actual_inputs parameter
+is a hash with formal input ID's as keys and MEX's as values.  (Each
+value can be either a single MEX object, or an array reference of MEX
+objects.)
+
+Calling code should not make any assumptions about the output of this
+method, except that it will be a string, and that two module
+executions with the same input tag are eligible candidates for
+attribute reuse.
 
 =cut
 
@@ -238,62 +417,157 @@ sub getInputTag {
     my $class = shift;
     my $factory = OME::Session->instance()->Factory();
 
-    my ($mex) = @_;
-    my $mexID = $mex->id();
+    my $param = shift;
 
-    my ($input_tag);
+    # This hairiness is to deal with the two ways of calling this
+    # method.  We store the contents of the "MEX" in local variables.
+    # With the first set of calling conventions, these variables are
+    # populated directly from the MEX object.  In the second, they're
+    # populated from the parameter list.  The actual inputs are
+    # stored as a subroutine ref, which returns a list of input MEX
+    # objects for a formal input.
+
+    my ($module,$dependence,$target,$get_input_mexes);
+    my ($iterator_tag,$new_feature_tag);
+
+    if (UNIVERSAL::isa($param,'OME::ModuleExecution')) {
+        # First method signature -
+        # getInputTag($mex)
+
+        my $mex = $param;
+
+        # See if we've already calculated the input tag
+        my $input_tag = $mex->input_tag();
+        return $input_tag if defined $input_tag;
+
+        $module = $mex->module();
+        $dependence = $mex->dependence();
+        if ($dependence eq 'G') {
+            $target = undef;
+        } elsif ($dependence eq 'D') {
+            $target = $mex->dataset();
+        } elsif ($dependence eq 'I') {
+            $target = $mex->image();
+        }
+        $iterator_tag = $mex->iterator_tag();
+        $new_feature_tag = $mex->new_feature_tag();
+
+        $get_input_mexes = sub {
+            my $formal_input = shift;
+
+            # Retrieve the actual input(s) for this formal input
+
+            my @actual_inputs = $factory->
+              findObjects("OME::ModuleExecution::ActualInput",
+                          {
+                           module_execution => $mex,
+                           formal_input     => $formal_input,
+                          });
+
+            # If there wasn't one, we're done with this input
+
+            unless (scalar(@actual_inputs) > 0) {
+                $input_tag .= "none) ";
+                next FORMAL_INPUT;
+            }
+
+            # Retrieve the MEX(es) that satisfied this formal input
+            my @input_mexes;
+            push @input_mexes, $_->input_module_execution()
+              foreach @actual_inputs;
+
+            return \@input_mexes;
+        };
+    } elsif (UNIVERSAL::isa($param,'OME::Module')) {
+        # Second method signature:
+        # getInputTag($module,$dependence,$target,\%actual_inputs)
+
+        $module = $param;
+        $dependence = shift;
+        $target = shift;
+        $iterator_tag = shift || $module->default_iterator();
+        $new_feature_tag = shift || $module->new_feature_tag();
+        my $actual_inputs = shift;
+
+        $get_input_mexes = sub {
+            my $formal_input = shift;
+            my $id = ref($formal_input)? $formal_input->id(): $formal_input;
+            my $val = $actual_inputs->{$id};
+
+            # This closure should return an array ref regardless of
+            # what's actually in the hash.
+            return ref($val) eq 'ARRAY'? $val: [$val];
+        };
+    }
+
+    my $input_tag;
 
     # Save the dependence and target of the MEX
 
-    if ($mex->dependence() eq 'G') {
+    if ($dependence eq 'G') {
         $input_tag = "G ";
-    } elsif ($mex->dependence() eq 'D') {
-        $input_tag = "D ".$mex->dataset()->id()." ";
+    } elsif ($dependence eq 'D') {
+        $input_tag = "D ".$target->id()." ";
     } else {
-        $input_tag = "I ".$mex->image()->id()." ";
+        $input_tag = "I ".$target->id()." ";
     }
+
+    $iterator_tag ||= "";
+    $new_feature_tag ||= "";
+
+    $input_tag .= "{".$iterator_tag."} {".$new_feature_tag."} ";
 
     my @formal_inputs;
 
+    # Add the formal inputs to the tag grouped by granularity
+
     foreach my $granularity ('G','D','I','F') {
         $input_tag .= lc($granularity)." ";
+
+        # Retrieve all of the formal inputs for this module of the
+        # current granularity.  Make sure that they're ordered!
+
         @formal_inputs = $factory->
           findObjects("OME::Module::FormalInput",
                       {
-                       module                      => $mex->module(),
+                       module                      => $module,
                        'semantic_type.granularity' => $granularity,
                        __order                     => 'id',
                       });
 
+      FORMAL_INPUT:
         foreach my $formal_input (@formal_inputs) {
             my $formal_inputID = $formal_input->id();
             $input_tag .= $formal_inputID."(";
 
-            my $actual_input = $factory->
-              findObject("OME::ModuleExecution::ActualInput",
-                         {
-                          module_execution => $mex,
-                          formal_input     => $formal_input,
-                         });
-            my $input_mex = $actual_input->input_module_execution();
+            my $input_mexes = $get_input_mexes->($formal_input);
+
+            # Get the attributes that satisfied this formal input
 
             my $semantic_type = $formal_input->semantic_type();
             my $attributes = $class->
-              getAttributesForMEX($input_mex,$semantic_type,
+              getAttributesForMEX($input_mexes,$semantic_type,
                                   {
                                    __order => 'id',
                                   });
 
-            my $first = $attributes->next();
+            # Create an ID list from these attributes.  Collapse
+            # consecutive sequential ID's into to prevent the string
+            # from growing overly large.
+
+            my $first = shift(@$attributes);
             if ($first) {
                 my $prev_id_top = $first->id();
                 my $prev_id_bot = $first->id();
 
-                while (my $attr = $attributes->next()) {
+                while (my $attr = shift(@$attributes)) {
                     my $id = $attr->id();
                     if ($id == $prev_id_bot+1) {
                         $prev_id_bot = $id;
                     } else {
+                        # Don't add anything to the tag until we've got
+                        # a break in the attribute ID's.
+
                         $input_tag .= ($prev_id_top == $prev_id_bot)?
                           "$prev_id_top ": "$prev_id_top-$prev_id_bot ";
 
