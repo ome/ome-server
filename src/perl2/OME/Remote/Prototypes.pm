ede3fa8a251820aac455c6be69f3c048aa90a097
Brian Hughes <bshughes@mit.edu>
2003-12-16 20:49:18 +0000

    entry points for image and dataset management
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2813 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Remote/Prototypes.pm
+++ src/perl2/OME/Remote/Prototypes.pm
@@ -410,6 +410,7 @@ addPrototype("OME::Project","owner",
 addPrototype("OME::Project","dataset_links",[],['OME::Project::DatasetMap','*']);
 addPrototype("OME::Project","dataset_links",[],['OME::Factory::Iterator'],
              publishedName => "iterate_dataset_links");
+addPrototype("OME::Project","addDataset", ['OME::Dataset'], ['OME::Dataset']);
 
 addPrototype("OME::Project::DatasetMap","project",
              ['OME::Project'],['OME::Project']);
@@ -429,6 +430,8 @@ addPrototype("OME::Dataset","project_links",[],['OME::Factory::Iterator'],
 addPrototype("OME::Dataset","image_links",[],['OME::Image::DatasetMap','*']);
 addPrototype("OME::Dataset","image_links",[],['OME::Factory::Iterator'],
              publishedName => "iterate_image_links");
+addPrototype("OME::Dataset","addImage", ['OME::Image'], ['OME::Image']);
+addPrototype("OME::Dataset","importImages",['$'],['$']);
 
 addPrototype("OME::Image","name",['$'],['$']);
 addPrototype("OME::Image","description",['$'],['$']);
@@ -831,7 +834,7 @@ sub findPrototype {
     my @classesToCheck = ($class);
     my $prototypeFound;
 
-    #print STDERR "*** $class ";
+    #print STDERR "*** $class\n ";
     while (my $nextClass = shift(@classesToCheck)) {
         if (exists $prototypes{$nextClass}->{$method}) {
             $prototypeFound = $prototypes{$nextClass}->{$method};
