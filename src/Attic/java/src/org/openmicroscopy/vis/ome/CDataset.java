34c206da72ec19651413ec61052bbd745dd4fce7
Harry Hochheiser <hsh@nih.gov>
2003-12-18 22:27:00 +0000

    dataset subclass for handling caching of images
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2839 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/Attic/java/src/org/openmicroscopy/vis/ome/CDataset.java
@@ -0,0 +1,88 @@
+/*
+ * org.openmicroscopy.vis.chains.ome.CDataset
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Harry Hochheiser <hsh@nih.gov>
+ *
+ *------------------------------------------------------------------------------
+ */
+ 
+package org.openmicroscopy.vis.ome;
+import org.openmicroscopy.remote.RemoteDataset;
+import org.openmicroscopy.remote.RemoteSession;
+import org.openmicroscopy.remote.RemoteObjectCache;
+import java.util.List;
+import java.util.Iterator;
+import java.util.Vector;
+
+
+ 
+/** 
+ * <p>A {@link Dataset} subclass used to hold information about datasets 
+ * in the chain builder.<p>
+ * 
+ * @author Harry Hochheiser
+ * @version 2.1
+ * @since OME2.1
+ */
+public class CDataset extends RemoteDataset {
+	
+	
+	static {
+		RemoteObjectCache.addClass("OME::Dataset",CDataset.class);
+	}
+	
+	public CDataset() {
+		super();
+	}
+	
+	public CDataset(RemoteSession session,String reference) {
+		super(session,reference);
+	}
+	
+	Vector images = new Vector();
+	
+	public void loadImages(Connection connection) {
+		List i = getImages();
+		Iterator iter = i.iterator();
+		while (iter.hasNext()) {
+			CImage image = (CImage) iter.next();
+			image.loadImageData(connection);
+			images.add(image);		
+		}
+	}
+	
+	public List getCachedImages() {
+		return images;
+	}
+}
