dc6fd4b84adbeeb6756dbeaa1bd3636f07751d4c
Harry Hochheiser <hsh@nih.gov>
2003-12-23 16:48:03 +0000

    a subclass of PImage that implements PBufferedBounds
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2869 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/Attic/java/src/org/openmicroscopy/vis/piccolo/PBufferedImage.java
@@ -0,0 +1,70 @@
+/*
+ * org.openmicroscopy.vis.piccolo.PBufferedImage
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Harry Hochheiser <hsh@nih.gov>
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+package org.openmicroscopy.vis.piccolo;
+import edu.umd.cs.piccolo.util.PBounds;
+import edu.umd.cs.piccolo.nodes.PImage;
+import java.awt.Image;
+
+/** 
+ * A {@link PImage} for displaying a thumbnail of an OME IMAGe
+ *
+ * 
+ * @author Harry Hochheiser
+ * @version 0.1
+ * @since OME2.0
+ */
+
+public class PBufferedImage extends PImage implements PBufferedNode {
+
+	
+	public PBufferedImage(Image image) {
+		super(image,false);
+	}
+	
+	public PBounds getBufferedBounds() {
+			PBounds b = getGlobalFullBounds();
+			return new PBounds(b.getX()-PConstants.BORDER,
+				b.getY()-PConstants.BORDER,
+				b.getWidth()+2*PConstants.BORDER,
+				b.getHeight()+2*PConstants.BORDER);
+	}
+}
