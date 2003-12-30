ef8276d5ddb888b1c296170f879463e555e8050d
Josiah Johnston <siah@nih.gov>
2003-12-30 14:53:02 +0000

    added additional search criteria results displayed using spiffy ImageTable restructured code & reduced reliance on HTMLFormat
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@2890 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/ImageSearch.pm
+++ src/perl2/OME/Web/ImageSearch.pm
@@ -43,8 +43,8 @@ use OME;
 $VERSION = $OME::VERSION;
 use CGI;
 
-use OME::Research::SearchEngine;
 use OME::Web::Helper::HTMLFormat;
+use OME::Web::ImageTable;
 
 use base qw(OME::Web);
 
@@ -56,55 +56,64 @@ sub getPageTitle {
 ####################
 
 sub getPageBody {
-	my	$self = shift ;
-	my 	$cgi = $self->CGI() ;
-	my	$htmlFormat=new OME::Web::Helper::HTMLFormat;
-
-	my 	$body="" ;
-
+	my $self = shift ;
+	my $cgi = $self->CGI() ;
+	my $session = $self->Session();
+	my $factory = $session->Factory();
+	my $body="" ;
+	
 	##########################
 	# DB info
 	# 	table name 
 	#	selected columns
-
-	my 	$table="images";			
-      my 	$selectedcolumns="name,inserted,image_id";	
-	##########################
-
-	my    $ref;
-	if ($cgi->param('search') ) {
-	   my $tableRows="";
-         my $string=cleaning($cgi->param('name'));
-	   $body.=format_form($htmlFormat,$cgi); 
-         return ('HTML',$body) unless length($string)>1;
-	   $body="";
-	   my $year=$cgi->param('year');
-	   my $month=$cgi->param('month');
-	   my $day=$cgi->param('day');
-	   my %htime=(
-		year=>$year,
-		month=>$month,
-		day =>$day,
-	   );
-
-
-
-         my $research=new OME::Research::SearchEngine($table,$selectedcolumns);
-         if (defined $research){
-	    $ref=$research->searchEngine($string,\%htime);
 	
-         }
-         if (defined $ref){
-		$body .=format_output($ref,$htmlFormat,$cgi);
-         }else{
-		$body.="No Image found.";
-		$body .=format_form($htmlFormat,$cgi);
+	my 	$table="images";			
+	my 	$selectedcolumns="name,inserted,image_id";	
+	##########################
+		
+	if ($cgi->param('search') ) {
+		my (@imgs, $imgName, %criteria);
+		( $imgName = $cgi->param('name') ) =~ s/^\s*(.*\S)\s*$/$1/;
+		
 
-         }
+		$criteria{ 'name' } = ['like', '%'.$imgName.'%']
+			if (length($imgName) > 1 );
 
-	}else{
-	   $body .=format_form($htmlFormat,$cgi);
-      }
+		my @create_after = grep( length($_) > 0, ( 
+			$cgi->param('create_after_year'),
+			$cgi->param('create_after_month'),
+			$cgi->param('create_after_day')
+		));
+		my $create_after_str = join( '-', @create_after);
+		$criteria{ 'created'} = ['>=',$create_after_str]
+			if scalar( @create_after ) == 3;
+
+		my @create_before = grep( length($_) > 0, ( 
+			$cgi->param('create_before_year'),
+			$cgi->param('create_before_month'),
+			$cgi->param('create_before_day')
+		) );
+		my $create_before_str = join( '-', @create_before);
+		$criteria{ 'created'} = ['<',$create_before_str]
+			if scalar( @create_before ) == 3;
+		
+		unless ( keys( %criteria ) > 0 ) {
+			$body .= format_form($cgi); 
+			return ('HTML',$body) ;
+		}
+		@imgs = $factory->findObjects( 'OME::Image', %criteria );
+		
+		if (scalar @imgs){
+			$body .= format_output(\@imgs,$cgi,\%criteria);
+			$body .= format_form($cgi);	
+		} else{
+			$body .= "No Images found.";
+			$body .= format_form($cgi);	
+		}
+	
+	} else{
+		$body .= format_form($cgi);
+	}
 	return ('HTML',$body) ;
 }
 
@@ -115,34 +124,137 @@ sub getPageBody {
 
 
 sub format_output{
-	my ($ref,$htmlFormat,$cgi)=@_;
+	my ($imgs,$cgi)=@_;
 	my $text="";
 	$text.="<h3>List of image(s) matching your data.</h3>";
-	$text.="<form>";
-	$text.=$htmlFormat->imageInDataset($ref,1,1);	
-	$text.="</form>";
+	my $t_generator = new OME::Web::ImageTable;
+	$text .= $t_generator->getTable( {
+			relations => 1,
+		}, @$imgs );
 	return $text;
 }
 
 
 
 sub format_form{
-	my ($htmlFormat,$cgi) =@_ ;
+	my ($cgi) =@_ ;
 	my $form="";
+
 	$form .=$cgi->startform;
-	$form.=$htmlFormat->formSearch("Images",1);
+	$form .="<h3>Search For Images </h3>";
+	$form .= $cgi->p( "<b>Name contains </b>",
+		$cgi->textfield( -name => 'name', -default => $cgi->param('name') || undef, -maxlength => 25 )
+	);
+	$form .= $cgi->p( "<b>Description contains </b>",
+		$cgi->textfield( -name => 'description', -default => $cgi->param('name') || undef, -maxlength => 25 )
+	);
+
+	my ($ryear,$rmonth,$rday) = OME::Web::Helper::HTMLFormat->YMD();
+	$form .= $cgi->p( $cgi->table( 
+		$cgi->caption( 'Creation Date' ),
+		$cgi->Tr( [
+			$cgi->th( { -colspan => 3, -align => 'CENTER' }, ['After', 'Before'] )
+		] ),
+		$cgi->Tr( {-align => 'LEFT'}, 
+			[
+				$cgi->td( ['year','month','day','year','month','day'] ),
+				$cgi->td( [
+					$cgi->popup_menu( 
+						-name   => 'create_after_year', 
+						-values => [ sort( keys( %$ryear ) ) ],
+						-default => $cgi->param('year') || undef,
+						-labels  => $ryear
+					),
+					$cgi->popup_menu( 
+						-name   => 'create_after_month', 
+						-values => [ sort( keys( %$rmonth ) ) ],
+						-default => $cgi->param('month') || undef,
+						-labels  => $rmonth
+					),
+					$cgi->popup_menu( 
+						-name   => 'create_after_day',
+						-values => [ sort( keys( %$rday ) ) ],
+						-default => $cgi->param('day') || undef,
+						-labels  => $rday
+					),
+					$cgi->popup_menu( 
+						-name   => 'create_before_year', 
+						-values => [ sort( keys( %$ryear ) ) ],
+						-default => $cgi->param('year') || undef,
+						-labels  => $ryear
+					),
+					$cgi->popup_menu( 
+						-name   => 'create_before_month', 
+						-values => [ sort( keys( %$rmonth ) ) ],
+						-default => $cgi->param('month') || undef,
+						-labels  => $rmonth
+					),
+					$cgi->popup_menu( 
+						-name   => 'create_before_day',
+						-values => [ sort( keys( %$rday ) ) ],
+						-default => $cgi->param('day') || undef,
+						-labels  => $rday
+					)
+				] )
+			]
+		)
+	) );
+
+#	$form .= $cgi->p( $cgi->table( 
+#		$cgi->caption( 'Import Date' ),
+#		$cgi->Tr( [
+#			$cgi->th( { -colspan => 3, -align => 'CENTER' }, ['After', 'Before'] )
+#		] ),
+#		$cgi->Tr( {-align => 'LEFT'}, 
+#			[
+#				$cgi->td( ['year','month','day','year','month','day'] ),
+#				$cgi->td( [
+#					$cgi->popup_menu( 
+#						-name   => 'import_after_year', 
+#						-values => [ sort( keys( %$ryear ) ) ],
+#						-default => $cgi->param('year') || undef,
+#						-labels  => $ryear
+#					),
+#					$cgi->popup_menu( 
+#						-name   => 'import_after_month', 
+#						-values => [ sort( keys( %$rmonth ) ) ],
+#						-default => $cgi->param('month') || undef,
+#						-labels  => $rmonth
+#					),
+#					$cgi->popup_menu( 
+#						-name   => 'import_after_day',
+#						-values => [ sort( keys( %$rday ) ) ],
+#						-default => $cgi->param('day') || undef,
+#						-labels  => $rday
+#					),
+#					$cgi->popup_menu( 
+#						-name   => 'import_before_year', 
+#						-values => [ sort( keys( %$ryear ) ) ],
+#						-default => $cgi->param('year') || undef,
+#						-labels  => $ryear
+#					),
+#					$cgi->popup_menu( 
+#						-name   => 'import_before_month', 
+#						-values => [ sort( keys( %$rmonth ) ) ],
+#						-default => $cgi->param('month') || undef,
+#						-labels  => $rmonth
+#					),
+#					$cgi->popup_menu( 
+#						-name   => 'import_before_day',
+#						-values => [ sort( keys( %$rday ) ) ],
+#						-default => $cgi->param('day') || undef,
+#						-labels  => $rday
+#					)
+#				] )
+#			]
+#		)
+#	) );
+	
+	$form .= $cgi->submit( -name => 'search', -value => 'Search' );
 	$form .=$cgi->endform;
 	return $form ;
 }
 
-
-sub cleaning{
- my ($string)=@_;
- chomp($string);
- $string=~ s/^\s*(.*\S)\s*/$1/;
- return $string;
-
-}
 
 1;
 
