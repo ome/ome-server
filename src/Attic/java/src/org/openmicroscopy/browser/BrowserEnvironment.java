e99f2189a158eff3d91030248fab51b9c1a29dcf
Jeff Mellen <jeffm@alum.mit.edu>
2004-01-13 23:09:07 +0000

    adding first round of method stubs (some partially implemented, the simple stuff)
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3060 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/Attic/java/src/org/openmicroscopy/browser/BrowserEnvironment.java
@@ -0,0 +1,121 @@
+/*
+ * org.openmicroscopy.browser.BrowserEnvironment
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Jeff Mellen <jeffm@alum.mit.edu>
+ *
+ *------------------------------------------------------------------------------
+ */
+package org.openmicroscopy.browser;
+
+import org.openmicroscopy.browser.ome.OMEBrowserAgent;
+
+/**
+ * Singleton class which contains references to all universally referencable
+ * browser components, such as the OMEBrowserAgent and OMEDataAgent.
+ * 
+ * @author Jeff Mellen, <a href="mailto:jeffm@alum.mit.edu">jeffm@alum.mit.edu</a>
+ * <b>Internal version:</b> $Revision$ $Date$
+ * @version 2.2
+ * @since 2.2
+ */
+public final class BrowserEnvironment
+{
+  private static boolean initialized = false;
+  private static BrowserEnvironment environment = null;
+  
+  private OMEBrowserAgent browserAgent;
+  // private static OMEDataAgent dataAgent;
+  private MessageHandler messageHandler;
+  
+  private BrowserEnvironment()
+  {
+    // dummy singleton constructor
+    initialized = true;
+  }
+  
+  /**
+   * Returns the environment's OMEBrowserAgent.
+   * 
+   * @return The OME browser agent of the environment.
+   */
+  public OMEBrowserAgent getBrowserAgent()
+  {
+    return browserAgent;
+  }
+  
+  /**
+   * Sets the environment's OMEBrowserAgent.
+   * 
+   * @param agent The OME browser agent of the environment.
+   */
+  public void setBrowserAgent(OMEBrowserAgent agent)
+  {
+    this.browserAgent = agent;
+  }
+  
+  /**
+   * Gets the message handler for the environment.
+   * 
+   * @return The message handler of the environment.
+   */
+  public MessageHandler getMessageHandler()
+  {
+    return messageHandler;
+  }
+  
+  /**
+   * Sets the message handler for the environment.
+   * 
+   * @param handler The message hander to use.
+   */
+  public void setMessageHandler(MessageHandler handler)
+  {
+    this.messageHandler = handler;
+  }
+  
+  // TODO: Add OMEDataAgent get/set methods
+  
+  /**
+   * Gets the BrowserEnvironment instance.
+   * @return
+   */
+  public static BrowserEnvironment getInstance()
+  {
+    if(!initialized)
+    {
+      environment = new BrowserEnvironment();
+    }
+    return environment;
+  }
+}
