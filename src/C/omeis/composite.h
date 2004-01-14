9d3be53ba125875ce6af609c31c7a7fffbda6ca6
Ilya Goldberg <igg@nih.gov>
2004-01-14 07:29:31 +0000

    The Composite method for omeis - initial commit
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3063 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/C/omeis/composite.h
@@ -0,0 +1,60 @@
+/*------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:	Ilya G. Goldberg <igg@nih.gov>   1/2004
+ * 
+ *------------------------------------------------------------------------------
+ */
+
+#ifndef OMEIS_MAGICK_H
+#define OMEIS_MAGICK_H
+
+#include "omeis.h"
+
+typedef struct {
+	PixelsRep *thePixels;
+	int theZ, theT;
+	int sizeX, sizeY;
+	char isRGB;
+	size_t nPix;
+	channelSpecType RGBAGr[5];
+	char format[32];
+	FILE *stream;
+	void *blob;
+} CompositeSpec;
+
+
+int DoComposite (PixelsRep *myPixels, int theZ, int theT, char **param);
+int DoCompositeIM   (CompositeSpec *myComposite, char **param);
+int DoCompositeJPEG (CompositeSpec *myComposite, char **param);
+
+
+#endif
