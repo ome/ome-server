99ffbf5d4198c2731ef6815d03aaff6b573062a3
Harry Hochheiser <hsh@nih.gov>
2004-01-27 17:56:05 +0000

    revised to use ImageIO instead of reading to a buffer. also, added code to create and use image cache
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3245 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/ome/ThumbnailAgent.java
+++ src/Attic/java/src/org/openmicroscopy/vis/ome/ThumbnailAgent.java
@@ -31,7 +31,8 @@
 
 /*------------------------------------------------------------------------------
  *
- * Written by:    Andrea Falconi <a.falconi@dundee.ac.uk>
+ * Written by:    Andrea Falconi <a.falconi@dundee.ac.uk> & 
+ * 				  Harry Hochheiser <hsh@nih.gov>
  *
  */
  
@@ -39,11 +40,12 @@ package org.openmicroscopy.vis.ome;
 
 import java.io.BufferedInputStream;
 import java.io.PrintWriter;
+import java.io.File;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
-import javax.swing.ImageIcon;
-import java.awt.Image;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 
 /** 
@@ -62,11 +64,15 @@ import java.awt.Image;
 
 public class ThumbnailAgent {
     
+    private static final String CACHE_DIR="/.ome-vis-cache/";
+    private static final String IMAGE_DIR="/images";
+    
 	private String      host;
 	private String      username;
 	private String      password;
 	private String      sessionKey;
   
+  	private File cacheDirFile;
   
 	public ThumbnailAgent(String host, String username, String password) {
 
@@ -96,36 +102,47 @@ public class ThumbnailAgent {
 		s = s.trim();
 		sessionKey = s.substring(0, s.indexOf(';'));  //"SESSION_KEY=blahblah"
 		System.err.println(" session key is "+sessionKey);
-
+		
+		// create cache if it doesn't exist.
+		String dir = System.getProperty("user.dir");
+		String cacheDir = new String(dir+CACHE_DIR+host+IMAGE_DIR);
+		System.err.println("cache dir is "+cacheDir);
+		// make it if it doesn't exist
+		cacheDirFile = new File(cacheDir); 
+		cacheDirFile.mkdirs();
+		
 	}
   
-	public Image getThumbnail(int id) throws Exception {
+	public BufferedImage getThumbnail(int id) throws Exception {
 		URL     thumbURL = new URL("http://"+host+
 					"/perl2/serve.pl?Page=OME::Web::ThumbWrite&ImageID="+id);
-		URLConnection   conn = thumbURL.openConnection();
-		//set session key (HTTP header)
-		conn.setRequestProperty("Cookie", sessionKey); 
+		// get file name
+		String imageFileName = new String("thumb-"+id+".jpg");
+		File imageFile = new File(cacheDirFile,imageFileName);
+		BufferedImage bufImage;
+		
+		if (!imageFile.exists()) { 
+			// if it doesn't exist, grab it.
+			System.err.println("getting image from network");
+			URLConnection   conn = thumbURL.openConnection();
+			//set session key (HTTP header)
+			conn.setRequestProperty("Cookie", sessionKey); 
 
-		//send request and get response stream
-		BufferedInputStream     
-			response = new BufferedInputStream(conn.getInputStream()); 
+			//send request and get response stream
+			BufferedInputStream     
+				response = new BufferedInputStream(conn.getInputStream()); 
     
-		//read response as stream of bytes
-		byte[]      buf = new byte[4096]; //4Kb
-		int i  =0 ;
-		for(int value=0; (value=response.read()) != -1; ++i) {
-			buf[i] = (byte)(value&0xFF);
-			if( i == buf.length-1 ) {
-			//	System.err.println("read 4096 bytes. copying..");
-				byte[]  tmp = new byte[buf.length+1024];
-				System.arraycopy(buf, 0, tmp, 0, buf.length);
-				buf = tmp;
-			}
+		
+			bufImage = ImageIO.read(response);
+			response.close();
+			
+			// write it
+			ImageIO.write(bufImage,"jpg",imageFile);
 		}
-		//System.err.println("read "+i+" image bytes");
-		response.close();
-		//build image from data (will work for jpeg and gif only)
-		ImageIcon res = new ImageIcon(buf);
-		return res.getImage(); 
+		else { // get it from cache.
+			System.err.println("getting cached image");
+			bufImage = ImageIO.read(imageFile);
+		}
+		return bufImage;
 	}
 }
