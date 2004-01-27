7e1457fa9d94558843c7c78bf16076c8cfc8877b
Josiah Johnston <siah@nih.gov>
2004-01-27 03:31:57 +0000

    added license added scale buttons by display channel made supplimentary control windows accessed by a popup list
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3207 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/JavaScript/SVGviewer/xyPlaneControls.js
+++ src/JavaScript/SVGviewer/xyPlaneControls.js
@@ -1,13 +1,30 @@
 /*****
 *
 *	imageControls.js
-*		Global Data dependencies: theT, theZ, Z, T
-*		Global Function dependencies: updateTheT, updateTheZ
-*		
-*		External file dependencies: none
+
+	Copyright (C) 2003 Open Microscopy Environment
+		Massachusetts Institute of Technology,
+		National Institutes of Health,
+		University of Dundee
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+	
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-*
+
+	Written by: Josiah Johnston <siah@nih.gov>
+
 *****/
 
 svgns = "http://www.w3.org/2000/svg";
@@ -35,17 +52,20 @@ ImageControls.prototype.init = function(  ) {
 buildControls( ... )
 
 parameters:
-	functionPointers - associative array (AKA hash) of functions to tie controls into
+	actions - associative array (AKA hash) of functions or 'method name'/object pairs
+		to tie controls into
+	supplimentaryControls - control window names to give access to in a pop-up list
+	channelLabels - labels of the channels in the image
 
 *****/
-ImageControls.prototype.buildControls = function( functionPointers ) {
+ImageControls.prototype.buildControls = function( actions, supplimentaryControls, channelLabels ) {
 
 	this.controlsRoot = svgDocument.createElementNS( svgns, "g" );
 
 	// Z section controls
 	this.zSlider = new Slider(
 		30, 120, 100, -90,
-		functionPointers['zSlider'],
+		actions['zSlider'],
 		skinLibrary["zSliderBody"],
 		skinLibrary["zSliderThumb"]
 	);
@@ -54,24 +74,24 @@ ImageControls.prototype.buildControls = function( functionPointers ) {
 	this.zSlider.getLabel().setAttribute( "text-anchor", "middle" );
 	this.zUpButton = new button(
 		15, 106, 
-		functionPointers['zUp'],
+		actions['zUp'],
 		skinLibrary["triangleUpWhite"]
 	);
 	this.zDownButton = new button(
 		15, 110,
-		functionPointers['zDown'],
+		actions['zDown'],
 		skinLibrary["triangleDownWhite"]
 	);
 	this.zAnimUpButton = new button(
 		15, 86, 
-		functionPointers['zAnimUp'],
+		actions['zAnimUp'],
 		skinLibrary["triangleUpRed"],
 		null,
 		skinLibrary["triangleUpWhite"]
 	);
 	this.zAnimDownButton = new button(
 		15, 90, 
-		functionPointers['zAnimDown'],
+		actions['zAnimDown'],
 		skinLibrary["triangleDownRed"],
 		null,
 		skinLibrary["triangleDownWhite"]
@@ -80,30 +100,30 @@ ImageControls.prototype.buildControls = function( functionPointers ) {
 	// Timepoint controls
 	this.tSlider = new Slider(
 		60, 30, 100, 0,
-		functionPointers['tSlider']
+		actions['tSlider']
 	);
 	this.tSlider.setLabel(60,-13,"");
 	this.tSlider.getLabel().setAttribute( "fill", "white" );
 	this.tUpButton = new button(
 		182, 25,
-		functionPointers['tUp'],
+		actions['tUp'],
 		skinLibrary["triangleRightWhite"]
 	);
 	this.tDownButton = new button(
 		178, 25,
-		functionPointers['tDown'],
+		actions['tDown'],
 		skinLibrary["triangleLeftWhite"]
 	);
 	this.tAnimUpButton = new button(
 		182, 35, 
-		functionPointers['tAnimUp'],
+		actions['tAnimUp'],
 		skinLibrary["triangleRightRed"],
 		null,
 		skinLibrary["triangleRightWhite"]
 	);
 	this.tAnimDownButton = new button(
 		178, 35, 
-		functionPointers['tAnimDown'],
+		actions['tAnimDown'],
 		skinLibrary["triangleLeftRed"],
 		null,
 		skinLibrary["triangleLeftWhite"]
@@ -111,8 +131,8 @@ ImageControls.prototype.buildControls = function( functionPointers ) {
 
 	// wavelength to channel popupLists
 	this.redPopupList = new popupList(
-		-50, 0, fluors, 
-		functionPointers['updateR'],
+		-50, 0, channelLabels, 
+		actions['setRedLogicalChannel'],
 		1,
 		skinLibrary["redAnchorText"],
 		skinLibrary["redItemBackgroundText"],
@@ -120,8 +140,8 @@ ImageControls.prototype.buildControls = function( functionPointers ) {
 	);
 
 	this.greenPopupList = new popupList(
-		0, 0, fluors, 
-		functionPointers['updateG'],
+		0, 0, channelLabels, 
+		actions['setGreenLogicalChannel'],
 		0,
 		skinLibrary["greenAnchorText"],
 		skinLibrary["greenItemBackgroundText"],
@@ -129,96 +149,110 @@ ImageControls.prototype.buildControls = function( functionPointers ) {
 	);
 
 	this.bluePopupList = new popupList(
-		50, 0, fluors, 
-		functionPointers['updateB'],
+		50, 0, channelLabels, 
+		actions['setBlueLogicalChannel'],
 		0,
 		skinLibrary["blueAnchorText"],
 		skinLibrary["blueItemBackgroundText"],
 		skinLibrary["blueItemHighlightText"]
 	);
 	
-	this.bwPopupList = new popupList(
-		0, 0, fluors, functionPointers['updateBW']
+	this.greyPopupList = new popupList(
+		0, 0, channelLabels, actions['setGreyLogicalChannel']
 	);
 	
 	// set up channel on/off buttons
 	this.redButton = new button( 
 		Math.round(this.redPopupList.x + this.redPopupList.width/2), -13, 
-		functionPointers['OnOffR'],
+		actions['OnOffR'],
 		skinLibrary["redButtonOn"],
 		skinLibrary["redButtonOff"],
 		skinLibrary["blankButtonRadius5Highlight"]
 	);
 	this.greenButton = new button( 
 		Math.round(this.greenPopupList.x + this.greenPopupList.width/2), -13, 
-		functionPointers['OnOffG'],
+		actions['OnOffG'],
 		skinLibrary["greenButtonOn"],
 		skinLibrary["greenButtonOff"],
 		skinLibrary["blankButtonRadius5Highlight"]
 	);
 	this.blueButton = new button(
 		Math.round(this.bluePopupList.x + this.bluePopupList.width/2), -13,
-		functionPointers['OnOffB'],
+		actions['OnOffB'],
 		skinLibrary["blueButtonOn"],
 		skinLibrary["blueButtonOff"],
 		skinLibrary["blankButtonRadius5Highlight"]
 	);
+
+	this.redScaleButton = new button(
+		Math.round(this.redPopupList.x + this.redPopupList.width/2), 25,
+		actions['showRedScale'],
+		'<text fill="maroon" text-anchor="middle">Scale</text>',
+		null,
+		'<text fill="white" text-anchor="middle">Scale</text>'
+	);
+	this.greenScaleButton = new button(
+		Math.round(this.greenPopupList.x + this.greenPopupList.width/2), 25,
+		actions['showGreenScale'],
+		'<text fill="darkgreen" text-anchor="middle">Scale</text>',
+		null,
+		'<text fill="white" text-anchor="middle">Scale</text>'
+	);
+	this.blueScaleButton = new button(
+		Math.round(this.bluePopupList.x + this.bluePopupList.width/2), 25,
+		actions['showBlueScale'],
+		'<text fill="midnightblue" text-anchor="middle">Scale</text>',
+		null,
+		'<text fill="white" text-anchor="middle">Scale</text>'
+	);
+	this.greyScaleButton = new button(
+		Math.round(this.greyPopupList.x + this.greyPopupList.width/2), 25,
+		actions['showGreyScale'],
+		'<text fill="black" text-anchor="middle">Scale</text>',
+		null,
+		'<text fill="white" text-anchor="middle">Scale</text>'
+	);
+
 	
 	// save button
 	this.saveButton = new button(
 		85, 130, 
-		functionPointers['Save'],
+		actions['Save'],
 		'<text fill="black" text-anchor="end">Save</text>',
 		null,
 		'<text fill="white" text-anchor="end">Save</text>'
 	);
 	this.loadButton = new button(
 		85, 140, 
-		functionPointers['preload'],
-		'<text fill="black" text-anchor="end">Load All</text>',
+		actions['preload'],
+		'<text fill="black" text-anchor="end">Prefetch</text>',
 		null,
-		'<text fill="white" text-anchor="end">Load All</text>'
+		'<text fill="white" text-anchor="end">Prefetch</text>'
 	);
 	
 
 	// set up RGB to grayscale button
 	this.RGB_BWbutton = new button(
-		110, 115, 
-		functionPointers['RGB2BW'],
+		105, 115, 
+		actions['RGB2BW'],
 		skinLibrary["RGB_BWButtonOn"],
 		skinLibrary["RGB_BWButtonOff"],
 		skinLibrary["blankButtonRadius13Highlight"]
 	);
 	
 	// buttons to access panes
-	this.scaleButton = new button(
-		190, 120,
-		functionPointers['showScale'],
-		'<text fill="black" text-anchor="end">Scale</text>',
-		null,
-		'<text fill="white" text-anchor="end">Scale</text>'
-	);
 	
 // Rest of these buttons should live somewhere else.
-	this.statsButton = new button(
-		190, 110, showStats,
-		'<text fill="black" text-anchor="end">Stats</text>',
+	this.panePopupList = new popupList(
+		125, 125,
+		supplimentaryControls,
+		actions['openWindow'],
 		null,
-		'<text fill="white" text-anchor="end">Stats</text>'
+		skinLibrary["transparentBox"],
+		null, 
+		skinLibrary["whiteTranslucentBox"],
+		[ 'text-anchor', 'end' ]
 	);
-	this.overlayButton = new button(
-		190, 130, showOverlay,
-		'<text fill="black" text-anchor="end">Overlay</text>',
-		null,
-		'<text fill="white" text-anchor="end">Overlay</text>'
-	);
-	this.settingsButton = new button(
-		190, 140, showPreferences,
-		'<text fill="black" text-anchor="end">Preferences</text>',
-		null,
-		'<text fill="white" text-anchor="end">Preferences</text>'
-	);
-		
 
 	// Z & T controls
 	this.zSlider.realize(this.controlsRoot);
@@ -250,20 +284,21 @@ ImageControls.prototype.buildControls = function( functionPointers ) {
 	this.redPopupList.realize( this.RGBpopupListBox );
 	this.greenPopupList.realize( this.RGBpopupListBox );
 	this.bluePopupList.realize( this.RGBpopupListBox );
+	this.redScaleButton.realize( this.RGBpopupListBox );
+	this.greenScaleButton.realize( this.RGBpopupListBox );
+	this.blueScaleButton.realize( this.RGBpopupListBox );
 
 	// Grayscale controls
 	this.BWpopupListBox = svgDocument.createElementNS( svgns, "g" );
 	this.BWpopupListBox.setAttribute( "transform", "translate( 95, 70 )" );
 	this.BWpopupListBox.setAttribute( "display", "none" );
 	this.controlsRoot.appendChild( this.BWpopupListBox );
-	this.bwPopupList.realize( this.BWpopupListBox );
-
-	this.statsButton.realize( this.controlsRoot );
-	this.scaleButton.realize( this.controlsRoot );
-	this.overlayButton.realize( this.controlsRoot );
-	this.settingsButton.realize( this.controlsRoot );
+	this.greyPopupList.realize( this.BWpopupListBox );
+	this.greyScaleButton.realize( this.BWpopupListBox );
+	
+	this.panePopupList.realize( this.controlsRoot );
 
 	return this.controlsRoot;
 
-}
+};
 
