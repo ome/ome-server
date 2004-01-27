422fc4012ac428da350aa0a4883ff0acdfa1afa4
Josiah Johnston <siah@nih.gov>
2004-01-27 03:37:18 +0000

    added license refactored API into a standard controller pattern simplified initialization parameters and decreased spaghetti factor by moving the control logic inside the controller class
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3209 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/JavaScript/SVGviewer/ViewerPreferences.js
+++ src/JavaScript/SVGviewer/ViewerPreferences.js
@@ -1,10 +1,29 @@
 /*****
 
 	ViewerPreferences.js
-		external file dependencies: widget.js, button.js, popupList.js, slider.js
-		
-		Author: Josiah Johnston
-		email: siah@nih.gov
+
+Copyright (C) 2003 Open Microscopy Environment
+		Massachusetts Institute of Technology,
+		National Institutes of Health,
+		University of Dundee
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+	
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+	Written by: Josiah Johnston <siah@nih.gov>
 	
 *****/
 
@@ -15,7 +34,8 @@ var svgns = "http://www.w3.org/2000/svg";
 	class variables
 	
 *****/
-ViewerPreferences.VERSION = 0.1;
+ViewerPreferences.VERSION = .2;
+ViewerPreferences.ScaleSliderWidth = 50;
 
 /********************************************************************************************/
 /********************************************************************************************/
@@ -37,85 +57,120 @@ function ViewerPreferences( resizeToolBox, resizeMultiToolBox, savePreferences )
 	this.init( resizeToolBox, resizeMultiToolBox, savePreferences );
 }
 
-/*****
+ViewerPreferences.prototype.buildToolBox = function( controlLayer ) {
+	var displayContent = this.buildDisplay();
+	var bbox = displayContent.getBBox();
+	var width = bbox.width + 2 * toolBox.prototype.padding+ 20;
+// hack alert!! '+ 20'
+	var height = bbox.height + 2 * toolBox.prototype.padding+ 20;
+	this.toolBox = new toolBox(
+		255, 250, width, height
+	);
+	this.toolBox.closeOnMinimize( true );
+	this.toolBox.setLabel(10,12,"Viewer Preferences");
+	this.toolBox.getLabel().setAttribute( "text-anchor", "start");
+	this.toolBox.realize( controlLayer );
+	this.displayPane = this.toolBox.getGUIbox();
+	this.displayPane.appendChild( displayContent );
 	
-	buildSVG
-	
-	returns:
-		SVG chunk describing Overlay pane
-	notes:
-		for use in conjuction with multipaneToolBox
-		
-	tested
-		
-*****/
-ViewerPreferences.prototype.buildSVG = function() {
+}
+
+ ViewerPreferences.prototype.buildDisplay = function() {
 	// check for initialization
-	if(this.resizeToolBox == null) return null;
+	if(! this.initialized) return null;
 
 // build SVG
-	this.root = svgDocument.createElementNS(svgns, "g");
+	this.displayContent = svgDocument.createElementNS(svgns, "g");
 
 	// set up GUI
 	this.toolBoxSizeSlider = new Slider(
-		120,15,50,0,
-		this.resizeToolBox
+		110,0,ViewerPreferences.ScaleSliderWidth,0,
+		{ obj: this, method: 'resizeToolboxes' }
 	);
 	this.toolBoxSizeSlider.setLabel(-95,3,"Resize toolbox:");
-	this.applyChangesToMultiButton = new button(
-		100, 35, 
-		this.resizeMultiToolBox,
-		'<g>'+
+	this.applyChangesToSelfButton = new button(
+		0, 25, 
+		{ obj: this, method: 'resizeSelf' },
+		'<g transform="translate(90,0)">'+
 		'	<rect x="-90" width="180" height="18" fill="lightslategray"/>'+
-		'	<text y="13" text-anchor="middle">Apply changes to this toolbox</text>'+
+		'	<text y="2" dominant-baseline="hanging" text-anchor="middle">Apply changes to this toolbox</text>'+
 		'	<rect x="-90" width="180" height="18" opacity="0"/>'+
 		'</g>',
 		null,
-		'<g>'+
-		'	<rect x="-90" y="-1" width="180" height="18" fill="aquamarine"/>'+
-		'	<text y="13" text-anchor="middle">Apply changes to this toolbox</text>'+
+		'<g transform="translate(90,0)">'+
+		'	<rect x="-90" width="180" height="18" fill="aquamarine"/>'+
+		'	<text y="2" dominant-baseline="hanging" text-anchor="middle">Apply changes to this toolbox</text>'+
 		'	<rect x="-90" width="180" height="18" opacity="0"/>'+
 		'</g>'
 	);
 	this.saveChangesButton = new button(
-		100, 65, 
-		this.savePreferences,
-		'<g>'+
+		40, 55, 
+		{ obj: this, method: 'savePreferences' },
+		'<g transform="translate(50,0)">'+
 		'	<rect x="-50" width="100" height="18" fill="lightslategray"/>'+
-		'	<text y="13" text-anchor="middle">Save changes</text>'+
+		'	<text y="2" dominant-baseline="hanging" text-anchor="middle">Save changes</text>'+
 		'	<rect x="-50" width="100" height="18" opacity="0"/>'+
 		'</g>',
 		null,
-		'<g>'+
+		'<g transform="translate(50,0)">'+
 		'	<rect x="-50" width="100" height="18" fill="aquamarine"/>'+
-		'	<text y="13" text-anchor="middle">Save changes</text>'+
+		'	<text y="2" dominant-baseline="hanging" text-anchor="middle">Save changes</text>'+
 		'	<rect x="-50" width="100" height="18" opacity="0"/>'+
 		'</g>'
 	);
 
-	this.toolBoxSizeSlider.realize( this.root );
-	this.applyChangesToMultiButton.realize( this.root );
-	this.saveChangesButton.realize( this.root );
+	this.toolBoxSizeSlider.realize( this.displayContent );
+	this.applyChangesToSelfButton.realize( this.displayContent );
+	this.saveChangesButton.realize( this.displayContent );
 	
-	return this.root;
-}
+	var translate = 'translate( '+ toolBox.prototype.padding + ', ' + toolBox.prototype.padding + ')';
+	this.displayContent.setAttribute( 'transform', translate );
 
-/********************************************************************************************/
-/********************************************************************************************/
-/************************** Functions without safety nets ***********************************/
-/********************************************************************************************/
-/********************************************************************************************/
+	return this.displayContent;
+};
 
-/*****
+ViewerPreferences.prototype.setWindowControllers = function(windowControllers) {
+	this.windowControllers   = windowControllers;
+};
 
-	init
-		
-	tested
+/********************************************************************************************
+                            Private Functions
+********************************************************************************************/
 
-*****/
+ViewerPreferences.prototype.init = function(imageControlToolbox) {
+	this.initialized         = true;
+	this.imageControlToolbox = imageControlToolbox;
+};
 
-ViewerPreferences.prototype.init = function(resizeToolBox, resizeMultiToolBox, savePreferences) {
-	this.resizeToolBox      = resizeToolBox;
-	this.resizeMultiToolBox = resizeMultiToolBox;
-	this.savePreferences    = savePreferences;
-}
+ViewerPreferences.prototype.resizeToolboxes = function( scale, applyToAll ) {
+	this.toolBoxSizeSlider.setValue(scale);
+	this.imageControlToolbox.setScale(1 + scale/ViewerPreferences.ScaleSliderWidth );
+	for( i in this.windowControllers ) {
+		if( applyToAll || this.windowControllers[i].toolBox != this.toolBox ) {
+			this.windowControllers[i].toolBox.setScale(1 + scale/ViewerPreferences.ScaleSliderWidth );
+		}
+	}
+};
+
+
+ViewerPreferences.prototype.resizeSelf = function() {
+	scale = 1 + this.toolBoxSizeSlider.getValue()/ViewerPreferences.ScaleSliderWidth;
+	this.toolBox.setScale(scale);
+};
+
+
+ViewerPreferences.prototype.savePreferences = function(  ) {
+	var tmpImg;
+	tmpImg = svgDocument.createElementNS(svgns,"image");
+	tmpImg.setAttribute("width",0);
+	tmpImg.setAttribute("height",0);
+	// The purpose of unique is to bypass any image caching
+	var unique  = Math.random();
+	var imageURL = SaveDisplayCGI_URL + 
+		'&toolBoxScale=' + controlToolBox.getScale() +
+		"&Unique=" + unique;
+	tmpImg.setAttributeNS(xlinkns, "xlink:href",imageURL);
+
+	this.controlsRoot.appendChild(tmpImg);
+	this.controlsRoot.removeChild(tmpImg);
+};
