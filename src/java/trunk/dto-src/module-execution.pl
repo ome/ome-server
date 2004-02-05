e369e24c09ba1a54eab5b3a63ba05d20ef1d72b0
Doug Creager <dcreager@alum.mit.edu>
2004-02-05 00:10:15 +0000

    These little babies automatically generate the DTOs for the core database objects
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@8473 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/java/trunk/dto-src/module-execution.pl
@@ -0,0 +1,60 @@
+# There is no shebang line b/c this file should be eval'ed.
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Written by:  Douglas Creager <dcreager@alum.mit.edu>
+#-------------------------------------------------------------------------------
+
+# Contains the DTO description for module executions
+
+%class_description = (
+                      Package => 'org.openmicroscopy.ds.dto',
+                      Class   => 'ModuleExecution',
+
+                      ImportPackages =>
+                      {
+                       # None needed
+                      },
+
+                      Fields  =>
+                      [
+                       ID            => ['int'],
+                       Module        => ['Module'],
+                       Virtual       => ['boolean','virtual_mex'],
+                       Dependence    => ['String'],
+                       Dataset       => ['Dataset'],
+                       Image         => ['Image'],
+                       IteratorTag   => ['String','iterator_tag',],
+                       NewFeatureTag => ['String','new_feature_tag'],
+                       InputTag      => ['String','input_tag'],
+                       Timestamp     => ['String'],
+                       TotalTime     => ['float','total_time'],
+                       Status        => ['String'],
+                       ErrorMessage  => ['String','error_message'],
+                       Inputs        => ['List:ActualInput'],
+                      ],
+                     );
