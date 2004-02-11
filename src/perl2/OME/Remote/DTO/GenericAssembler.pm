007ac0234e08c0df1ed370036cb79b2be692e9c7
Doug Creager <dcreager@alum.mit.edu>
2004-02-11 23:47:24 +0000

    Generic facade and assembler; used by the DataFactory in Java, maybe also by you?
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3367 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Remote/DTO/GenericAssembler.pm
@@ -0,0 +1,130 @@
+# OME/Remote/DTO/GenericAssembler.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Douglas Creager <dcreager@alum.mit.edu>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Remote::DTO::GenericAssembler;
+use OME;
+our $VERSION = $OME::VERSION;
+
+our $SHOW_ASSEMBLY = 0;
+
+use OME::Remote::DTO;
+
+=head1 NAME
+
+OME::Remote::DTO::GenericAssembler - routines for creating/parsing
+arbitrary DTO's from internal DBObjects
+
+=cut
+
+sub __massageFieldsWanted ($) {
+    my $fields_wanted = shift;
+
+    # We need to massage the $fields_wanted hash to make it useful to
+    # the __makeDTO helper method.
+
+    my %massaged;
+
+    foreach my $key (%$fields_wanted) {
+        if ($key eq ".") {
+            $massaged{""} = $fields_wanted->{$key};
+        } else {
+            $massaged{".${key}"} = $fields_wanted->{$key};
+        }
+    }
+
+    return \%massaged;
+}
+
+sub makeDTO {
+    my ($proto,$object,$fields_wanted) = @_;
+    $fields_wanted = __massageFieldsWanted($fields_wanted);
+    return __genericDTO("",$object,$fields_wanted);
+}
+
+sub makeDTOList {
+    my ($proto,$object_list,$fields_wanted) = @_;
+    $fields_wanted = __massageFieldsWanted($fields_wanted);
+    my @dto_list;
+
+    foreach my $object (@$object_list) {
+        push @dto_list, __genericDTO("",$object,$fields_wanted);
+    }
+    return \@dto_list;
+}
+
+sub __genericDTO ($$$) {
+    my ($prefix,$object,$fields_wanted) = @_;
+
+    print STDERR "$prefix $object\n" if $SHOW_ASSEMBLY;
+
+    my $columns = $fields_wanted->{$prefix};
+    die "Fields not specified for '$prefix' element"
+      unless (defined $columns) && (ref($columns) eq 'ARRAY');
+    my $dto = __makeHash($object,undef,$columns);
+
+    foreach my $column (@$columns) {
+        print STDERR "  $column ",ref($dto->{$column}),"\n"
+          if $SHOW_ASSEMBLY;
+
+        my $type = $object->getColumnType($column);
+        # __makeHash will have already ensured that each column exists
+
+        if ($type eq 'has-one') {
+            my $ref_object = $dto->{$column};
+            $dto->{$column} = __genericDTO("${prefix}.${column}",
+                                           $ref_object,$fields_wanted);
+        } elsif ($type eq 'has-many') {
+            foreach my $ref_object (@{$dto->{$column}}) {
+                $ref_object = __genericDTO("${prefix}.${column}",
+                                           $ref_object,$fields_wanted);
+            }
+        }
+    }
+
+    print STDERR "/$prefix\n"
+      if $SHOW_ASSEMBLY;
+
+    return $dto;
+}
+
+1;
+
+=head1 AUTHOR
+
+Douglas Creager (dcreager@alum.mit.edu)
+
+=cut
---
+++ src/perl2/OME/Remote/Facades/GenericFacade.pm
@@ -0,0 +1,166 @@
+# OME/Remote/Facades/GenericFacade.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Douglas Creager <dcreager@alum.mit.edu>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Remote::Facades::GenericFacade;
+use OME;
+our $VERSION = $OME::VERSION;
+
+use OME::Session;
+use OME::Factory;
+
+use OME::Remote::DTO::GenericAssembler;
+
+our %DATA_CLASSES =
+  (
+   Project           => 'OME::Project',
+   Dataset           => 'OME::Dataset',
+   Image             => 'OME::Image',
+   Feature           => 'OME::Feature',
+   DataTable         => 'OME::DataTable',
+   DataColumn        => 'OME::DataTable::Column',
+   SemanticType      => 'OME::SemanticType',
+   SemanticElement   => 'OME::SemanticType::Element',
+   LookupTable       => 'OME::LookupTable',
+   LookupTableEntry  => 'OME::LookupTable::Entry',
+   Module            => 'OME::Module',
+   FormalInput       => 'OME::Module::FormalInput',
+   FormalOutput      => 'OME::Module::FormalOutput',
+   ModuleCategory    => 'OME::Module::Category',
+   AnalysisChain     => 'OME::AnalysisChain',
+   AnalysisNode      => 'OME::AnalysisChain::Node',
+   AnalysisLink      => 'OME::AnalysisChain::Link',
+   AnalysisPath      => 'OME::AnalysisPath',
+   AnalysisPathEntry => 'OME::AnalysisPath::Map',
+   ModuleExecution   => 'OME::ModuleExecution',
+   ActualInput       => 'OME::ModuleExecution::ActualInput',
+   NodeExecution     => 'OME::AnalysisChainExecution::NodeExecution',
+   ChainExecution    => 'OME::AnalysisChainExecution'
+  );
+
+=head1 NAME
+
+OME::Remote::Facades::GenericFacade - remote facade methods for
+retrieving arbitrary DBObjects and Attributes
+
+=cut
+
+sub countObjects {
+    my ($proto,$object_type,$criteria) = @_;
+
+    my $class_name = $DATA_CLASSES{$object_type};
+    die "Unknown object type" unless defined $class_name;
+
+    OME::Factory->__checkClass($class_name);
+    my $factory = OME::Session->instance()->Factory();
+
+    die "Criteria must be a hash, not $criteria"
+      if (defined $criteria) && (ref($criteria) ne 'HASH');
+
+    return $factory->countObjects($class_name,$criteria);
+}
+
+sub loadObject {
+    my ($proto,$object_type,$id,$fields_wanted) = @_;
+
+    my $class_name = $DATA_CLASSES{$object_type};
+    die "Unknown object type" unless defined $class_name;
+
+    OME::Factory->__checkClass($class_name);
+    my $factory = OME::Session->instance()->Factory();
+
+    die "Fields wanted must be a hash, not $fields_wanted"
+      if (defined $fields_wanted) && (ref($fields_wanted) ne 'HASH');
+
+    my $result = $factory->loadObject($class_name,$id);
+    my $dto = OME::Remote::DTO::GenericAssembler->
+      makeDTO($result,$fields_wanted);
+
+    return $dto;
+}
+
+sub retrieveObject {
+    my ($proto,$object_type,$criteria,$fields_wanted) = @_;
+
+    my $class_name = $DATA_CLASSES{$object_type};
+    die "Unknown object type" unless defined $class_name;
+
+    OME::Factory->__checkClass($class_name);
+    my $factory = OME::Session->instance()->Factory();
+
+    die "Criteria must be a hash, not $criteria"
+      if (defined $criteria) && (ref($criteria) ne 'HASH');
+
+    die "Fields wanted must be a hash, not $fields_wanted"
+      if (defined $fields_wanted) && (ref($fields_wanted) ne 'HASH');
+
+    my $result = $factory->findObject($class_name,$criteria);
+    my $dto = OME::Remote::DTO::GenericAssembler->
+      makeDTO($result,$fields_wanted);
+
+    return $dto;
+}
+
+sub retrieveObjects {
+    my ($proto,$object_type,$criteria,$fields_wanted) = @_;
+
+    my $class_name = $DATA_CLASSES{$object_type};
+    die "Unknown object type" unless defined $class_name;
+
+    OME::Factory->__checkClass($class_name);
+    my $factory = OME::Session->instance()->Factory();
+
+    die "Criteria must be a hash, not $criteria"
+      if (defined $criteria) && (ref($criteria) ne 'HASH');
+
+    die "Fields wanted must be a hash, not $fields_wanted"
+      if (defined $fields_wanted) && (ref($fields_wanted) ne 'HASH');
+
+    my @result = $factory->findObjects($class_name,$criteria);
+    my $dtos = OME::Remote::DTO::GenericAssembler->
+      makeDTOList(\@result,$fields_wanted);
+
+    return $dtos;
+}
+
+1;
+
+=head1 AUTHOR
+
+Douglas Creager (dcreager@alum.mit.edu)
+
+=cut
+
