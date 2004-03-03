fdcab439fc05adad4d8b2552121f02af7b5b6401
Josiah Johnston <siah@nih.gov>
2004-03-03 20:10:57 +0000

    separated subclasses into separate files
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3607 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/ModuleExecution/SemanticTypeOutput.pm
@@ -0,0 +1,77 @@
+# OME/ModuleExecution/SemanticTypeOutput.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Douglas Creager <dcreager@alum.mit.edu>
+#
+#-------------------------------------------------------------------------------
+
+package OME::ModuleExecution::SemanticTypeOutput;
+
+use strict;
+use OME;
+our $VERSION = $OME::VERSION;
+
+use OME::DBObject;
+use base qw(OME::DBObject);
+
+__PACKAGE__->newClass();
+__PACKAGE__->setDefaultTable('semantic_type_outputs');
+__PACKAGE__->setSequence('semantic_type_output_seq');
+__PACKAGE__->addPrimaryKey('semantic_type_output_id');
+__PACKAGE__->addColumn(module_execution_id => 'module_execution_id');
+__PACKAGE__->addColumn(module_execution => 'module_execution_id',
+                       'OME::ModuleExecution',
+                       {
+                        SQLType => 'integer',
+                        NotNull => 1,
+                        ForeignKey => 'module_executions',
+                       });
+__PACKAGE__->addColumn(semantic_type_id => 'semantic_type_id');
+__PACKAGE__->addColumn(semantic_type => 'semantic_type_id',
+                       'OME::SemanticType',
+                       {
+                        SQLType => 'integer',
+                        NotNull => 1,
+                        ForeignKey => 'module_executions',
+                       });
+
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Douglas Creager <dcreager@alum.mit.edu>,
+Open Microscopy Environment, MIT
+
+=cut
---
+++ src/perl2/OME/ModuleExecution/VirtualMEXMap.pm
@@ -0,0 +1,74 @@
+# OME/ModuleExecution/VirtualMEXMap.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Douglas Creager <dcreager@alum.mit.edu>
+#
+#-------------------------------------------------------------------------------
+
+package OME::ModuleExecution::VirtualMEXMap;
+
+use strict;
+use OME;
+our $VERSION = $OME::VERSION;
+
+use OME::DBObject;
+use base qw(OME::DBObject);
+
+__PACKAGE__->newClass();
+__PACKAGE__->setDefaultTable('virtual_mex_map');
+__PACKAGE__->addColumn(module_execution_id => 'module_execution_id');
+__PACKAGE__->addColumn(module_execution => 'module_execution_id',
+                       'OME::ModuleExecution',
+                       {
+                        SQLType => 'integer',
+                        NotNull => 1,
+                        Indexed => 1,
+                        ForeignKey => 'module_executions',
+                       });
+__PACKAGE__->addColumn(['attribute_id','attribute'] => 'attribute_id',
+                       {
+                        SQLType => 'integer',
+                        NotNull => 1,
+                        Indexed => 1,
+                       });
+
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Douglas Creager <dcreager@alum.mit.edu>,
+Open Microscopy Environment, MIT
+
+=cut
