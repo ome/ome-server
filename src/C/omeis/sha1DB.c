429caba8c747fa79b988bcb455c77bdcf55f673f
Ilya Goldberg <igg@nih.gov>
2004-03-07 03:40:14 +0000

    replaced sha1DB code with placeholder code
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3655 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/C/omeis/sha1DB.c
+++ src/C/omeis/sha1DB.c
@@ -1,19 +1,41 @@
-/*
-  Binary Insertion Sort
-  
-  Record        - the index file is made up of contiguous Records
-  Record.digest - the digest of the Record  - DIGEST_LENGTH bytes
-  Record.ID     - the ID of the Record      - ID_LENGTH bytes
-  
-  mmap
-  nRecords
-  
-  insert         - wether to actually insert the Record.
-    The mmap must have a zero record at int(nRecords/2) if insert is true.
-    The mmap will have a zero record at int(nRecords/2) when it returns.
-  
-*/
+/*------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+/*------------------------------------------------------------------------------
+ *
+ * Written by:	Ilya G. Goldberg <igg@nih.gov>   3/2004
+ * 
+ *------------------------------------------------------------------------------
+ */
 
+/*
+  This is some place-holder code for a B-Tree implementation to do reverse lookups
+  SHA1 -> File/PixelsID
+  
+  Don't mind the noise with the parameters - this is just to avoid unused parameter warnings.
+*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <limits.h>
@@ -26,50 +48,27 @@
 
 
 DB *sha1DB_open (const char *file) {
-DB *myDB;
-
-/* O_EXLOCK is a BSD extension */
-#ifdef __DARWIN__
-	myDB = dbopen (file,O_CREAT|O_RDWR|O_EXLOCK, 0600, DB_BTREE, NULL);
-#else
-	myDB = dbopen (file,O_CREAT|O_RDWR, 0600, DB_BTREE, NULL);
-#endif
-	
-	return (myDB);
+char foo = *file;
+	return (1);
 }
 
 int sha1DB_close (DB *myDB) {
-int err;
-	err = (myDB->close)(myDB);
-	free (myDB);
-	return (err);
+	myDB = 0;
+	return (0);
 }
 
 OID sha1DB_get (DB *myDB, unsigned char *md_value) {
-DBT key, value;
-OID theOID;
-
-	key.size = OME_DIGEST_LENGTH;
-	key.data = (void *)md_value;
-	value.size = sizeof (OID);
-	value.data = (void *) &theOID;
-
-	if ( ((myDB->get)(myDB, &key, &value, 0)) == 0)
-		return (* ( (OID *)(value.data) ) );
-	else
-		return ((OID)0);
+char foo = *md_value;
+	return (0);
 }
 
 
 int sha1DB_put (DB *myDB, unsigned char *md_value, OID theOID) {
-DBT key, value;
+char foo = *md_value;
+char OID = 0;
+	myDB = 0;
 
-	key.size = OME_DIGEST_LENGTH;
-	key.data = (void *)md_value;
-	value.size = sizeof (OID);
-	value.data = (void *) &theOID;
-
-	return ((myDB->put)(myDB, &key, &value, R_NOOVERWRITE));
+	return (0);
 }
 
 
--- src/C/omeis/sha1DB.h
+++ src/C/omeis/sha1DB.h
@@ -37,14 +37,14 @@
 #ifndef sha1DB_h
 #define sha1DB_h
 
-#include <sys/types.h>
-
-/* BSD doesn't ship newer versions of Berkeley DB by default */
-#ifdef __DARWIN__
+/*
+ This is commented out until we work out issues with db.h
 #include <db.h>
-#else
-#include <db_185.h>
-#endif
+Once they're worked out, remove:
+typedef void DB.
+*/
+typedef void DB;
+
 #include "digest.h"
 #include "repository.h"
 
