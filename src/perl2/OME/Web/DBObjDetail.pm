0b08a58183aee1b40c11c07ab1e581b67af3f47d
Josiah Johnston <siah@nih.gov>
2004-03-09 22:10:59 +0000

    show detailed info about an object
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3699 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Web/DBObjDetail.pm
@@ -0,0 +1,116 @@
+# OME/Web/ObjectDetail.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Josiah Johnston
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Web::ObjectDetail;
+
+#*********
+#********* INCLUDES
+#*********
+
+use strict;
+use vars qw($VERSION);
+use CGI;
+use Log::Agent;
+
+use OME;
+use OME::Web::RenderData;
+
+#*********
+#********* GLOBALS AND DEFINES
+#*********
+
+$VERSION = $OME::VERSION;
+use base qw(OME::Web);
+
+#*********
+#********* PUBLIC METHODS
+#*********
+
+sub new {
+	my $proto = shift;
+	my $class = ref($proto) || $proto;
+	my $self  = $class->SUPER::new(@_);
+
+	return $self;
+}
+
+sub getMenuBuilder { return undef }  # No menu
+
+sub getHeaderBuilder { return undef }  # No header
+
+sub getPageTitle {
+	my $self = shift;
+	my $q    = $self->CGI();
+	my $id   = $q->param( 'id' );
+	
+    return "Object Detail";
+}
+
+sub getPageBody {
+	my $self = shift;
+	my $q    = $self->CGI();
+	my $factory = $self->Session()->Factory();
+	
+	my $type = $q->param( 'Type' )
+		or die "Type not specified";
+	my $id   = $q->param( 'id' )
+		or die "ID not specified";
+	my $object = $factory->loadObject( $type, $id )
+		or die "Could not load $type, id=$id";
+
+	my $html;
+
+	my @fieldNames = OME::Web::RenderData->getAllFieldNames( $object );
+	my %labels  = OME::Web::RenderData->getFieldLabels( $object, \@fieldNames );
+	my %record  = OME::Web::RenderData->renderSingle( $object, 'html', \@fieldNames );
+	
+	$html .= $q->table(
+		$q->caption( $type ),
+		map(
+			$q->Tr( 
+				$q->td( { align => 'left' }, $labels{ $_ } ),
+				$q->td( { align => 'right' }, $record{ $_ } ) 
+			),
+			@fieldNames
+		)
+	);
+
+	return ('HTML', $html);
+}
+
+
+1;
