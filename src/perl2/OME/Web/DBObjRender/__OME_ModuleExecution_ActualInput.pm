4f1e376561628060ff64df23d71c09163a7cb924
Josiah Johnston <siah@nih.gov>
2004-03-19 21:25:07 +0000

    more specialized renderers
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@3827 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Web/DBObjRender/__OME_ModuleExecution_ActualInput.pm
@@ -0,0 +1,102 @@
+# OME/Web/DBObjRender/__OME_ModuleExecution_ActualInput.pm
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#		Massachusetts Institute of Technology,
+#		National Institutes of Health,
+#		University of Dundee
+#
+#
+#
+#	 This library is free software; you can redistribute it and/or
+#	 modify it under the terms of the GNU Lesser General Public
+#	 License as published by the Free Software Foundation; either
+#	 version 2.1 of the License, or (at your option) any later version.
+#
+#	 This library is distributed in the hope that it will be useful,
+#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	 Lesser General Public License for more details.
+#
+#	 You should have received a copy of the GNU Lesser General Public
+#	 License along with this library; if not, write to the Free Software
+#	 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+#
+# Written by:  
+#	Josiah Johnston <siah@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Web::DBObjRender::__OME_ModuleExecution_ActualInput;
+
+=pod
+
+=head1 NAME
+
+OME::Web::DBObjRender::__OME_ModuleExecution_ActualInput -
+Specialized rendering for OME::ModuleExecution::ActualInput
+
+=head1 DESCRIPTION
+
+Provides custom behavior for rendering an OME::ModuleExecution::ActualInput
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use vars qw($VERSION);
+use OME;
+use OME::Session;
+use base qw(OME::Web::DBObjRender);
+
+# Class data
+__PACKAGE__->_fieldLabels( {
+	'id'             => "ID",
+});
+__PACKAGE__->_fieldNames( [
+	'id',
+	'module_execution',
+	'formal_input',
+	'input_module_execution',
+] ) ;
+__PACKAGE__->_allFieldNames( [
+	@{__PACKAGE__->_fieldNames() },
+] ) ;
+
+sub getObjectLabel {
+	my ($proto,$obj,$format) = @_;
+
+	return $obj->formal_input()->name()
+		if( $obj->formal_input() );
+	return $obj->id();
+}
+
+=head2 getRelationAccessors
+
+DBObject methods + attributes
+
+=cut
+
+sub getRelationAccessors {
+	my ($proto,$obj) = @_;
+
+	my $relation_accessors = $obj->getPublishedManyRefs();
+	my ($package_name, $common_name, $formal_name, $ST) = OME::Web->_loadTypeAndGetInfo( $obj->formal_input()->semantic_type );
+	$relation_accessors->{attributes} = $formal_name;
+	return %$relation_accessors if wantarray;
+	return $relation_accessors;
+}
+
+=head1 Author
+
+Josiah Johnston <siah@nih.gov>
+
+=cut
+
+1;
