b9d22a8b52be36ba73a071a19b10e3cdef2d621d
Josiah Johnston <siah@nih.gov>
2004-04-06 17:57:33 +0000

    Module search fields are now dropdown lists
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4048 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Web/DBObjRender/__OME_Module.pm
@@ -0,0 +1,95 @@
+# OME/Web/DBObjRender/__OME_Module.pm
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#		Massachusetts Institute of Technology,
+#		National Institutes of Health,
+#		University of Dundee
+#
+#
+#
+#	 This library is free software; you can redistribute it and/or
+#	 modify it under the terms of the GNU Lesser General Public
+#	 License as published by the Free Software Foundation; either
+#	 version 2.1 of the License, or (at your option) any later version.
+#
+#	 This library is distributed in the hope that it will be useful,
+#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	 Lesser General Public License for more details.
+#
+#	 You should have received a copy of the GNU Lesser General Public
+#	 License along with this library; if not, write to the Free Software
+#	 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:  
+#	Josiah Johnston <siah@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Web::DBObjRender::__OME_Module;
+
+=pod
+
+=head1 NAME
+
+OME::Web::DBObjRender::__OME_Module - Specialized rendering for Modules
+
+=head1 DESCRIPTION
+
+Provides custom behavior for rendering a Module
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use OME;
+our $VERSION = $OME::VERSION;
+
+use OME::Session;
+use base qw(OME::Web::DBObjRender);
+
+=head2 getRefSearchField
+
+returns a dropdown list of Experimenter names valued by id.
+
+=cut
+
+sub getRefSearchField {
+	my ($proto, $from_type, $to_type, $accessor_to_type) = @_;
+	my (undef, undef, $from_formal_name) = OME::Web->_loadTypeAndGetInfo( $from_type );
+	my $factory = OME::Session->instance()->Factory();
+
+	# Modules list
+	my @modules = $factory->findObjects( "OME::Module" );
+	my %module_names = map{ $_->id() => $_->name() } @modules;
+	my $module_order = [ '', sort( { $module_names{$a} cmp $module_names{$b} } keys( %module_names ) ) ];
+	$module_names{''} = 'All';
+
+	my $q = new CGI;
+
+	return $q->popup_menu( 
+		-name	=> $from_formal_name."_".$accessor_to_type,
+		'-values' => $module_order,
+		-labels	 => \%module_names
+	);
+
+}
+
+
+=head1 Author
+
+Josiah Johnston <siah@nih.gov>
+
+=cut
+
+1;
