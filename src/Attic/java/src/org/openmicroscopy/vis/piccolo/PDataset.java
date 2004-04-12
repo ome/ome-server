adde0a689a3b267684af89e9798e1f0e03ac7b63
Harry Hochheiser <hsh@nih.gov>
2004-04-12 14:30:56 +0000

    revised many little details associated with addition of chain labels on dataset boxes
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4109 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/piccolo/PDataset.java
+++ src/Attic/java/src/org/openmicroscopy/vis/piccolo/PDataset.java
@@ -39,12 +39,14 @@
 
 package org.openmicroscopy.vis.piccolo;
 
+import org.openmicroscopy.vis.chains.SelectionState;
 import org.openmicroscopy.vis.ome.CDataset;
 import org.openmicroscopy.vis.ome.CImage;
+
 import org.openmicroscopy.vis.ome.Connection;
 import edu.umd.cs.piccolo.nodes.PText;
-import java.util.Vector;
-import java.util.List;
+import edu.umd.cs.piccolo.util.PBounds;
+import java.util.Collection;
 import java.util.Iterator;
 
 /** 
@@ -69,25 +71,21 @@ public class PDataset extends PGenericBox {
 	private float maxHeight = 0;
 	private PText nameLabel;
 	
-	public PDataset(CDataset dataset,Connection connection) {
+	private double LABEL_SCALE=.4;
+	
+	public PDataset(CDataset dataset,Connection connection,
+				SelectionState selectionState) {
 		super();
 		this.dataset = dataset;
 		this.connection = connection;
-		layoutImages();
-	}
-	
-	public PDataset(float x,float y,CDataset dataset,Connection connection) {
-		super(x,y);
-		this.dataset = dataset;
-		this.connection = connection;
-		layoutImages();
+		layoutImages(selectionState);
 	}
 
-	private void layoutImages() {
+	private void layoutImages(SelectionState selectionState) {
 		
 		//	draw label
 		//System.err.println("laying out dataset "+dataset.getName());
-		 nameLabel = new PText(dataset.getName());
+		 nameLabel = new PText(dataset.getLabel());
 		 addChild(nameLabel);
 		 int sz = dataset.getImageCount();
 
@@ -96,11 +94,11 @@ public class PDataset extends PGenericBox {
 			displayDatasetSizeText(sz);
 			return;
 		}
-		List images = dataset.getCachedImages(connection);
+		Collection images = dataset.getCachedImages(connection);
 		Iterator iter = images.iterator();
 		maxHeight = 0;
 		float maxWidth = 0;
-		Vector nodes = new Vector();
+		//Vector nodes = new Vector();
 		
 		nameLabel.setOffset(x,y);
 		y+= nameLabel.getHeight()+VGAP;
@@ -118,7 +116,6 @@ public class PDataset extends PGenericBox {
 				maxHeight = height;
 			if (width > maxWidth) 
 				maxWidth = width;
-			nodes.add(thumb);
 		}
 		
 	//	System.err.println("laying out images. width is "+maxWidth);
@@ -126,18 +123,24 @@ public class PDataset extends PGenericBox {
 		// space them
 		maxHeight += VGAP;
 		maxWidth+= HGAP;
-		iter = nodes.iterator();
-		int rowSz = (int) Math.sqrt(nodes.size());
+
+		iter = getChildrenIterator();
+		int rowSz= (int) Math.sqrt(getChildrenCount());
+		
 		int i =0;
 		double maxRowWidth=0;
 		while (iter.hasNext()) {
-			PThumbnail thumb = (PThumbnail) iter.next();
+			Object obj = iter.next();
+			if (!(obj instanceof PThumbnail))
+				continue;
+			
+			//PThumbnail thumb = (PThumbnail) iter.next();
+			PThumbnail thumb = (PThumbnail) obj;
 		
 			thumb.setOffset(x,y);
 			x += maxWidth;
 			if (x > maxRowWidth)
-				maxRowWidth = x;
-			
+				maxRowWidth = x; 
 			if (i++ >= rowSz) {
 				// if we're at the end of the row by count and it's longer
 				// than previous rows (don't want to stop a row
@@ -150,12 +153,39 @@ public class PDataset extends PGenericBox {
 		// roll back y if we were just about to start a new row
 		if (x== HGAP)
 			y-=maxHeight;
-			
-		height =(float)(y+maxHeight-VGAP);
 		
-		maxRowWidth -= HGAP; // leave of the last horizontal space between nodes
+		y +=maxHeight-VGAP; // move y ahead. to next row.
+		
+		// adjust width
+		maxRowWidth -= HGAP; // leave off the last horizontal space 
 		if (maxRowWidth < nameLabel.getWidth())
 			maxRowWidth = nameLabel.getWidth();
+		// insert chains, if any.
+		Collection chains = dataset.getChains(connection);
+		
+		if (chains.size() > 0) {
+			PText chainLabel =new PText("Executions: ");
+			chainLabel.setFont(PConstants.LABEL_FONT);
+			chainLabel.setOffset(x+HGAP,y);
+			chainLabel.setPickable(false);
+			chainLabel.setScale(LABEL_SCALE);
+			addChild(chainLabel);
+			PBounds clbounds = chainLabel.getGlobalFullBounds();
+			//y+=clbounds.getHeight()+VGAP;
+			double chainWidth = maxRowWidth - (clbounds.getWidth()+2*HGAP);
+			PChainLabels chainLabels = new
+				PChainLabels(chains,chainWidth,selectionState);
+			chainLabels.setOffset(x+clbounds.getWidth()+2*HGAP,y);
+			addChild(chainLabels);
+			PBounds b2 =chainLabels.getGlobalFullBounds();
+			y+= b2.getHeight()+VGAP;
+			if (maxRowWidth < clbounds.getWidth()+2*HGAP+b2.getWidth())
+				maxRowWidth = clbounds.getWidth()+2*HGAP+b2.getWidth();
+		}
+		
+		height =(float)y-VGAP;
+		
+	
 		setExtent(maxRowWidth+PConstants.SMALL_BORDER,
 			height+PConstants.SMALL_BORDER);
 	}
@@ -170,5 +200,11 @@ public class PDataset extends PGenericBox {
 			width = nameLabel.getWidth();
 		}
 		setExtent(width+2*HGAP,y);
+	}
+	/**
+	 * @return Returns the dataset.
+	 */
+	public CDataset getDataset() {
+		return dataset;
 	}
 }
