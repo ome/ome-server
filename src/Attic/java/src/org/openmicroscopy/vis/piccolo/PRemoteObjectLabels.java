409c08c87596fe94ce71a14bcac0a435caeceea5
Harry Hochheiser <hsh@nih.gov>
2004-04-12 14:32:14 +0000

    superclass for datasetlabels on chains and chain labels on datasets
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4110 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/Attic/java/src/org/openmicroscopy/vis/piccolo/PRemoteObjectLabels.java
@@ -0,0 +1,108 @@
+/*
+ * org.openmicroscopy.vis.piccolo.PRemoteObjectLabels
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003-2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+/*------------------------------------------------------------------------------
+ *
+ * Written by:    Harry Hochheiser <hsh@nih.gov>
+ *
+ *------------------------------------------------------------------------------
+ */
+
+package org.openmicroscopy.vis.piccolo;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.openmicroscopy.remote.RemoteObject;
+import org.openmicroscopy.vis.chains.SelectionState;
+
+
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.util.PBounds;
+
+
+/** 
+ * A parent node for labels that hold the names of RemoteObjects
+ * 
+ * 
+ * @author Harry Hochheiser
+ * @version 2.1
+ * @since OME2.1
+ *
+ */
+
+
+
+public abstract class PRemoteObjectLabels extends PNode {
+	
+	private double x =0;
+	private double y =0;
+	private double HGAP=40;
+	
+	private double width=0;
+	
+	public PRemoteObjectLabels(Collection items,double width,
+			SelectionState selectionState) {
+		super();
+		this.width = width;
+		Iterator iter = items.iterator();
+		RemoteObject ro;
+		
+		while (iter.hasNext()) {
+			//ds = (CDataset) iter.next();
+			ro = (RemoteObject) iter.next();
+			buildLabel(ro,selectionState);
+		}
+	}
+	
+	private void buildLabel(RemoteObject ro,SelectionState selectionState) {
+		PNode p = getNode(ro,selectionState);
+	
+		addChild(p);
+		PBounds b = p.getGlobalFullBounds();
+		double vgap = getVerticalGap();
+		double labelWidth = b.getWidth();
+		
+		// if I exceed desired width and I've already put something on this line
+		// move on to next. if i haven't already put something on this line,
+		// suck up and deal.
+		if (x+labelWidth > width && x> 0) {
+			x =0;
+			y+=b.getHeight()+vgap;
+		}
+		
+		p.setOffset(x,y);
+		x += labelWidth+HGAP;
+	}
+	
+	protected  abstract PNode getNode(RemoteObject ro,
+			SelectionState selectionState);
+	
+	protected abstract double getVerticalGap();
+}
