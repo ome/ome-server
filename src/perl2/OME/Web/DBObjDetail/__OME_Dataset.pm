e98bbc4cb7015774bd12aceed295360111633cc2
Josiah Johnston <siah@nih.gov>
2004-04-23 06:47:56 +0000

    removed most of the code. the Layout is in the template now
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4220 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/DBObjDetail/__OME_Dataset.pm
+++ src/perl2/OME/Web/DBObjDetail/__OME_Dataset.pm
@@ -41,7 +41,7 @@ package OME::Web::DBObjDetail::__OME_Dataset;
 
 =head1 NAME
 
-OME::Web::DBObjDetail::__OME_Dataset - Show detailed information on an Dataset
+OME::Web::DBObjDetail::__OME_Dataset
 
 =head1 DESCRIPTION
 
@@ -57,17 +57,7 @@ use strict;
 use OME;
 our $VERSION = $OME::VERSION;
 
-use CGI;
 use Log::Agent;
-
-use OME::Web::DBObjRender;
-use OME::Web::DBObjTable;
-
-#*********
-#********* GLOBALS AND DEFINES
-#*********
-
-$VERSION = $OME::VERSION;
 use base qw(OME::Web::DBObjDetail);
 
 #*********
@@ -89,96 +79,11 @@ sub new {
 	return $self;
 }
 
-=head2 getDBObjDetail
-
-Makes a table with less whitespace.
-
-=cut
-
-sub getObjDetail {
-	my ($self, $object) = @_;
-
-	my $specializedDetail;
-	return $specializedDetail->getObjDetail( )
-		if( $specializedDetail = $self->__specialize( ) and
-		    ref( $self ) eq __PACKAGE__ );
-
-	my $q = $self->CGI();
-
-	my $title = $q->font( { -class => 'ome_header_title' },
-		OME::Web::DBObjRender->getObjectTitle($object, 'html') );
-
-	my @fieldNames = OME::Web::DBObjRender->getAllFieldNames( $object );
-	my %labels  = OME::Web::DBObjRender->getFieldLabels( $object, \@fieldNames, 'html' );
-	my %record  = OME::Web::DBObjRender->renderSingle( $object, 'html', \@fieldNames );
-	%record = %{ $self->_overrideRecord( \%record ) };
-
-	my $detail .= $q->table( 
-		$q->Tr( [
-			$q->td( { -align => 'left' },
-				$title 
-			),
-			$q->td( { -align => 'left' },
-				join( ', ', map( 
-					$q->span( $labels{ $_ }.': ' ).$record{ $_ },
-					grep( !m/name|description/, @fieldNames )
-				) )
-			)
-		]
-		),
-		$q->Tr( 
-			$q->td( { -align => 'left'},
-				"Name ".$q->textfield( {
-						-name => 'name',
-						-value => $object->name(),
-						-size => 30,
-					}
-				)
-			),
-		),
-		$q->Tr( 
-			$q->td( { -align => 'left'}, 
-				"Description"
-			)
-		),
-		$q->Tr(
-			$q->td( { -align => 'left' },
-				$q->textarea( {
-					-name => 'description',
-					-value => $object->description(),
-					-rows => 3,
-					-columns => 50,
-				} )
-			)
-		),
-		$q->Tr(
-			$q->td( { -align => 'right'}, $q->table( 
-				{
-					-class => 'ome_table',
-					-cellpadding => 3,
-				},
-				$q->Tr($q->td({style => 'background-color: #D1D7DC'}, 
-					$q->a( {
-						class => 'ome_widget',
-						href => "javascript:document.forms['".$self->{ form_name }."'].action.value='SaveChanges'; document.forms['".$self->{ form_name }."'].submit(); return false;",
-					}, 'Save Changes').' | '.
-					$q->a( {
-						class => 'ome_widget',
-						href => "javascript:openRelationships('OME::Dataset', 'OME::Image', " . $object->id() . ");"
-					}, 'Add/Remove Images')
-				) )
-			) )
-		)
-	);
-	
-	return $detail;
-}
 
 sub _takeAction {
 	my $self = shift;
 	my $object = $self->_loadObject();
 	my $q = $self->CGI();
-
 	if( $q->param( 'action' ) eq 'SaveChanges' ) {
 		$object->description( $q->param( 'description' ) );
 		$object->name( $q->param( 'name' ) );
@@ -220,26 +125,6 @@ sub doLayout {
 	return $html;
 }
 
-sub _overrideRecord {
-	my ($self, $record) = @_;
-	my $object = $self->_loadObject();
-	my $q = $self->CGI();
-	
-	$record->{'description'} = $q->textarea( {
-			-name => 'description',
-			-value => $object->description(),
-			-rows => 5,
-			-columns => 30,
-		}
-	);
-	$record->{'name'} = $q->textfield( {
-			-name => 'name',
-			-value => $object->name(),
-			-size => 30,
-		}
-	);
-	return $record;
-}
 
 =head1 Author
 
