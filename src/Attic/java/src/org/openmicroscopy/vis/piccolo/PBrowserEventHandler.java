aeaee333709e15c23b432a2ec6513881fadd2125
Harry Hochheiser <hsh@nih.gov>
2004-05-24 18:17:41 +0000

    clean up code. modify animation stuff to use new calls in PGenericZoomEventHandler
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4470 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/piccolo/PBrowserEventHandler.java
+++ src/Attic/java/src/org/openmicroscopy/vis/piccolo/PBrowserEventHandler.java
@@ -42,12 +42,9 @@
 
 package org.openmicroscopy.vis.piccolo;
 import org.openmicroscopy.vis.chains.SelectionState;
-import org.openmicroscopy.vis.ome.CDataset;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.PNode;
 import edu.umd.cs.piccolo.PCanvas;
-import edu.umd.cs.piccolo.PCamera;
-import edu.umd.cs.piccolo.util.PBounds;
 import java.awt.event.MouseEvent;
 
 /** 
@@ -78,30 +75,28 @@ public class PBrowserEventHandler extends  PGenericZoomEventHandler {
 	public void mouseEntered(PInputEvent e) {
 		PNode n = e.getPickedNode();
 		System.err.println("browser event mouse entered "+n);
-		canvas.clearExecutionList();
 		if (n instanceof PSelectableText) {
 			((PSelectableText) n).setHighlighted(true);
 			if (n instanceof PChainLabelText) {
 				PChainLabelText clt = (PChainLabelText) n;
 				canvas.showExecutionList(clt);
-
 			}
-			e.setHandled(true);
 		} 
-		else if (n instanceof PThumbnail)  {
-			PThumbnail pt = (PThumbnail) n;
-			pt.setHighlighted(true);
-			pt.setZoomingHalo(true);
-			e.setHandled(true);
+		else {
+			canvas.clearExecutionList();
+			if (n instanceof PThumbnail)  {
+				PThumbnail pt = (PThumbnail) n;
+				pt.setHighlightedWithHalo(true);
+			}
+			else if (n instanceof PDataset) {
+				PDataset dn = (PDataset) n;
+				dn.rollover();	
+			}
+			else if (!(n instanceof PThumbnailSelectionHalo))  {// entering anything else means setting selected datset is null
+				SelectionState.getState().setRolloverDataset(null);	 
+			}
 		}
-		else if (n instanceof PDataset) {
-			PDataset dn = (PDataset) n;
-			CDataset c = dn.getDataset();
-			SelectionState.getState().setRolloverDataset(c);
-		}
-		else if (!(n instanceof PThumbnailSelectionHalo))  {// entering anything else means setting selected datset is null
-			SelectionState.getState().setRolloverDataset(null);	 
-		}
+		e.setHandled(true);
 	}
 	
 	public void mouseExited(PInputEvent e) {
@@ -109,14 +104,12 @@ public class PBrowserEventHandler extends  PGenericZoomEventHandler {
 		System.err.println("browser event exited.."+n);
 		if (n instanceof PThumbnail) {
 			PThumbnail pt = (PThumbnail) n;
-			pt.setHighlighted(false);
-			pt.setZoomingHalo(false);
-			e.setHandled(true);
+			pt.setHighlightedWithHalo(false);
 		}
 		else if (n instanceof PSelectableText) {
 			((PSelectableText)n).setHighlighted(false);
-			e.setHandled(true);
 		} 
+		e.setHandled(true);
 	}	
 	
 	public void mouseReleased(PInputEvent e) {
@@ -135,16 +128,13 @@ public class PBrowserEventHandler extends  PGenericZoomEventHandler {
 		PNode node = e.getPickedNode();
 		System.err.println("popup..."+node);
 		if (node instanceof PDataset) {
-			//zooming out, so...
+			System.err.println("popup on dataset...");
 			SelectionState selectionState = SelectionState.getState();	
 			selectionState.setSelectedDataset(null);
 			((PDataset) node).enableHalo();
 		}
 		else if (node instanceof PThumbnail) {
-			PBufferedNode bn = ((PThumbnail) node).getBufferedParentNode();
-			PBounds b = bn.getBufferedBounds();
-			PCamera camera = ((PCanvas) canvas).getCamera();
-			camera.animateViewToCenterBounds(b,true,PConstants.ANIMATION_DELAY);
+			animateToNode(node);
 			PDatasetImagesNode pin = ((PThumbnail) node).getDatasetImagesNode();
 			if (pin != null) 
 				pin.enableHalo();
@@ -161,7 +151,6 @@ public class PBrowserEventHandler extends  PGenericZoomEventHandler {
 			return; 
 		}
 		PNode node = e.getPickedNode();
-		System.err.println("...clicked on browsercnavas.."+node);
 		SelectionState selectionState = SelectionState.getState();
 		if ((e.getModifiers() & MouseEvent.BUTTON1_MASK) 
 			== MouseEvent.BUTTON1_MASK) {
@@ -169,49 +158,30 @@ public class PBrowserEventHandler extends  PGenericZoomEventHandler {
 				//System.err.println("zooming in on dataset");
 				PDataset d = (PDataset) node;
 				selectionState.setSelectedDataset(d.getDataset());
-				PBounds b = d.getBufferedBounds();
-				PCamera camera = ((PCanvas) canvas).getCamera();
-				camera.animateViewToCenterBounds(b,true,PConstants.ANIMATION_DELAY);
-				e.setHandled(true);
+				animateToNode(d);
 			}
-		
+
 			else if (node instanceof PExecutionText) {
-				//System.err.println("clicked on execution text!");
-				e.setHandled(true);
+				//placeholder
 			}
-			else if (node instanceof PCamera || node == 
-				((PCanvas) canvas).getLayer()) {
+			else if (isBackgroundClick(node)) {
+				System.err.println("clicking on layer or camera..");
 				selectionState.setSelectedDataset(null);
-				e.setHandled(true);
 			} else if (node instanceof PThumbnail) {
 				PThumbnail thumb = (PThumbnail)node;
 				if (thumb.isZoomable())
 					super.mouseClicked(e);
 				else { // zoom to halo?
 					System.err.println("zooming in to halo?");
+		
 					PDatasetImagesNode pin = thumb.getDatasetImagesNode();
 					if (pin != null)
 						pin.zoomToHalo(((PCanvas) canvas).getCamera());
 				}
-				e.setHandled(true);
 			}
 			else 
-				super.mouseClicked(e);
+				super.mouseClicked(e);	
 		}
-		else if (e.isControlDown() || ((e.getModifiers() & MouseEvent.BUTTON3_MASK) 
-			== MouseEvent.BUTTON3_MASK)) {
-			System.err.println("right button clicked on "+node);
-		  // right button.
-		  	if (node instanceof PDataset) {
-		  		selectionState.setSelectedDataset(null);
-		  		e.setHandled(true);
-		  	}
-		  	else {
-		  		handlePopup(e);
-		  		e.setHandled(true);
-		  	}
-		}
-		else 
-			e.setHandled(true);
+		e.setHandled(true);
 	}
 }
