e37c9c11c2690d806634f20faf5ace772672674f
Harry Hochheiser <hsh@nih.gov>
2004-05-24 18:20:42 +0000

    add animateTo*** and isBackgroundClick() methods. Move mouseEntered/exited code to PModuleEventHandler
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4473 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/Attic/java/src/org/openmicroscopy/vis/piccolo/PGenericZoomEventHandler.java
+++ src/Attic/java/src/org/openmicroscopy/vis/piccolo/PGenericZoomEventHandler.java
@@ -100,7 +100,6 @@ public class PGenericZoomEventHandler extends  PBasicInputEventHandler {
 	}
 	
 	
-	
 
 	/**
 	 * If the mouse is clicked on a buffered node (either a 
@@ -125,16 +124,11 @@ public class PGenericZoomEventHandler extends  PBasicInputEventHandler {
 				handlePopup(e);	
 			}
 			else if (node instanceof PBufferedNode) {
-				PBufferedNode cBox = (PBufferedNode) node;				
-				PBounds b = cBox.getBufferedBounds();
-				PCamera camera = ((PCanvas) canvas).getCamera();
-				camera.animateViewToCenterBounds(b,true,PConstants.ANIMATION_DELAY);
+				animateToNode(node);
 				e.setHandled(true); 
 			}
-			else if (node instanceof PCamera || node == ((PCanvas) canvas).getLayer()) {
-				PBounds b = canvas.getBufferedBounds();
-				PCamera camera = ((PCanvas) canvas).getCamera();
-				camera.animateViewToCenterBounds(b,true,PConstants.ANIMATION_DELAY);
+			else if (isBackgroundClick(node)) {
+				animateToCanvasBounds();
 				e.setHandled(true);
 			}
 			else
@@ -160,73 +154,6 @@ public class PGenericZoomEventHandler extends  PBasicInputEventHandler {
 	}
 	
 	
-	/**
-	 * When the mouse enters a PFormalParameter or a PModule, set corresponding
-	 * items to be highlighted, according to the following rule:
-	 * 		1) If necessary, Clear anything that had been highlighted 
-	 * 				previously
-	 * 		2) If the node that is entered is a formal parameter, set its
-	 * 				corresponding parameters(parameters it can ink to)
-	 * 				to be highlighted, along with all of the module widgets 
-	 * 				for the containing module. 
-	 * 		3)  if It is a module widget, set all outputs and inputs (from
-	 * 				other modules) that might be linked to this module 
-	 * 				to be highlighted. Also set all instances of this module
-	 * 				to be highlighted.
-	 * 
-	 */
-	public void mouseEntered(PInputEvent e) {
-		PNode node = e.getPickedNode();
-		
-		unhighlightModules();
-		if (node instanceof PFormalParameter) {
-			PFormalParameter param = (PFormalParameter) node;
-			param.setParamsHighlighted(true);
-			PModule pmod = param.getPModule();
-			pmod.setModulesHighlighted(true);
-			e.setHandled(true);
-		}
-		
-		else if (node instanceof PModule) {
-			PModule pmod = (PModule) node;
-			pmod.setAllHighlights(true);
-			e.setHandled(true);
-			lastEntered = pmod;
-		}
-		else {
-			super.mouseEntered(e);
-		}
-	} 
-	
-
-	/**
-	 * When the mouse exits a node, set all of the modules and/or 
-	 * parameters that correspond to no longer be selected. Note that leaving a 
-	 * {@link PFormalParameter} might immediately and directly lead to
-	 * entering a {@link PModule}, so a {@link mouseEntered()} call 
-	 * might immediately follow.
-	 * 
-	 */
-	public void mouseExited(PInputEvent e) {
-		PNode node = e.getPickedNode();
-
-		if (node instanceof PFormalParameter) {
-			PFormalParameter param = (PFormalParameter) node;
-			param.setParamsHighlighted(false);
-			PModule pmod = param.getPModule();
-			pmod.setAllHighlights(false);
-			e.setHandled(true);			
-		}
-		else if (node instanceof PModule) {
-			PModule pmod = (PModule) node;
-			pmod.setAllHighlights(false);
-			e.setHandled(true);
-			lastEntered = null;
-		}
-		else
-			super.mouseExited(e);
-	}
-	
 	
 	
 	/***
@@ -237,16 +164,30 @@ public class PGenericZoomEventHandler extends  PBasicInputEventHandler {
 		PNode node = e.getPickedNode();
 		PNode p = node.getParent();
 		if (p instanceof PBufferedNode) {
-			PBufferedNode bn=(PBufferedNode) p;
-			PBounds b = bn.getBufferedBounds();
-			PCamera camera = ((PCanvas) canvas).getCamera();
-			camera.animateViewToCenterBounds(b,true,PConstants.ANIMATION_DELAY);		
-		} else if (p instanceof PCamera || p == ((PCanvas) canvas).getLayer() ||
-					node instanceof PCamera || node == ((PCanvas) canvas).getLayer()) {
-			PBounds b = canvas.getBufferedBounds();
-			PCamera camera = ((PCanvas) canvas).getCamera();
-			camera.animateViewToCenterBounds(b,true,PConstants.ANIMATION_DELAY);
+			animateToNode(p);		
+		} else if (isBackgroundClick(node) || isBackgroundClick(p)) {
+			animateToCanvasBounds();
 		}
 		e.setHandled(true);
+	}
+
+	public  void animateToBounds(PBounds b) {
+		PCamera camera = ((PCanvas) canvas).getCamera();
+		camera.animateViewToCenterBounds(b,true,PConstants.ANIMATION_DELAY);
+	}
+	
+	protected void animateToCanvasBounds() {
+		animateToBounds(canvas.getBufferedBounds());
+	}
+	
+	protected void animateToNode(PNode node) {
+		if (node instanceof PBufferedNode) {
+			animateToBounds(((PBufferedNode) node).getBufferedBounds());
+		}
+	}
+	
+	protected boolean isBackgroundClick(PNode node) {
+		return (node instanceof PCamera || 
+			node == ((PCanvas) canvas).getLayer());
 	}
  }
