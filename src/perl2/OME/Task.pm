e1a360c73470580432dffff4cd5120d6fce510a0
Ilya Goldberg <igg@nih.gov>
2004-05-30 05:36:53 +0000

    Added task progress API
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4545 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Task.pm
@@ -0,0 +1,289 @@
+# OME/Task.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Ilya Goldberg <igg@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Task;
+
+=head1 NAME
+
+OME::Task - OME Tasks are used to keep track of long-running processes.
+
+=head1 DESCRIPTION
+
+The C<Task> class is used to keep track of long-running tasks.  This DBObject
+allows updating task information independently of any other transactions
+that may be running.
+
+This class is meant to be created, recalled and deleted through the
+L<OME::Tasks::NotificationManager|OME::Tasks::NotificationManager> class.
+	my $task = new OME::Tasks::NotificationManager ($name, $nSteps);
+
+=cut
+
+use strict;
+use OME;
+our $VERSION = $OME::VERSION;
+
+use Carp;
+
+use OME::DBObject;
+use base qw(OME::DBObject);
+
+=head1 FIELDS
+
+Note that using these fields as mutators will not make the values available to other
+processes until storeObject() is called.  Use the provided methods to do immediate
+updates.
+
+=head2 name ()
+
+Acessor/mutator for getting/setting the task's name.  It is bad practice to
+re-set the tasks name after its been created.  Use C<message()> to send messages.
+
+=head2 session_id ()
+
+Acessor for getting the task's associated session ID.
+
+=head2 session ()
+
+Acessor for getting the task's associated L<OME::UserState|OME::UserState> object.
+
+=head2 process_id ()
+
+Acessor for getting the task's process ID (i.e., the operating system's PID).
+
+=head2 state ()
+
+Acessor for getting the task's state.  This is a controlled
+vocabulary, so state is supposed to be 'IN PROGRESS', 'FINISHED', and 'ABORTED'.
+Do not modify the state directly.
+
+=head2 message ()
+
+Acessor/mutator for getting/setting a message for the task.  The messages are not
+cumulative.  Each call replaces the message in the task.
+
+=head2 error ()
+
+Acessor/mutator for getting/setting a error message for the task.
+The error messages are not cumulative.  Each call replaces the error message in the task.
+
+=head2 n_steps ()
+
+Acessor for getting the number of steps in the task.
+
+=head2 last_step ()
+
+Acessor for getting the last completed step in the task.
+
+=head2 t_start ()
+
+Acessor for getting the timestamp when the task started.
+
+=head2 t_stop ()
+
+Acessor for getting the timestamp when the task finished.
+
+=head2 t_last ()
+
+Acessor for getting the timestamp when the previous step in the task finished.
+
+=cut
+
+use OME::Tasks::NotificationManager;
+
+__PACKAGE__->newClass();
+__PACKAGE__->setSequence('task_seq');
+__PACKAGE__->setDefaultTable('tasks');
+__PACKAGE__->addPrimaryKey('task_id');
+__PACKAGE__->addColumn(name => 'name',
+                       {
+                        SQLType => 'varchar(64)',
+                        NotNull => 1,
+                       });
+__PACKAGE__->addColumn(name => 'process_id',
+                       {
+                        SQLType => 'integer',
+                        NotNull => 1,
+                       });
+__PACKAGE__->addColumn('session_id' => 'session_id',
+                       {
+                        SQLType => 'integer',
+                        NotNull => 1,
+                        ForeignKey => 'ome_sessions',
+                       });
+__PACKAGE__->addColumn('session' => 'session_id','OME::UserState');
+__PACKAGE__->addColumn(name => 'state',
+                       {
+                        SQLType => 'varchar(64)',
+                        NotNull => 1,
+                       });
+__PACKAGE__->addColumn(name => 'message',
+                       {
+                        SQLType => 'text',
+                       });
+__PACKAGE__->addColumn(name => 'error',
+                       {
+                        SQLType => 'text',
+                       });
+__PACKAGE__->addColumn(name => 'n_steps',
+                       {
+                        SQLType => 'integer',
+                       });
+__PACKAGE__->addColumn(name => 'last_step',
+                       {
+                        SQLType => 'integer',
+                       });
+__PACKAGE__->addColumn(name => 't_start',
+                       {
+                        SQLType => 'timestamp',
+                        NotNull => 1,
+                       });
+__PACKAGE__->addColumn(name => 't_stop',
+                       {
+                        SQLType => 'timestamp',
+                       });
+__PACKAGE__->addColumn(name => 't_last',
+                       {
+                        SQLType => 'timestamp',
+                       });
+
+
+# Over-ride the DBObject->getFactory() method.
+# We return a special factory here - one that is not tied
+# to a session, and who's DB handle is totally independent
+# of the Session's Factory's DB handle.
+# This is all so that this object can be updated to the DB
+# asynchronously and independently of any transactions that
+# may be going on.
+# This special factory is managed by OME::Tasks::NotificationManager
+# Since this handle is auto-commit, we only need one.
+# Hence the class (not instance) variable to hold the factory.
+sub getFactory {
+print STDERR "Retreiving factory from Task\n";
+	return OME::Tasks::NotificationManager->taskFactory();
+}
+
+=head1 METHODS
+
+=head2 step($stepNum)
+
+Update the step we're on in the task.  If C<$stepNum> is specified, the
+tasks's last_step is set to the specified $stepNum.  Otherwise, the
+last_step is incremented.  The current timestamp is also set for t_last.
+Returns the last completed step ($stepNum).
+
+=cut
+
+sub step {
+	my $self = shift;
+	my $nSteps = shift;
+	$nSteps = $self->last_step()+1 unless $nSteps;
+	$self->last_step($nSteps);
+	$self->t_last('now');
+	$self->storeObject();
+	return $nSteps;
+}
+
+
+=head2 finish()
+
+Signal that the task is finished.  The state is set to 'FINISHED', and the t_stop
+timestamp is set to the current time.  Nothing is done with last_step or t_last.
+
+=cut
+
+sub finish {
+	my $self = shift;
+	$self->t_stop('now');
+	$self->state ('FINISHED');
+	$self->storeObject();
+}
+
+=head2 setMessage()
+
+Set the message for the task.  Unlike the C<message()> field, this will immediately
+write the object to the DB, making the message available to other processes.
+
+=cut
+
+sub setMessage {
+	my $self = shift;
+	my $message;
+	
+	if (@_) {
+		$message = shift;
+		$self->message($message);
+		$self->storeObject();
+	}
+	return $message;
+}
+
+
+=head2 setError()
+
+Set the error message for the task.  Unlike the C<error()> field, this will immediately
+write the object to the DB, making the error message available to other processes.
+
+=cut
+
+sub setError {
+	my $self = shift;
+	my $error;
+	
+	if (@_) {
+		$error = shift;
+		$self->error($error);
+		$self->storeObject();
+	}
+	return $error;
+}
+
+
+
+=head1 AUTHOR
+
+Ilya Goldberg (igg@nih.gov)
+
+=head1 SEE ALSO
+
+L<OME::Tasks::NotificationManager|OME::Tasks::NotificationManager>
+
+=cut
+
+
+1;
+
---
+++ src/perl2/OME/Tasks/NotificationManager.pm
@@ -0,0 +1,180 @@
+# OME/Tasks/NotificationManager.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Ilya Goldberg <igg@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Tasks::NotificationManager;
+
+use OME;
+use OME::Session;
+use Log::Agent;
+our $VERSION = $OME::VERSION;
+
+our $__soleFactory = undef;
+
+
+=head1 NAME
+
+OME::Tasks::NotificationManager - utility methods to manage OME::Tasks objects
+
+=head1 SYNOPSIS
+
+	# Create a new task notification object
+	# $nsteps is optional, but the first parameter (name) is required.
+	my $progress = new OME::Tasks::NotificationManager ("Reaaly long task", $nsteps);
+
+	# do something
+	$progress->step(); # or optionally, $progress->step ($theStepNum)
+	# report an error
+	$progress->setError ("Is och und vey, I'm verklempt!");
+	# message - the message is replaced every time its called
+	$progress->setMessage ("Zei gezunt!");
+	# finish
+	$progress->finish()
+
+	#  Meanwhile, in another process:
+	# optionally, supply a state or taskID filter.
+	my @tasks = OME::Tasks::NotificationManager->list (ID=>$taskID);
+
+	# Clear tasks
+	# optionally, supply a state or taskID filter.
+	OME::Tasks::NotificationManager->clear (state=>'FINISHED');
+
+=head1 DESCRIPTION
+
+OME::Tasks::NotificationManager provides utility methods to manage L<OME::Task|OME::Task> objects
+
+=head1 METHODS
+
+=head2 new ($name, $nsteps)
+
+Make and return a new L<OME::Task|OME::Task> object with the specified name and
+optionally, number of steps in the task.  The steps can mean anything you want.  It could be
+number of images or number of bytes or whatever.  Everytime a step (or several) are completed,
+call L<OME::Task|OME::Task>->step().
+
+=cut
+
+sub new {
+	my $proto = shift;
+    my $class = ref($proto) || $proto;
+   	my ($name,$nsteps) = @_;
+
+	$task = $class->taskFactory()->newObject("OME::Task", {
+		name        => $name,
+		process_id  => $$,
+		session_id  => OME::Session->instance()->ID(),
+		state       => 'IN PROGRESS',
+		message     => undef,
+		error       => undef,
+		n_steps     $nsteps,
+		last_step   => undef,
+		t_start     => 'now',
+		t_stop      => undef,
+		t_last      => undef
+	});
+	return ($task);
+
+}
+
+=head2 list (ID => $taskID, state => $state, session => $session)
+
+Return a list of L<OME::Task|OME::Task> objects with the specified optional criteria.
+See the fields of the OME::Task class for a full list of useable criteria.
+If C<session> is not supplied, the current user's session is used.
+
+=cut
+
+sub list {
+	my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my %criteria = @_;
+
+    $criteria{session_id} = OME::Session->instance()->ID()
+    	unless exists $criteria{session_id}
+    	or  exists $criteria{session};
+
+    return ($class->taskFactory()->findObjects('OME::Task',%criteria));
+}
+
+=head2 clear (ID => $taskID, state => $state, session => $session)
+
+Clear (delete) all of the L<OME::Task|OME::Task> objects that fit the supplied criteria.
+See the fields of the OME::Task class for a full list of useable criteria.
+If C<session> is not supplied, the current user's session is used.
+
+=cut
+
+sub clear {
+	my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my %criteria = @_;
+
+    $criteria{session_id} = OME::Session->instance()->ID()
+    	unless exists $criteria{session_id}
+    	or  exists $criteria{session};
+
+    my @tasks = $class->taskFactory()->findObjects('OME::Task',%criteria);
+    foreach (@tasks) {
+    	$_->deleteObject();
+    }
+}
+
+# This is a semi-private method.  Besides this class, it should only be used by OME::Task.
+sub taskFactory {
+
+	if ($__soleFactory) {
+		return ($__soleFactory);
+	} else {
+		$__soleFactory = OME::Factory->new({AutoCommit => 1});
+		return ($__soleFactory);
+	}
+}
+
+
+
+=head1 AUTHOR
+
+Ilya Goldberg (igg@nih.gov)
+
+=head1 SEE ALSO
+
+L<OME::Task|OME::Task>
+
+=cut
+
+
+1;
+
