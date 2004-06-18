eee430ecdf6c5eae2ac84140b9133487d8fa5c8f
Josiah Johnston <siah@nih.gov>
2004-06-18 19:18:42 +0000

    [Bug 92] name and ref render rhe module_execution now
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4686 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/DBObjRender/__OME_AnalysisChainExecution_NodeExecution.pm
+++ src/perl2/OME/Web/DBObjRender/__OME_AnalysisChainExecution_NodeExecution.pm
@@ -41,61 +41,48 @@ package OME::Web::DBObjRender::__OME_AnalysisChainExecution_NodeExecution;
 
 =head1 NAME
 
-OME::Web::DBObjRender::__OME_AnalysisChainExecution_NodeExecution - Specialized rendering for OME::AnalysisChainExecution::NodeExecution
+OME::Web::DBObjRender::__OME_AnalysisChainExecution_NodeExecution -
+Specialized rendering
 
 =head1 DESCRIPTION
 
-This name will be the name of the module execution it represents.
-The reference will contain both links to the MEX and the Node Execution.
+Return links to the MEX instead of ref to self.
+OME::AnalysisChainExecution::NodeExecution
 
 =head1 METHODS
 
 =cut
 
 use strict;
+use vars qw($VERSION);
 use OME;
-our $VERSION = $OME::VERSION;
-
-use HTML::Template;
-use OME::Tasks::ImageManager;
-use OME::Tasks::ModuleExecutionManager;
-use Carp 'cluck';
+use OME::Session;
 use base qw(OME::Web::DBObjRender);
 
 =head2 _getName
 
+returns name of MEX
+
 =cut
 
-#sub _getName {
+sub _getName {
+	my ($self, $obj, $options) = @_;
+
+	return $self->getName( $obj->module_execution() );
+}
 
 =head2 _getRef
+
+returns ref of MEX
 
 =cut
 
 sub _getRef {
-	my ($self,$obj,$format) = @_;
-	
-	for( $format ) {
-		if( /^txt$/ ) {
-			return $obj->id();
-		}
-		if( /^html$/ ) {
-			my ($package_name, $common_name, $formal_name, $ST) =
-				OME::Web->_loadTypeAndGetInfo( $obj );
-			my $id   = $obj->id();
-			my $mex_name;
-			if( $obj->module_execution()->module() ) {
-				$mex_name = $obj->module_execution()->module()->name();
-			} else {
-				$mex_name = "Virtual Module ".$obj->module_execution()->id();
-			}
-			my $mex_href = $self->getObjDetailURL( $obj->module_execution() );
-			my $obj_href = $self->getObjDetailURL( $obj );
-			my $ref = "<a href='$mex_href' title='Details on this Module Execution'>$mex_name</a> (<a href='$obj_href' title='Details on this Node Execution'>$id</a>)";
-			return $ref;
-		}
-	}
+	my ($self, $obj, $options) = @_;
+
+	return $self->getRef( $obj->module_execution(), 'html' );
 }
+
 
 =head1 Author
 
