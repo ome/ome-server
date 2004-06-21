b7460604a39f6ecd772d7bc1cdfd88f8295bef93
Tomas Macura <tmacura@nih.gov>
2004-06-21 23:50:43 +0000

    UberImport has been updated to use the OME::ImportEngine::ImportEngine importer and not the old OME::Tasks::OMEImport . Also UberImport now uploads files into OME::Image::Server::File .
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4699 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tests/UberImport.pl
+++ src/perl2/OME/Tests/UberImport.pl
@@ -1,8 +1,10 @@
 use strict;
-use OME::Tasks::OMEImport;
-use OME::SessionManager;
 use XML::LibXML;
 use Log::Agent;
+use OME::SessionManager;
+use OME::ImportEngine::ImportEngine;
+use OME::Image::Server::File;
+
 if( ! $ARGV[0] ) {
 	print "Usage is:\n\t perl UberImport.pl [file1] [file2] ...\n";
 	exit -1;
@@ -11,34 +13,21 @@ if( ! $ARGV[0] ) {
 # Set OME_DEBUG environment variable to turn on debugging.
 #
 if ($ENV{OME_DEBUG} > 0) {
-#	print 'Debugging output enabled';
-#	logconfig(
-#		-driver    => Log::Agent::Driver::File->make(
-#			-prefix      => '$0',
-#			-showpid     => 1,
-#			-channels    => {
-#	#			 'error'  => '/OME/Logs/OME.err',
-#	#			 'output' => '/OME/Logs/OME.out',
-#				 'debug'  => "$0.dbg",
-#			}
-#		),
-#	# for now, debug output is on.
-#		-level    => 'debug'
-#	);
-	
 	logconfig(
 		-prefix      => "$0",
 		-level    => 'debug'
 	);
 }
-
 my $manager = OME::SessionManager->new();
 my $session = $manager->TTYlogin();
-#$session->Factory()->obtainDBH()->trace(3);
-my $OMEImporter = OME::Tasks::OMEImport->new( session => $session, debug => 1 );
-
+my $repository = $session->findRepository(); # make sure there is one, and its activated.
+my $importer = OME::ImportEngine::ImportEngine->new (session => $session);
+my @files;
 foreach my $path (@ARGV) {
-	print "\n\nImporting $path.\n";
-	$OMEImporter->importFile( $path );
+	print "Uploading to OMEIS: $path.\n";
+	push @files, OME::Image::Server::File->upload($path);
 }
-
+print "Uploading to OME:".join(" ","",@files). "\n";
+$importer->startImport();
+$importer->importFiles(\@files);
+$importer->finishImport();
