9f8a2a0271c5552bff69d078510377b8f1c1e35b
Josiah Johnston <siah@nih.gov>
2004-07-16 21:15:19 +0000

    [Bug 13] added controls to explicitly merge outputs of module executions.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4812 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/DBObjDetail/__OME_ModuleExecution.pm
+++ src/perl2/OME/Web/DBObjDetail/__OME_ModuleExecution.pm
@@ -53,6 +53,7 @@ use strict;
 use OME;
 our $VERSION = $OME::VERSION;
 use Log::Agent;
+use HTML::Template;
 
 use OME::Web::DBObjTable;
 use OME::Tasks::ModuleExecutionManager;
@@ -70,6 +71,7 @@ use base qw(OME::Web::DBObjDetail);
 
 sub getPageBody {
 	my $self = shift;
+	my $factory = $self->Session()->Factory();
 	my $q = $self->CGI();
 
 	my $mex = $self->_loadObject();
@@ -77,124 +79,147 @@ sub getPageBody {
 		if $mex->virtual_mex();
 
 	( $self->{ form_name } = $q->param( 'Type' ).$q->param( 'ID' ) ) =~ s/[:@]/_/g;
-	my $html = $q->startform( { -name => $self->{ form_name } } ).
-	           $q->hidden({-name => 'Type', -default => $q->param( 'Type' ) }).
-	           $q->hidden({-name => 'ID', -default => $q->param( 'ID' ) }).
-	           $q->hidden({-name => 'action', -default => ''});
-
+	my $html = $q->startform( -name => $self->{ form_name }  ).
+	           $q->hidden(-name => 'Type', -default => $q->param( 'Type' ) ).
+	           $q->hidden(-name => 'ID', -default => $q->param( 'ID' ) );
+	
 	my @actual_inputs = $mex->inputs();
 	my @formal_outputs = $mex->module()->outputs();
+	@formal_outputs = grep( defined $_->semantic_type(), @formal_outputs );
 	my @formal_inputs = $mex->module()->inputs();
 	my @untyped_outputs = $mex->untypedOutputs();
-
-	# Inputs & Outputs Table
-	my $ioTable = $q->table( { -class => 'ome_table' },
-		$q->Tr(
-			$q->td( { -class => 'ome_td' }, [
-				'Inputs', 'Outputs'
-			]),
-		),
-		$q->Tr(
-			$q->td( { -class => 'ome_td', -rowspan => 3, -valign => 'top' },
-				join( '<br>',map( 
-					$q->a( { href => '#'.$_->name() }, $_->name() ),
-					@formal_inputs
-				) ),
-			),
-			$q->td( { -class => 'ome_td', -valign => 'top' },
-				join( '<br>',map( 
-					$q->a( { href => '#'.$_->name() }, $_->name() ),
-					@formal_outputs
-				) ),
-			),
-		),
-		$q->Tr(
-			$q->td( { -class => 'ome_td', -valign => 'top' },
-				join( '<br>',map( 
-					$q->a( { href => '#untyped_'.$_->semantic_type->name() }, $_->semantic_type->name() ),
-					@untyped_outputs
-				) ),
-			),
-		)
-	);
-
-	# Details of execution & I/O overview
-	$html .= $q->table( {-cellpadding => 10 },
-		$q->Tr(
-			$q->td(  { -width => '50%', -valign => 'top', -align => 'center'}, 
-				$self->Renderer()->render( $mex, 'detail' )
-			),
-			$q->td(  { -width => '50%', -valign => 'top', -align => 'center'}, 
-				$ioTable
-			),
-		)
-	);
-
-	# Actual Outputs Tables
-	my $tableMaker = OME::Web::DBObjTable->new( CGI => $q );
-	$html .= $q->h1( 'Outputs' );
+	my $tmpl_path = $self->Renderer()->_findTemplate( ref( $mex ), 'detail1' );
+	my $tmpl = HTML::Template->new( filename => $tmpl_path, case_sensitive => 1 );
+	my %tmpl_data;
+	my $tableMaker = OME::Web::DBObjTable->new( CGI => $q );	
 	
-	@formal_outputs = grep( defined $_->semantic_type(), @formal_outputs );
+	############
+	#	 Merge outputs
 	
-	# figure out which formal_outputs should be joined based on being in the same database table 
-	my $fo_table_name;
-	my %clumped_list;
-	my @list;
-	foreach my $fo (@formal_outputs){
-		$fo_table_name = $fo->semantic_type()->semantic_elements()->next()->data_column()->data_table()->table_name();
-		push( @{ $clumped_list{$fo_table_name} },$fo);
+	# pick up the previously stored merged groups of outputs
+	my @merged_output_groups;
+	if( $q->param( 'merged_output_groups' ) ) {
+		@merged_output_groups = $q->param( 'merged_output_groups' );
+		# remove groups as requested
+		@merged_output_groups = grep( $_ ne $q->param( 'UngroupID' ), @merged_output_groups );
+		# parse the comma delimited ids
+		@merged_output_groups = map( [ split( ',', $_ ) ], @merged_output_groups );
+		# turn ids into objects for each merge group
+		foreach my $merged_output( @merged_output_groups ) {
+			$merged_output = [ map( 
+				$factory->loadObject( "OME::Module::FormalOutput", $_ ), 
+				@$merged_output ) ];
+		}
 	}
 	
+	# pick up recent merge request & store it
+	if( $q->param( 'Merge Outputs' ) && $q->param( 'formal_outputs_to_merge' )) {
+		my @formal_outputs_to_merge = $q->param( 'formal_outputs_to_merge' );
+		# load the formal output object for each id
+		@formal_outputs_to_merge = map( 
+			$factory->loadObject( "OME::Module::FormalOutput", $_ ), 
+			@formal_outputs_to_merge );
+		# add to the list of groups
+		push( @merged_output_groups, \@formal_outputs_to_merge );
+	}
 	
-	# Making the Typed Output Tables
-	foreach my $clump (sort keys %clumped_list){
-		my @tables;
-		
-		#if we are displaying only a single formal output we don't need a join
-# 		if (scalar(@{$clumped_list{$clump}}) == 1 ) {
-# 			my $fo = $clumped_list{$clump}->[0];
-# 			my $attributes = OME::Tasks::ModuleExecutionManager->getAttributesForMEX($mex, $fo->semantic_type);
-# 			@tables = $tableMaker->getTable( 
-# 				{
-# 					excludeFields    => { module_execution => undef },
-# 					embedded_in_form => $self->{ form_name },
-# 					title            => $fo->name(),
-# 				},
-# 				$self->_STformalName( $fo->semantic_type() ),
-# 				$attributes
-# 			);
-# 		
-# 		# display a merged table only on the formal outputs stored in the same database table
-# 		} else {
-			my @table_data;
-			foreach my $fo ( @{$clumped_list{$clump}} ) {
-				next unless $fo->semantic_type;
-				my $attributes = OME::Tasks::ModuleExecutionManager->
-					getAttributesForMEX($mex,$fo->semantic_type);
-				push @table_data, {
-					title   => $fo->name(),
-					type    => $self->_STformalName( $fo->semantic_type() ),
-					objects => $attributes
-				};
-			}
-			@tables = $tableMaker->getJoinTable( 
+	# save formal output groupings 
+	my @merged_output_groups_id;
+	# make an array that looks like ('id,id,id', 'id,id')
+	foreach my $output_set ( @merged_output_groups ) {
+		push @merged_output_groups_id, join( ',', map( $_->id(), @$output_set ) );
+	}
+ 	$html .= $q->hidden(
+		-name => 'merged_output_groups', 
+		-default => \@merged_output_groups_id, 
+		-override => 1 
+	);
+
+
+	##############
+	#	Start Rendering
+
+	# Execution Details
+	$tmpl_data{ mex_detail } = $self->Renderer()->render( $mex, 'detail' );
+
+	# I/O Table
+	# input column of i/o table
+	$tmpl_data{ input_names } = [ map( { name => $q->a( { href => '#'.$_->name() }, $_->name() ) }, @formal_inputs) ];
+	# output column of i/o table
+	$tmpl_data{ output_names } = [ map( { 
+		name => $q->a( { href => '#'.$_->name() }, $_->name() ),
+		checkbox => $q->checkbox( -name => 'formal_outputs_to_merge', -value => $_->id(), -label => '', -checked => undef )
+	}, @formal_outputs) ];
+	# untyped outputs column of i/o table
+	$tmpl_data{ untyped_output_names } = [ map( { 
+		name => $q->td( $q->a( { href => '#untyped_'.$_->semantic_type->name() }, $_->semantic_type->name() ) ),
+#		checkbox => $q->checkbox( -name => 'untyped_outputs_to_merge', -value => $_->id(), -label => '', -checked => undef )
+	}, @untyped_outputs) ];
+	# Ouput Groups column
+	foreach my $group ( @merged_output_groups ) {
+		push( @{ $tmpl_data{ output_groups } }, { 
+		group => $q->a( { href => '#'.join( ',', map( $_->id(), @$group ) ) }, join( ', ', map( $_->name(), @$group ) ) ),
+		# Should I add an ungroup button here?
+		} );
+	}
+	# button to merge outputs
+	$tmpl_data{ merge_button } = $q->submit( -name => 'Merge Outputs' );
+	
+
+
+	# Merged outputs.
+	foreach my $group( @merged_output_groups ) {
+		my @table_data;
+		my $group_id = join( ',', map( $_->id, @{$group} ) );
+		foreach my $fo ( @{$group} ) {
+			next unless $fo->semantic_type;
+			my $attributes = OME::Tasks::ModuleExecutionManager->
+				getAttributesForMEX($mex,$fo->semantic_type);
+			push @table_data, {
+				title   => $fo->name(),
+				type    => $self->_STformalName( $fo->semantic_type() ),
+				objects => $attributes
+			};
+		}
+		my @tables = $tableMaker->getJoinTable( 
+			{
+				excludeFields    => { module_execution => undef },
+				embedded_in_form => $self->{ form_name },
+			},
+			\@table_data
+		);
+		push( @{ $tmpl_data{ grouped_outputs } }, { output => 
+			$q->a( { 
+				-title => 'Click here to Ungroup these Outputs', 
+				-href  => "javascript: document.forms['".$self->{ form_name }."'].elements['UngroupID'].value='$group_id'; document.forms['".$self->{ form_name }."'].submit();", 
+			}, "Ungroup Outputs" )."<br>".
+			$q->a( { -name => $group_id }, ' ').
+			join( '<br>', @tables ) 
+		} );
+	}
+	
+	# Render outputs ( ungrouped )
+	foreach my $fo ( @formal_outputs ) {
+		my $attributes = OME::Tasks::ModuleExecutionManager->
+			getAttributesForMEX($mex,$fo->semantic_type);		
+		push( @{ $tmpl_data{ outputs } }, { 
+			output => $tableMaker->getTable( 
 				{
-					excludeFields    => { module_execution => undef },
+					noSearch => 1,
+					title    => $fo->name(),
 					embedded_in_form => $self->{ form_name },
 				},
-				\@table_data
-			);
-#		}
+				$self->_STformalName( $fo->semantic_type() ),
+				$attributes
+			)
+		} );
+	}
 		
-		$html .= join( '<br>', @tables );
- 	}
-	
 	# Untyped Outputs Tables
-	$html .= $q->h1( 'Untyped Outputs' );
 	foreach my $sto ( @untyped_outputs ) {
 		my $attributes = OME::Tasks::ModuleExecutionManager->
 			getAttributesForMEX($mex,$sto->semantic_type);
-		$html .= 
+		push( @{ $tmpl_data{ untyped_outputs } }, { untyped_output => 
 			$q->a( { -name => 'untyped_'.$sto->semantic_type->name() }, ' ').
 			$tableMaker->getTable( 
 				{
@@ -204,15 +229,15 @@ sub getPageBody {
 				},
 				$self->_STformalName( $sto->semantic_type() ),
 				$attributes
-			);
+			)
+		} );
 	}
 
 	# Actual Inputs Tables
-	$html .= $q->h1( 'Inputs' );
 	foreach my $ai ( @actual_inputs ) {
 		my $attributes = OME::Tasks::ModuleExecutionManager->
 			getAttributesForMEX($ai->input_module_execution,$ai->formal_input()->semantic_type);
-		$html .= 
+		push( @{ $tmpl_data{ inputs } }, { input => 
 			$q->a( { -name => $ai->formal_input()->name() }, ' ' ).
 			$tableMaker->getTable( 
 				{
@@ -221,9 +246,15 @@ sub getPageBody {
 				},
 				$self->_STformalName( $ai->formal_input()->semantic_type() ),
 				$attributes
-			);
+			)
+		} );
 	}
 	
+	# populate the template & print it out
+	$tmpl->param( %tmpl_data );
+	$html .= $tmpl->output();
+	$q->delete( 'UngroupID' );
+	$html .= $q->hidden(-name => 'UngroupID' );
 	$html .= $q->endform();
 
 	$self->_takeAction( );
