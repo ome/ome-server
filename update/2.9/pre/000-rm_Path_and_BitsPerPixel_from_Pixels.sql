5d865da53d1a6a3fb37031b071595aa124e32b26
Josiah Johnston <siah@nih.gov>
2004-07-28 17:59:35 +0000

    [Bug 226] forgetting to update rarely used PixelsPlane ST was causing dangling references to BitsPerPixel & Path when it was used
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@4954 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- update/2.9/pre/000-rm_Path_and_BitsPerPixel_from_Pixels.sql
+++ update/2.9/pre/000-rm_Path_and_BitsPerPixel_from_Pixels.sql
@@ -2,6 +2,6 @@
 -- postgresql v < 7.3 doesn't allow column removals from tables, so we're gonna leave the table IMAGE_PIXELS alone. 
 
 BEGIN;
-delete from data_columns where (semantic_elements.name='Path' or semantic_elements.name='BitsPerPixel') and semantic_elements.semantic_type_id=semantic_types.semantic_type_id and semantic_types.name='Pixels' and data_columns.data_column_id=semantic_elements.data_column_id;
-delete from semantic_elements where (semantic_elements.name='Path' or semantic_elements.name='BitsPerPixel') and semantic_elements.semantic_type_id=semantic_types.semantic_type_id and semantic_types.name='Pixels';
+delete from data_columns where (semantic_elements.name='Path' or semantic_elements.name='BitsPerPixel') and semantic_elements.semantic_type_id=semantic_types.semantic_type_id and ( semantic_types.name='Pixels' or semantic_types.name='PixelsPlane' ) and data_columns.data_column_id=semantic_elements.data_column_id;
+delete from semantic_elements where (semantic_elements.name='Path' or semantic_elements.name='BitsPerPixel') and semantic_elements.semantic_type_id=semantic_types.semantic_type_id and ( semantic_types.name='Pixels' or semantic_types.name='PixelsPlane' );
 END;
