9f7909c89962f87fbb101e167639a060c5d6cf03
Josiah Johnston <siah@nih.gov>
2004-09-27 17:55:38 +0000

    s/PixelsSlice\.Pixels/PixelsSlice.Parent/
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5397 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Analysis/Modules/PPM/FourierTransform.pm
+++ src/perl2/OME/Analysis/Modules/PPM/FourierTransform.pm
@@ -72,7 +72,7 @@ sub startImage {
 		my $parent = $factory->
 		  newAttribute('PixelsSlice',undef,$mex,
 					   {
-						Pixels => $slice->Pixels(),
+						Parent => $slice->Parent(),
 						StartX => $slice->StartX(),
 						EndX   => $slice->EndX(),
 						StartY => $slice->StartY(),
