89a5487379d448c401d3ef0527159fe1c5195e78
Tomas Macura <tmacura@nih.gov>
2004-12-11 20:12:38 +0000

    ChebyshevStatistics are signatures (32 bin histogram) computed from the coefficients of 2D Chebyshev transform.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5665 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/matlab/OME/Statistics/ChebyshevStatistics.m
@@ -0,0 +1,52 @@
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% Copyright (C) 2003 Open Microscopy Environment
+%       Massachusetts Institue of Technology,
+%       National Institutes of Health,
+%       University of Dundee
+%
+%
+%
+%    This library is free software; you can redistribute it and/or
+%    modify it under the terms of the GNU Lesser General Public
+%    License as published by the Free Software Foundation; either
+%    version 2.1 of the License, or (at your option) any later version.
+%
+%    This library is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%    Lesser General Public License for more details.
+%
+%    You should have received a copy of the GNU Lesser General Public
+%    License along with this library; if not, write to the Free Software
+%    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Written by:  Tom Macura <norlov@nih.gov>
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+%% Function computes signatures (32 bin histogram) from coefficients of 2D 
+%% Chebyshev transform (by default its 20th order)
+%%
+%% Tom Macura 
+%% Computational Biology Unit, LG, NIA/NIH
+%% :: revision :: 12-01-2004 ::
+%%
+%% output: ch (Chebyshev Histogram)
+%% input: 
+%%       Im,   Image 
+%%       Coef, Coeff order (Optional)
+%%
+%% Examples:
+%% [ch] = ChebyshevStatistics(Im);
+
+function ch = ChebyshevStatistics(Im,N);
+
+if nargin < 2 N = 20; end
+
+ch = ChebyshevTransform(Im,N);
+ch = hist(ch(:),32);
+return;
