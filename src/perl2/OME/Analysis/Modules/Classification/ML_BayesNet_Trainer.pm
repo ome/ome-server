c81af4d6ce548998f4fce0f4e4fe4cc30b585870
Josiah Johnston <siah@nih.gov>
2005-01-06 20:31:44 +0000

    It actually works now. At least as a black box shell that accepts inputs and shuffles them into a signature matrix.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5735 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Analysis/Modules/Classification/ML_BayesNet_Trainer.pm
+++ src/perl2/OME/Analysis/Modules/Classification/ML_BayesNet_Trainer.pm
@@ -58,7 +58,7 @@ our $VERSION = $OME::VERSION;
 
 use Log::Agent;
 use Carp;
-use OME::Matlab::Engine;
+use OME::Matlab;
 use OME::Util::Classifier;
 
 use base qw(OME::Analysis::Handler);
@@ -76,48 +76,30 @@ sub execute {
 	my $conf = $session->Configuration() or croak "couldn't retrieve Configuration variables";
 	my $matlab_src_dir = $conf->matlab_src_dir or croak "couldn't retrieve matlab src dir from configuration";
 	logdbg "debug", "Matlab src dir is $matlab_src_dir\n".
-	$engine->eval("addpath(genpath('$matlab_src_dir'));");
+	$matlab_engine->eval("addpath(genpath('$matlab_src_dir'));");
 
 	# Compile Signature matrix and place into matlab for input
 	my $start_time = [gettimeofday()];
 	my @images = $dataset->images( );
 	@images = sort {$a->id <=> $b->id} @images;
-	my $actual_inputs_list = $self->getActualInputs( 'SignatureVectors' );
-    my @input_mexes = map { $_->input_module_execution() } @$actual_inputs;
-	my $signature_matrix = OME::Util::Classifier->compile_signature_matrix( \@input_mexes, \@images );
+
+    my @classification_mexes = map { $_->input_module_execution() } @{ 
+    	$self->getActualInputs( 'Classifications' ) };
+    my @sigVectors_mexes = map { $_->input_module_execution() } @{ 
+    	$self->getActualInputs( 'SignatureVectors' ) };
+	my $signature_matrix = OME::Util::Classifier->compile_signature_matrix( \@sigVectors_mexes, \@images, \@classification_mexes );
 	$matlab_engine->eval("global signature_matrix");
 	$matlab_engine->putVariable( "signature_matrix", $signature_matrix);
 	$mex->attribute_db_time(tv_interval($start_time));
 	
-# TEST ME
-	# Execute the function
-	my $command = "[serialized_bayes_net_classifier, signatures_used] = trainer_fuction( signature_matrix );";
-	logdbg "debug", "***** Command to Matlab: $command\n";
-	my $outBuffer      = " " x 2048;
-	my $blankOutBuffer = " " x 2048;
-	$matlab_engine->setOutputBuffer($outBuffer, length($outBuffer));
-	$start_time = [gettimeofday()];
-	$matlab_engine->eval($command);
-	$outBuffer =~ s/(\0.*)$//;
-	$mex->total_time(tv_interval($start_time));
-	if ($outBuffer ne $blankOutBuffer) {
-		$mex->error_message("$outBuffer");
-		logdbg "debug", "***** Output from Matlab:\n $outBuffer\n";
-	} else {
-		logdbg "debug", "***** Output from Matlab:\n";
-	}
+# Dump the signature matrix to test what we have so far
+# my $output_file_name = '~/foo.mat';
+# $matlab_engine->eval("global signature_vector");
+# $matlab_engine->putVariable('signature_vector',$signature_matrix);
+# $matlab_engine->eval( "save $output_file_name signature_vector;" );
+# print STDERR "Saved signature vector to file $output_file_name.\n\n\n";
 
-	# retrieve output & save to DB
-	$start_time = [gettimeofday()];
-	my $classifier = $matlab_engine->getVariable( "serialized_bayes_net_classifier" )
-		or die "Couldn't retrieve output 'serialized_bayes_net_classifier'";
-	$classifier->makePersistent();
-# FIXME: extract data from $classifier & save to DB
-	my $sigs_used = $matlab_engine->getVariable( "signatures_used" )
-		or die "Couldn't retrieve output 'signatures_used'";
-	$sigs_used->makePersistent();
-# FIXME: extract data from $sigs_used & save to DB
-	$mex->attribute_create_time(tv_interval($start_time));
+# IMPLEMENT THE REST OF THIS
 
 	# close connection to matlab
 	$matlab_engine->close();
