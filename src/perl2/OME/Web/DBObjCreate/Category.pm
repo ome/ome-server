a7ead46cbba7c64dd0426d751f39c0426895c080
Josiah Johnston <siah@nih.gov>
2005-01-12 17:26:25 +0000

    Category D -> G transition
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5769 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/DBObjCreate/Category.pm
+++ src/perl2/OME/Web/DBObjCreate/Category.pm
@@ -73,13 +73,8 @@ sub _create {
 		return( $returnType, $body );
 	}
 	
-#FIXME: remove the dataset loading line to transition this to Global Categories
-my $dataset = $factory->loadObject( "OME::Dataset", $q->param( 'dataset' ) ) 
-or die "Couldn't load dataset id=".$data_hash{ dataset };
 	my ($mex, $objs) = OME::Tasks::AnnotationManager->
-#FIXME: remove annotateDataset and uncomment the annotateGlobal line to transition this to Global Categories
-annotateDataset( $dataset, "Category", \%data_hash );
-#		annotateGlobal( "Category", \%data_hash );
+		annotateGlobal( "Category", \%data_hash );
  	$session->commitTransaction();
  	my $obj = $objs->[0];
  	
