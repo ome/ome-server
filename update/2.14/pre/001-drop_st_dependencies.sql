560acf509eab4c90f5afcabe53f9950e4dfd5430
Chris Allan <callan@lifesci.dundee.ac.uk>
2005-01-13 18:02:47 +0000

    Updated to follow dropColumn() database delegate semantics and play nicely with PostgreSQL version < 7.3.0.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5787 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- update/2.14/pre/001-drop_st_dependencies.sql
+++ update/2.14/pre/001-drop_st_dependencies.sql
@@ -1,14 +1,7 @@
--- These columns are not needed. Module execution timestamps and experimenter
--- references should be used instead.
-
--- remove columns from image_annotations
-ALTER TABLE image_annotations DROP timestamp CASCADE;
-ALTER TABLE image_annotations DROP experimenter CASCADE;
+-- remove semantic type references to legacy columns from image_annotations
 DELETE FROM semantic_elements WHERE semantic_type_id = (SELECT semantic_type_id FROM semantic_types WHERE name = 'ImageAnnotation') AND name IN ('Timestamp', 'Experimenter');
 
--- remove columns from dataset_annotations
-ALTER TABLE dataset_annotations DROP timestamp CASCADE;
-ALTER TABLE dataset_annotations DROP experimenter CASCADE;
+-- remove semantic type references to legacy columns from dataset_annotations
 DELETE FROM semantic_elements WHERE semantic_type_id = (SELECT semantic_type_id FROM semantic_types WHERE name = 'DatasetAnnotation') AND name IN ('Timestamp', 'Experimenter');
 
 -- cleanup now orphaned data columns
