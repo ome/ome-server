2d2510256b3f852baac5ea1d2862c2bf82a2f170
Chris Allan <callan@lifesci.dundee.ac.uk>
2005-01-13 14:49:54 +0000

    Wooops! Forgot to cleanup the mess now left in data_columns and semantic_elements.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5785 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- update/2.14/pre/000-drop_annotation_columns.sql
+++ update/2.14/pre/000-drop_annotation_columns.sql
@@ -1,10 +1,15 @@
 -- These columns are not needed. Module execution timestamps and experimenter
 -- references should be used instead.
 
--- image_annotations
+-- remove columns from image_annotations
 ALTER TABLE image_annotations DROP timestamp CASCADE;
 ALTER TABLE image_annotations DROP experimenter CASCADE;
+DELETE FROM semantic_elements WHERE semantic_type_id = (SELECT semantic_type_id FROM semantic_types WHERE name = 'ImageAnnotation') AND name IN ('Timestamp', 'Experimenter');
 
--- dataset_annotations
+-- remove columns from dataset_annotations
 ALTER TABLE dataset_annotations DROP timestamp CASCADE;
 ALTER TABLE dataset_annotations DROP experimenter CASCADE;
+DELETE FROM semantic_elements WHERE semantic_type_id = (SELECT semantic_type_id FROM semantic_types WHERE name = 'DatasetAnnotation') AND name IN ('Timestamp', 'Experimenter');
+
+-- cleanup now orphaned data columns
+DELETE FROM data_columns WHERE data_column_id NOT IN (SELECT data_column_id FROM semantic_elements);
