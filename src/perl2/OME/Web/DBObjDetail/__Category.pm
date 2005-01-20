025cc12b92e7df354c99d74885c82b5bdb9025b5
Josiah Johnston <siah@nih.gov>
2005-01-20 23:54:52 +0000

    moved some logic into CategoryManager. added declassification support
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5825 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/DBObjDetail/__Category.pm
+++ src/perl2/OME/Web/DBObjDetail/__Category.pm
@@ -57,6 +57,7 @@ use strict;
 use OME;
 our $VERSION = $OME::VERSION;
 use OME::Tasks::AnnotationManager;
+use OME::Tasks::CategoryManager;
 
 use Log::Agent;
 use base qw(OME::Web::DBObjDetail);
@@ -74,50 +75,46 @@ sub _takeAction {
 	
 	my $image_ids = $q->param( 'images_to_categorize' );
 	if( $image_ids ) {
- 		foreach my $image_id ( split( m',', $image_ids ) ) {
- 			# Don't add this image if it belongs to another image in this Category Group.
- 			# Don't bother searching if there are no other categories in this group;
- 			# DBObject can't handle empty lists with the 'in' operator.
- 			my @other_classifications = ( scalar ( @other_categories_in_this_group ) > 0 ?
-				$factory->findObjects( '@Classification', {
-					image                           => $image_id,
-					'module_execution.experimenter' => $session->User(),
-# for some unknown reason, this next parameter consistently fails with
-# "DBD::Pg::st execute failed: ERROR:  parser: parse error at or near "'" at /Users/josiah/OME/cvs/OME/src/perl2//OME/Factory.pm line 1069."
-# so i'll ignore it and grep it out at the next step
-#					Valid                           => [ "is not", 0 ],
-					Category => [ 'in', [ @other_categories_in_this_group] ]
-				} ) :
-				()
-			);
-			@other_classifications = grep( (not defined $_->Valid || $_->Valid != 0 ), @other_classifications );
-			if( @other_classifications ) {
-	 			$message .= "<font color='red'>Cannot add image ".
-	 				$self->Renderer()->render( $other_classifications[0]->image(), 'ref' ).
- 					" to this category because it belongs to another category. ".
- 					$self->Renderer()->render( $other_classifications[0]->Category(), 'ref' ).
- 					".</font><br>";
- 				next;
- 			}
- 			
+		my @these_images_are_already_classified;
+		foreach my $image_id ( split( m',', $image_ids ) ) {
 			my $image = $factory->loadObject( 'OME::Image', $image_id )
 				or die "Couldn't load image id=$image_id";
- 			# skip if the image has already been classified with this classification
- 			if( $factory->findObject( '@Classification', {
-				Category => $obj,
-				image    => $image_id,
-				'module_execution.experimenter' => $session->User()
-			} ) ) {
+ 			my $rc = OME::Tasks::CategoryManager->classifyImage( $image, $obj );
+ 			next if $rc->semantic_type()->name() eq 'Classification';
+ 			if( $rc->id ne $obj->id ) {
 	 			$message .= "<font color='red'>Cannot add image ".
 	 				$self->Renderer()->render( $image, 'ref' ).
- 					" to this category because it already belongs to this category.</font><br>";
- 				next;
+ 					" to this category because it belongs to another category in this group.".
+ 					$self->Renderer()->render( $rc, 'ref' )."</font><br>";
+ 			} else {
+ 				push( @these_images_are_already_classified, $image );
 			}
-			my ( $mex, $attrs ) = OME::Tasks::AnnotationManager->
-				annotateImage( $image, 'Classification', { Category => $obj } );
 		}
- 		$session->commitTransaction();
+		$message .= "<font color='red'>Cannot add image".
+			( ( @these_images_are_already_classified > 1 ) ? 's ' : ' ' ).
+	 		$self->Renderer()->renderArray( \@these_images_are_already_classified, 'bare_ref_mass', { type => 'OME::Image' } ).
+ 			" because ".
+			( ( @these_images_are_already_classified > 1 ) ? 'they already belong' : 'it already belongs' ).
+ 			" to this category.</font><br>"
+ 			if( @these_images_are_already_classified );
+		$session->commitTransaction();
 	}
+	
+	my $image_id_to_declassify = $q->param( 'declassifyImage' );
+	if( $image_id_to_declassify ) {
+		my $image = $factory->loadObject( 'OME::Image', $image_id_to_declassify )
+			or die "Couldn't load image (id=$image_id_to_declassify)";
+		my $classification = $factory->findObject( 
+			'@Classification', 
+			Category => $obj,
+			image    => $image
+		) or die "Couldn't find a classification for this category & image";
+		$classification->Valid( 0 );
+		$classification->storeObject();
+		$session->commitTransaction();
+		$message .= "Declassified image ".$self->Renderer()->render( $image, 'ref' )."<br>";
+	}
+	
 	return $message;
 }
 
