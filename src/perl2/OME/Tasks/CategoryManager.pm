7bcba8780556472cc06ad283a4d5c9301129f74c
Josiah Johnston <siah@nih.gov>
2005-01-20 23:59:36 +0000

    added classification and declassification methods
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5827 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tasks/CategoryManager.pm
+++ src/perl2/OME/Tasks/CategoryManager.pm
@@ -39,6 +39,7 @@ package OME::Tasks::CategoryManager;
 use strict;
 use OME;
 use OME::Session;
+use OME::Tasks::AnnotationManager;
 our $VERSION = $OME::VERSION;
 
 =head1 NAME
@@ -77,6 +78,86 @@ sub getImagesInCategory {
 	return @images;
 }
 
+=head2 classifyImage()
+
+	OME::Tasks::CategoryManager->classifyImage($image, $category);
+
+Returns:
+	a Category if the image is classified under other categories in this category's group
+	this Category if the image is already classified with this Category
+	the classification attribute if successful
+
+=cut
+
+sub classifyImage {
+	my ($proto, $image, $category) = @_;
+	my $session = OME::Session->instance();
+	my $factory = $session->Factory();
+
+	my @other_categories_in_this_group = 
+		$category->CategoryGroup->CategoryList( id => [ '!=', $category->id ] );
+	# Don't add this image if it belongs to another image in this Category Group.
+	# Don't bother searching if there are no other categories in this group;
+	# DBObject can't handle empty lists with the 'in' operator.
+	my @other_classifications = ( scalar ( @other_categories_in_this_group ) > 0 ?
+		$factory->findObjects( '@Classification', {
+			image                           => $image,
+			'module_execution.experimenter' => $session->User(),
+# for some unknown reason, this next parameter consistently fails with
+# "DBD::Pg::st execute failed: ERROR:  parser: parse error at or near "'" at /Users/josiah/OME/cvs/OME/src/perl2//OME/Factory.pm line 1069."
+# so i'll ignore it and grep it out at the next step
+#					Valid                           => [ "is not", 0 ],
+			Category => [ 'in', [ @other_categories_in_this_group] ]
+		} ) :
+		()
+	);
+	@other_classifications = grep( (not defined $_->Valid || $_->Valid eq 1 ), @other_classifications );
+	return $other_classifications[0]->Category() if( @other_classifications );
+	
+	# skip if the image has already been classified with this classification
+	if( my $classification = $factory->findObject( '@Classification', {
+		Category => $category,
+		image    => $image,
+		'module_execution.experimenter' => $session->User(),
+	} ) ) {
+		return $category if $classification->Valid();
+		$classification->Valid( 1 );
+		$classification->storeObject();
+		$session->commitTransaction();
+		return $classification;
+	}
+
+	my ( $mex, $attrs ) = OME::Tasks::AnnotationManager->
+		annotateImage( $image, 'Classification', 
+		{ 
+			Category => $category,
+			Valid   => 1
+		}
+		);
+	return $attrs->[0];
+}
+
+=head2 classifyImage()
+
+	OME::Tasks::CategoryManager->declassifyImage($image, $category);
+
+declassifies an image.
+
+=cut
+
+sub declassifyImage {
+	my ($proto, $image, $category ) = @_;
+	my $session = OME::Session->instance();
+	my $factory = $session->Factory();
+	my $classification = $factory->findObject( 
+		'@Classification', 
+		Category => $category,
+		image    => $image
+	) or die "Couldn't find a classification for this category & image";
+	$classification->Valid( 0 );
+	$classification->storeObject();
+	$session->commitTransaction();
+}
 
 =head1 AUTHOR
 
