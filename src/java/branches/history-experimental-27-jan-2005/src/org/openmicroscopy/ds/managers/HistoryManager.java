f19187f07c7e1a8cb95f8788fcfeffab3bb31f73
Harry Hochheiser <hsh@nih.gov>
2005-01-27 20:07:46 +0000

    initial classes for data history retrieval
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@8590 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/java/branches/history-experimental-27-jan-2005/src/org/openmicroscopy/ds/managers/HistoryManager.java
@@ -0,0 +1,107 @@
+/*
+ * org.openmicroscopy.ds.managers.HistoryManager
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2004 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+
+
+/*------------------------------------------------------------------------------
+ * Written by:    Douglas Creager <dcreager@alum.mit.edu>
+ *------------------------------------------------------------------------------
+ */
+
+
+package org.openmicroscopy.ds.managers;
+
+
+import org.openmicroscopy.ds.AbstractService;
+import org.openmicroscopy.ds.DataException;
+import org.openmicroscopy.ds.DataServices;
+import org.openmicroscopy.ds.InstantiatingCaller;
+import org.openmicroscopy.ds.Instantiator;
+import org.openmicroscopy.ds.RemoteCaller;
+import org.openmicroscopy.ds.dto.ModuleExecution;
+import org.openmicroscopy.ds.dto.History;
+
+
+/**
+ * A Manager for retrieving data histories for Mexes
+ * @author Harry Hochheiser (hsh@nih.gov)
+ * @version 2.2 <small><i>(Internal: $Revision$ $Date$)</i></small>
+ * @since OME2.2
+ */
+
+public class HistoryManager
+    extends AbstractService
+{
+	
+	protected InstantiatingCaller icaller = null;
+    protected Instantiator  instantiator = null;
+
+    public HistoryManager() { super(); }
+    
+    /**
+     * Creates a new <code>HistoryManager</code> which communicates
+     * with a data server using the specified {@link RemoteCaller}.
+     * This {@link RemoteCaller} is first wrapped in an instance of
+     * {@link InstantiatingCaller}.
+     */
+    public HistoryManager(RemoteCaller caller)
+    {
+        super();
+        initializeService(DataServices.getInstance(caller));
+    }
+    
+    public void initializeService(DataServices services)
+    {
+        super.initializeService(services);
+        icaller = (InstantiatingCaller)
+            services.getService(InstantiatingCaller.class);
+        instantiator = icaller.getInstantiator();
+    }
+
+    public History getDataHistory(ModuleExecution mex) 
+    {
+        if (mex == null)
+            throw new IllegalArgumentException("Module execution cannot be null");
+        
+        Integer mexID = null;
+
+        try
+        {
+            mexID = new Integer(mex.getID());
+        } catch (DataException e) {
+            throw new IllegalArgumentException("Module execution must be in the database");
+        }
+        
+        return (History) 
+			icaller.dispatch(History.class,"getDataHistory",
+                        new Object[] {
+                            mexID
+                        });
+    }
+
+}
