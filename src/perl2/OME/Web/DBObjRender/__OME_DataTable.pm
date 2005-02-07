1895fa3351d8931641dd4ac9dceeeb85a5f4f960
Josiah Johnston <siah@nih.gov>
2005-02-07 16:59:38 +0000

    sets name to the table name
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5885 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Web/DBObjRender/__OME_DataTable.pm
@@ -0,0 +1,77 @@
+# OME/Web/DBObjRender/__OME_DataTable.pm
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#		Massachusetts Institute of Technology,
+#		National Institutes of Health,
+#		University of Dundee
+#
+#
+#
+#	 This library is free software; you can redistribute it and/or
+#	 modify it under the terms of the GNU Lesser General Public
+#	 License as published by the Free Software Foundation; either
+#	 version 2.1 of the License, or (at your option) any later version.
+#
+#	 This library is distributed in the hope that it will be useful,
+#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	 Lesser General Public License for more details.
+#
+#	 You should have received a copy of the GNU Lesser General Public
+#	 License along with this library; if not, write to the Free Software
+#	 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:  
+#	Josiah Johnston <siah@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Web::DBObjRender::__OME_DataTable;
+use strict;
+use OME;
+our $VERSION = $OME::VERSION;
+use base qw(OME::Web::DBObjRender);
+
+=pod
+
+=head1 NAME
+
+OME::Web::DBObjRender::__OME_DataTable
+
+=head1 METHODS
+
+=head2 _renderData
+
+sets '/name' to table_name
+
+=cut
+
+sub _renderData {
+	my ($self, $obj, $field_requests, $options) = @_;
+	my %record;
+	if( exists $field_requests->{ '/name' } ) {
+		foreach my $request ( @{ $field_requests->{ '/name' } } ) {
+			my $request_string = $request->{ 'request_string' };
+			my $name = $obj->table_name();
+			$record{ $request_string } = $self->_trim( $name, $request );
+		}
+	}
+	return %record;
+}
+
+=head1 Author
+
+Josiah Johnston <siah@nih.gov>
+
+=cut
+
+1;
