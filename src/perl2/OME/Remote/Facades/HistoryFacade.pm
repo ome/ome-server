dd6994f16f924b08c0b0710dce2a32a940eaa216
Harry Hochheiser <hsh@nih.gov>
2005-02-28 20:40:54 +0000

    sync with history branch
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@5997 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Remote/Facades/HistoryFacade.pm
@@ -0,0 +1,142 @@
+
+# OME/Remote/Facades/HistoryFacade.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Harry Hochheiser <hsh@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Remote::Facades::HistoryFacade;
+use OME;
+our $VERSION = $OME::VERSION;
+
+use OME::Remote::DTO::GenericAssembler;
+use OME::Session;
+use OME::AnalysisChainExecution;
+use OME::Tasks::HistoryManager;
+
+=head1 NAME
+
+OME::Remote::Facades::HistoryFacade - implementation of remote
+facade methods for retrieving data histories.
+
+=cut
+
+use constant DEFAULT_CHEX_SPEC =>
+  {
+   '.' => ['id','module','inputs','predecessors','timestamp'],
+   'module' => ['id','name'],
+   'predecessors' => ['id'],
+   'inputs' =>
+       ['id','input_module_execution','module_execution','formal_input',
+       'formal_output'],
+   'inputs.formal_input' =>
+       ['id','name','semantic_type'],
+   'inputs.formal_input.semantic_type' =>
+       ['id','name'],
+   'inputs.formal_output' =>
+       ['id','name','semantic_type'],
+   'inputs.formal_output.semantic_type' =>
+       ['id','name']
+
+  };
+
+
+=head1 METHODS
+
+=head2 getMexDataHistory
+
+    my @mexes =
+          OME::Remote::Facades::HistoryFacade->getMexDataHistory(
+              $mex_id,$fields_wanted)
+
+
+    Returns a list of module executions that reperesent the complete
+    data history of the module execution $mex_id. The returned module
+    executions will be encoded as DTOs, as necessary for XMLRPC
+    transmission. The actual history retrieval is done by
+    OME::Tasks::HistoryManager - this facade serves a s a thin
+    wrapper.
+
+=head2 getChainDataHistory
+
+    my @mexes =
+          OME::Remote::Facades::HistoryFacade->getChainDataHistory(
+              $chex_id,$fields_wanted)
+
+
+    Returns a list of module executions that reperesent the complete
+    data history of the chain execution $chex_id. 
+
+
+    For both calls, the $spec is optional. If no argument is provided,
+    a default specification will be used to populate the data fields.
+
+=cut 
+
+sub getMexDataHistory {
+    my ($proto,$mex_id,$spec) = @_;
+
+    $spec = DEFAULT_CHEX_SPEC
+	unless defined $spec && ref($spec) eq 'HASH';
+
+    my @mexes = OME::Tasks::HistoryManager->getMexDataHistory($mex_id);
+
+    my $dtos =
+	OME::Remote::DTO::GenericAssembler->makeDTOList(\@mexes,$spec);
+
+    return $dtos;
+}
+
+
+sub getChainDataHistory {
+    my ($proto,$chex_id,$spec) = @_;
+
+    $spec = DEFAULT_CHEX_SPEC
+	unless defined $spec && ref($spec) eq 'HASH';
+
+    my @mexes = OME::Tasks::HistoryManager->getChainDataHistory($chex_id);
+
+    my $dtos =
+	OME::Remote::DTO::GenericAssembler->makeDTOList(\@mexes,$spec);
+
+    return $dtos;
+}
+1;
+
+__END__
+=head1 AUTHOR
+
+Harry Hochheiser (hsh@nih.gov)
+
+=cut
---
+++ src/perl2/OME/Tasks/HistoryManager.pm
@@ -0,0 +1,160 @@
+# OME/Tasks/HistoryManager.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2005 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Harry Hochheiser <hsh@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Tasks::HistoryManager;
+
+
+=head1 NAME
+
+OME::Tasks::HistoryManager - retrieve data history
+
+=head1 DESCRIPTION
+
+The OME::Tasks::HistoryManager is a manager for    retrieving the data
+    derivation history of a module execution. Given a module
+    exeuction, this manager will return a list containing all of the
+    module exeuctions in the data history of that module execution. 
+
+    For each module execution included in the returned list, the
+    "inputs" list contains instances of
+    OME::ModuleExecution::ActualInput. These items indicate the
+    linkages between modules executions in this list. Taken together
+    with the module executions themselves, these actual inputs
+    provide enough information to specify the entire data history.
+
+    Note that the module execution for which history is being derived
+    is itself included in the list that is returned. This is
+    necessary because the inputs to that module execution are needed
+    for construction of the data history view.  
+
+
+=head1 METHODS (ALPHABETICAL ORDER)
+
+The following methods are available to a "HistoryManager."
+
+=head2 getMexDataHistory($mex_id)
+
+Retrieve the data historyfor the specified mex.
+
+=head2 getChainDataHistory($chex_id)
+
+Retrieve the data historyfor the specified chain execution.
+
+=cut
+
+use strict;
+use OME::SetDB;
+use OME::DBObject;
+use OME::ModuleExecution;
+use OME::AnalysisChainExecution::NodeExecution;
+OME::DBObject->Caching(0);
+use Carp;
+
+use OME;
+our $VERSION = $OME::VERSION;
+
+   
+sub new{
+	my $class=shift;
+	my $self={};
+
+	return bless($self,$class);
+}
+
+sub getMexDataHistory {
+    my ($class,$mex_id)= @_;
+
+    # get factory
+    my $session=OME::Session->instance();
+    my $factory = $session->Factory();
+    
+
+     # retrieve the module execution
+     my $mex= $factory->loadObject('OME::ModuleExecution',$mex_id)
+	    or confess "Couldn't load module execution $mex_id";
+
+    #mark it in a hash
+
+    # return all module executions that we have seen.
+    return _getMexClosures($mex);
+}
+
+sub getChainDataHistory {
+     my ($class,$chex_id) = @_;
+
+    # get factory
+    my $session=OME::Session->instance();
+    my $factory = $session->Factory();
+
+
+    # get all of the module executions that I can for this chex
+    # first, get node executions
+    my @nexes = $factory->findObjects(
+	'OME::AnalysisChainExecution::NodeExecution',
+        { analysis_chain_execution_id => $chex_id });
+
+    # then, get module executions for those.
+    my @mexes = map { $_->module_execution()} @nexes;
+
+    # filter out anything that's null
+    @mexes = grep (($_),@mexes);
+
+    # use these as the input.
+    return _getMexClosures(@mexes);
+}
+
+sub _getMexClosures {
+    my (@mexes) = @_;
+
+    my %mexes;
+    my $mex;
+    my @preds;
+
+    while (scalar(@mexes) > 0 ) {
+	$mex = pop @mexes;
+	if (!exists($mexes{$mex->ID()})) {
+	    $mexes{$mex->ID()} = $mex;
+	    @preds = $mex->predecessors();
+	    push @mexes,@preds;
+	}
+	
+    }
+    return values %mexes;
+}
+
+1;
+
