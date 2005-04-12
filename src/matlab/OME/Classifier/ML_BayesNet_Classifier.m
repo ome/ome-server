3d2a1fce973919ad18648f10c5b27ac5cd3016da
Tomas Macura <tmacura@nih.gov>
2005-04-12 18:09:54 +0000

    added sanity check. Fixed discretization step
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6087 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/matlab/OME/Classifier/ML_BayesNet_Classifier.m
+++ src/matlab/OME/Classifier/ML_BayesNet_Classifier.m
@@ -14,15 +14,18 @@
 %                      probabilities the instance belongs to those classes
 
 function [marginal_probs] = ML_BayesNetClassifier (bnet, contData, sigs_used, discWalls);
+% sanity checks
+if (length(contData) ~= length(sigs_used)) 
+	error ("contData and sigs_used must be the same size\n");
+end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % use learned wall placements in discretization                 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 discData = ones(size(contData)); 
-for i = 1:length (sigs_used)
-	for j = 1:length(discWalls{j})
-		discData(i,:) = discData(i,:) + (contData(i,:) > discWalls{i}(j));
+for i = 1:length(sigs_used)
+	for j = 1:length(discWalls{sigs_used(i)})
+		discData(i) = discData(i) + (contData(i) > discWalls{sigs_used(i)}(j));
 	end
 end
 
+% use discretized vector in classification
 marginal_probs = BayesNetClassifier (bnet, discData);
