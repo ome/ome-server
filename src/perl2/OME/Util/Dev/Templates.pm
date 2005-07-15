0dc838a8114aadea1530f393ea2668bb2a711c93
Josiah Johnston <siah@nih.gov>
2005-07-15 21:17:43 +0000

    A utility for scanning a template directory and creating template attributes.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6375 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Util/Dev/Templates.pm
@@ -0,0 +1,201 @@
+# OME/Util/Templates.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institute of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Josiah Johnston <siah@nih.gov>
+#
+#-------------------------------------------------------------------------------
+
+package OME::Util::Templates;
+
+use strict;
+use OME;
+our $VERSION = $OME::VERSION;
+
+use base qw(OME::Util::Commands);
+
+use Carp;
+use OME::Tasks::ModuleExecutionManager;
+
+use Getopt::Long;
+Getopt::Long::Configure("bundling");
+
+
+
+sub getCommands {
+    return
+      {
+       'update' => 'update',
+      };
+}
+
+sub update_help {
+    my ($self,$commands) = @_;
+    my $script = $self->scriptName();
+    my $command_name = $self->commandName($commands);
+    
+    $self->printHeader();
+    print <<"USAGE";
+Usage:
+    $script $command_name [<options>]
+
+This utility will scan the html template directory and register each
+template file in the database by creating template attributes. Files
+already registered will be ignored.
+
+Options:
+
+	-u all          Update all template directories.
+	-u Display/One  Update the Display/One directory
+	-u Display/Many Update the Display/Many directory
+
+USAGE
+    CORE::exit(1);
+}
+
+
+sub update {
+	my ($self,$commands) = @_;
+	my %allowed_paths = (
+		'Display/One'  => [ 'updateDisplayTemplates', 'one' ],
+		'Display/Many' => [ 'updateDisplayTemplates', 'many' ],
+	);
+	my $session = $self->getSession();
+	my $factory = $session->Factory();
+	
+	my( $update_dir );
+	GetOptions('u=s' => \$update_dir);
+
+	if( not defined $update_dir ) {
+		die "You must specify a directory to update.";
+	} elsif( $update_dir eq 'all' ) {
+		$update_dir = [ keys %allowed_paths ];
+	} elsif( $update_dir =~ m/,/ ) {
+		$update_dir = [ split( m/,/, $update_dir ) ];
+	} else {
+		$update_dir = [ $update_dir ];
+	}
+	
+	foreach my $tmpl_dir ( @$update_dir ) {
+		die "Template directory $tmpl_dir is unknown or unallowed"
+			unless( exists $allowed_paths{ $tmpl_dir } );
+		print "updating $tmpl_dir...\n";
+		my $tmpl_root = $session->Configuration()->template_dir();
+		my ( $function, @extra_params ) = @{ $allowed_paths{ $tmpl_dir } };
+		my( $mex, @new_tmpl_attrs) = $self->$function( $tmpl_root.'/'.$tmpl_dir, @extra_params );
+		print "Created ".scalar(@new_tmpl_attrs)." new templates. MEX id is ".$mex->id."\n";
+	}
+}
+
+sub updateDisplayTemplates {
+	my ($self, $tmpl_dir, $arity) = @_;
+	my $session = $self->getSession();
+	my $factory = $session->Factory();
+	my $module = $factory->findObject( 'OME::Module', name => 'Global import' )
+		or die "couldn't laod Global import module";
+	my $mex = OME::Tasks::ModuleExecutionManager->createMEX($module,'G' )
+		or die "Couldn't get mex for Global import";
+	my @new_tmpls;
+	
+	print "\tScanning $tmpl_dir\n";
+	my @file_paths = get_template_files( $tmpl_dir, 1);
+	foreach my $path (@file_paths) {
+		# parse the path
+		my $file = $path;
+		$file =~ s/$tmpl_dir\/?//;
+		my @parts = split( /\//, $file );
+		$file = pop @parts;
+	
+		# figure out mode & object type
+		( my $mode = $file ) =~ s/\.tmpl//;
+		my $object_type;
+		# a DBObject template?
+		if( @parts && $parts[0] eq 'OME' ) {
+			$object_type = join( '::', @parts );
+		# or a ST template?
+		} elsif( scalar( @parts) > 0 ) {
+			$object_type = '@'.$parts[0];
+		}
+		# object_type will be blank IFF it's a generic template
+		unless ($object_type) {
+			$object_type = '_generic';
+			$mode        =~ s/generic_//;
+		}
+	
+		# get the file contents
+#		open( FH, "< $path" );
+#		my $file_contents;
+#		while( defined( my $line = <FH> ) ) {
+#			$file_contents .= $line;
+#		}
+#		close(FH);
+	
+		my %data_hash = (
+			Name       => "(default)",
+			Arity      => $arity,
+			Mode       => $mode,
+			ObjectType => $object_type,
+#			Template   => $file_contents
+			Template   => $path
+		);
+		
+		# turn all this into a new DisplayTemplate?
+		unless( $factory->findAttribute( "DisplayTemplate", \%data_hash ) ) {
+			print "\tCreating new template attribute for $path\n";
+			my $new_tmpl = $factory->newAttribute( "DisplayTemplate", undef, $mex, \%data_hash ) 
+				or die "Couldn't make a new DisplayTemplate for $path";
+			push( @new_tmpls, $new_tmpl );
+		}
+	}
+	$mex->status( 'FINISHED' );
+	$mex->storeObject();
+	$session->commitTransaction();
+	return ($mex, @new_tmpls );
+}
+
+sub get_template_files {
+	my $root_dir = shift;
+	my $depth = shift;
+	opendir( DH, $root_dir );
+	my( @files, @dirs);
+	while( defined (my $file = readdir DH )) {
+		next if $file =~ m/^\.|CVS/;
+		my $full_path = $root_dir."/".$file;
+		if( $file =~ m/\.tmpl$/ ) {
+			push @files, $full_path;
+		} else {
+			push @dirs, $full_path;		
+		}
+	}
+	closedir( DH);
+	push @files, get_template_files( $_, $depth+1 ) foreach @dirs;	
+	return @files;
+}
