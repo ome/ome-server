cc47b9701ae76961a00acc4392c2b280b7eb293a
anagaraja <anagaraja@unknown>
2005-08-09 14:59:43 +0000

    Removed duplicate methods.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6435 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Tasks/CategoryManager.pm
+++ src/perl2/OME/Tasks/CategoryManager.pm
@@ -195,6 +195,18 @@ sub classifyImage {
 		return $classification;
 	}
 
+# my @good_categories;
+# my @params;
+# foreach my $good_category (@good_categories) {
+# 	push( @params, 'Classification' );
+# 	push( @params, { 
+# 		Category   => $good_category,
+# 		Valid      => 1,
+# 		Confidence => 1
+# 	} );
+# }
+# my ( $mex, $attrs ) = OME::Tasks::AnnotationManager->annotateImage( $image, @params );
+
 	my ( $mex, $attrs ) = OME::Tasks::AnnotationManager->
 		annotateImage( $image, 'Classification', 
 		{ 
@@ -255,16 +267,6 @@ sub getImageClassification {
 	return $classifications[0]
 		if( @classifications eq 1 );
 	return undef;
-}
-
-
-sub countUnclassifiedImagesInDataset {
-	my ($proto, $categoryGroup, $dataset) = @_;
-	my $num_images_unclassified = $dataset->count_images() - $dataset->count_images( 
-		'image.ClassificationList.Valid' => 't', 
-		'image.ClassificationList.Category.CategoryGroup' => $categoryGroup->id, 
-		__distinct => 'image_id' );
-	return $num_images_unclassified;
 }
 
 =head2 getUnclassifiedImagesInDataset()
@@ -278,156 +280,6 @@ Returns the images in $dataset that are not classified by $categoryGroup.
 
 
 =cut
-
-=head2 getClassificationsInDataset()
-
-	my @classificationsForCG = OME::Tasks::CategoryManager->
-		getClassificationsInDataset($categoryGroup, $dataset);
-	my @classificationsForCategory = OME::Tasks::CategoryManager->
-		getClassificationsInDataset($category, $dataset);
-
-Returns the classifications for images in $dataset that are belong to
-$categoryGroup or $category.
-
-=cut
-
-sub getClassificationsInDataset {
-	my ($proto, $cSomething, $dataset) = @_;
-	my $factory = OME::Session->instance()->Factory();
-	if( $cSomething->semantic_type->name eq 'CategoryGroup' ) {
-		return $factory->findObjects( '@Classification', {
-			'Valid'                          => 't',
-			'Category.CategoryGroup'         => $cSomething,
-			'image.dataset_links.dataset_id' => $dataset,
-			'__order'                        => 'image.name'
-		} );
-	} elsif( $cSomething->semantic_type->name eq 'Category' ) {
-		return $factory->findObjects( '@Classification', {
-			'Valid'                          => 't',
-			'Category'                       => $cSomething,
-			'image.dataset_links.dataset_id' => $dataset,
-			'__order'                        => 'image.name'
-		} );
-	} else {
-		die "parameter $cSomething not recognized";
-	}
-}
-
-=head2 classifyImage()
-
-	OME::Tasks::CategoryManager->classifyImage($image, $category);
-
-Returns:
-	a Category if the image is classified under other categories in this category's group
-	this Category if the image is already classified with this Category
-	the classification attribute if successful
-
-=cut
-
-sub classifyImage {
-	my ($proto, $image, $category) = @_;
-	my $session = OME::Session->instance();
-	my $factory = $session->Factory();
-
-	my @other_categories_in_this_group = 
-		$category->CategoryGroup->CategoryList( id => [ '!=', $category->id ] );
-	# Don't add this image if it belongs to another image in this Category Group.
-	# Don't bother searching if there are no other categories in this group;
-	# DBObject can't handle empty lists with the 'in' operator.
-	my @other_classifications = ( scalar ( @other_categories_in_this_group ) > 0 ?
-		$factory->findObjects( '@Classification', {
-			image                           => $image,
-			'module_execution.experimenter' => $session->User(),
-# for some unknown reason, this next parameter consistently fails with
-# "DBD::Pg::st execute failed: ERROR:  parser: parse error at or near "'" at /Users/josiah/OME/cvs/OME/src/perl2//OME/Factory.pm line 1069."
-# so i'll ignore it and grep it out at the next step
-#					Valid                           => [ "is not", 0 ],
-			Category => [ 'in', [ @other_categories_in_this_group] ]
-		} ) :
-		()
-	);
-	@other_classifications = grep( ( (not defined $_->Valid) || ( $_->Valid eq 1 ) ), @other_classifications );
-	if( @other_classifications ) {
-		return $other_classifications[0]->Category();
-	}
-	
-	# skip if the image has already been classified with this classification
-	if( my $classification = $factory->findObject( '@Classification', {
-		Category => $category,
-		image    => $image,
-		'module_execution.experimenter' => $session->User(),
-	} ) ) {
-		return $category if $classification->Valid();
-		# Mimic Shoola behavior by remarking this classification as valid 
-		# if the user is changing their mind for the second time.
-		$classification->Valid( 1 );
-		$classification->storeObject();
-		$session->commitTransaction();
-		return $classification;
-	}
-
-	my ( $mex, $attrs ) = OME::Tasks::AnnotationManager->
-		annotateImage( $image, 'Classification', 
-		{ 
-			Category   => $category,
-			Valid      => 1,
-			Confidence => 1
-		}
-		);
-	return $attrs->[0];
-}
-
-=head2 declassifyImage()
-
-	OME::Tasks::CategoryManager->declassifyImage($image, $category);
-
-declassifies an image.
-
-=cut
-
-sub declassifyImage {
-	my ($proto, $image, $category ) = @_;
-	my $session = OME::Session->instance();
-	my $factory = $session->Factory();
-	my $classification = $factory->findObject( 
-		'@Classification', 
-		Category => $category,
-		image    => $image
-	) or die "Couldn't find a classification for this category & image";
-	$classification->Valid( 0 );
-	$classification->storeObject();
-	$session->commitTransaction();
-}
-
-=head2 getImageClassification()
-
-	my $classification = OME::Tasks::CategoryManager->
-		getImageClassification($image, $categoryGroup);
-
-Find the classification for the image in this categoryGroup. If there is
-more than one valid classification found, this method will return an
-array reference. If exactly one valid classification is found, it will
-return it. If none are found, it will return undef.
-
-=cut
-
-sub getImageClassification {
-	my ($proto, $image, $categoryGroup ) = @_;
-	my $session = OME::Session->instance();
-	my $factory = $session->Factory();
-	my @classifications = $factory->findObjects( 
-		'@Classification', 
-		'Category.CategoryGroup' => $categoryGroup,
-		image                    => $image
-	);
-	@classifications = grep( ( ( not defined $_->Valid ) || $_->Valid ne 0 ), @classifications );
-	return \@classifications
-		if( @classifications > 1 );
-	return $classifications[0]
-		if( @classifications eq 1 );
-	return undef;
-}
-
 
 =head2 createGroupAndCategories()
 
