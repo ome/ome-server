b731af967efae539a22b008ab7e120fb962e8ffe
Tomas Macura <tmacura@nih.gov>
2005-08-27 23:01:32 +0000

    this is an extensive test of your OMEIS/MATLAB interface
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6522 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/matlab/Tests/omeis_mex_test.m
@@ -0,0 +1,66 @@
+% Tom Macura August 27th, 2005
+%
+% This test exploit's OMEIS SHA1 based pixels redundancy check to test if the
+% getPixels, setPixels, getROI, setROI commands work correctly.
+% It is following a convolution path which essentially takes a PixelsSet and
+% clones it. We know the clone process happened correctly if the PixelsID
+% returned by finishPixels matches the original PixelID
+
+function omeis_mex_test ()
+
+% I am assuming that OMEIS is hosted on the local computer
+[s, hostname] = system('hostname');
+hostname = hostname(1:end-1);
+
+% This is hacked code and is evil.
+% It queries the ome database to figure out what are the valid OMEIS pixel ids
+perl_script = fopen ('omeis_test_helper.pl', 'w');
+fprintf(perl_script, 'use strict;\nmy $inf = "ids_tmp";\nmy $ouf = "ids_tmp_clean";\nopen (FILE_IN, "< $inf") or die;\nopen (FILE_OUT, "> $ouf") or die;\nwhile (<FILE_IN>) {\n    print FILE_OUT $1." " if m/\\s*(\\d+)\\s*/;\n}\nclose(FILE_IN);\nclose(FILE_OUT);\n');
+fclose(perl_script);
+
+system('/usr/local/bin/psql -d ome -o ids_tmp -c "SELECT image_server_id FROM image_pixels"');
+perl('omeis_test_helper.pl');
+ids = load('ids_tmp_clean');
+system('rm ids_tmp ids_tmp_clean omeis_test_helper.pl');
+
+fprintf ('Extracted Info about your OME install\n');
+fprintf ('            OMEIS URL: %s\n', hostname);
+fprintf ('   OMEIS Pixels Count: %d\n\n', length(ids));
+
+fprintf ('Starting OME/MATLAB Test:');
+
+for pix_id = ids
+    fprintf ('Processing PixelsID %d:\n', pix_id);
+    
+    fprintf ('\tPixelsInfo ...'); im_struct = pixelsInfo (is, pix_id);   fprintf (' done\n');
+    fprintf ('\tnewPixels ...');  n_pix_id = newPixels (is, im_struct);  fprintf (' done\n');
+    
+    % get image and remove top left corner
+    fprintf ('\tgetPixels ...');  im = getPixels(is, pix_id);            fprintf (' done\n');
+    % imshow(im, [min(min(im)) max(max(im))]);
+    
+    [sizeX, sizeY, sizeZ, sizeC, sizeT] = size(im);
+    im (1:floor(sizeX/2), 1:floor(sizeY/2)) = 0;
+    
+    % set image (without top left corner) on OMEIS
+    fprintf ('\tsetPixels ...');  setPixels(is, n_pix_id, im);           fprintf (' done\n');
+    
+    % get top left corner and set it on the new imae
+    fprintf ('\tgetROI ...');
+    im_q = getROI(is, pix_id, 0, 0, 0, 0, 0, floor(sizeX/2)-1, floor(sizeY/2)-1, 0, 0, 0);
+    fprintf (' done\n');
+    %imshow(im_q, [min(min(im_q)) max(max(im_q))]);
+    
+    fprintf ('\tsetROI ...');
+    num_pixels = setROI (is, n_pix_id, 0, 0, 0, 0, 0, floor(sizeX/2)-1, floor(sizeY/2)-1, 0, 0, 0, im_q);;
+    fprintf (' done\n');
+    
+    fprintf ('\tfinishPixels ...'); n_pix_id = finishPixels (is, n_pix_id); fprintf (' done\n');
+    % figure; imshow(getPixels(is,n_pix_id), [min(min(im)) max(max(im))]);
+    
+    if (n_pix_id == pix_id)
+       fprintf ('TEST PASSED \n\n');
+    else
+       error ('TEST FAILED New PixelsID is %d\n\n', n_pix_id);
+    end
+end
