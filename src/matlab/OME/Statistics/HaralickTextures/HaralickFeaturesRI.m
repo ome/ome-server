f06885f05291f87eb574bcdced8ac5b421da4c91
Tomas Macura <tmacura@nih.gov>
2005-09-15 10:21:05 +0000

    ++ Haralick Features 2D Rotation Invariant
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6598 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/matlab/OME/Statistics/HaralickTextures/HaralickFeaturesRI.m
@@ -0,0 +1,48 @@
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% Copyright (C) 2003 Open Microscopy Environment
+%       Massachusetts Institue of Technology,
+%       National Institutes of Health,
+%       University of Dundee
+%
+%
+%
+%    This library is free software; you can redistribute it and/or
+%    modify it under the terms of the GNU Lesser General Public
+%    License as published by the Free Software Foundation; either
+%    version 2.1 of the License, or (at your option) any later version.
+%
+%    This library is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%    Lesser General Public License for more details.
+%
+%    You should have received a copy of the GNU Lesser General Public
+%    License along with this library; if not, write to the Free Software
+%    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Written by:  Tom Macura <tmacura@nih.gov>
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% HaralickFeatures2D accepts an optional input of rotation angle. If the input
+% is unspecified, it will compute features on four angles and return the average.
+% If the input is specified, it will return the Haralick features for that angle.
+
+function [haralick_avg_and_range] = HaralickFeatures2D (Im, dist);
+
+if nargin < 2
+	dist = 1;
+end
+
+haralick_features(:,1) = mb_texture (Im, dist, 0);
+haralick_features(:,2) = mb_texture (Im, dist, 45);
+haralick_features(:,3) = mb_texture (Im, dist, 90);
+haralick_features(:,4) = mb_texture (Im, dist, 135);
+
+haralick_avg = mean(haralick_features, 2);					
+haralick_range = range(haralick_features, 2);
+
+haralick_avg_and_range = [haralick_avg; haralick_range];
