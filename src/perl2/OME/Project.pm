d09be4dc9d086f7d659a208307896410a0e1a252
Josiah Johnston <siah@nih.gov>
2005-09-28 18:22:01 +0000

    Removed several depricated methods. All methods removed had been marked "depricated" for some time, and were not referenced elsewhere in the codebase.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6625 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Project.pm
+++ src/perl2/OME/Project.pm
@@ -128,13 +128,7 @@ __PACKAGE__->manyToMany('datasets',
 __PACKAGE__->addACL({user => 'owner_id',group => 'group_id'});
 
 
-sub unlockedDatasets {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-	my $self = shift;
-	return grep {not $_->locked()} $self->datasets();
-}
-
+# Referenced by line 412 of OME::Remote::Prototypes
 sub addDataset {
     carp "DEPRECATED! Please use the ProjectManager";
 
@@ -168,107 +162,6 @@ sub addDataset {
 	}
 
 	return $dataset;
-}
-
-sub addDatasetID {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-	
-	my $self = shift;
-	my $datasetID = shift;
-	my $factory=$self->Session()->Factory();
-	#my $dataset = OME::Dataset->retrieve ($datasetID);
-	my $dataset	=$factory->loadObject("OME::Dataset",$datasetID);
-	return $self->addDataset($dataset);
-}
-
-# stub for future development
-sub deleteDataset {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-
-}
-
-# stub for future development
-sub deleteDatasetID {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-
-}
-
-# stub for future development
-sub removeDataset {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-
-}
-
-# stub for future development
-sub removeDatasetID {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-
-}
-
-# returns 1 if this project contains the dataset, else returns undef
-sub doesDatasetBelong {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-	my $self = shift;
-	my $dataset = shift;
-	my $factory=$self->Session()->Factory();
-	return undef unless defined $dataset;
-	my @datasets =$factory->findObjects("OME::Project::DatasetMap",
-				 dataset_id => $dataset->ID(), 
-				 project_id => $self->ID()
-				);
-
-
-
-	#my @datasets = OME::Project::DatasetMap->search( dataset_id => $dataset->ID(), project_id => $self->ID() );
-	return 1 if scalar @datasets > 0;
-	return undef;
-}
-
-# extension of doesDatasetBelong
-sub doesDatasetBelongID {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-	my $self = shift;
-	my $datasetID = shift;
-	my $dataset = $self->Session()->Factory()->loadObject("OME::Dataset",$datasetID);
-	return $self->doesDatasetBelong($dataset);
-}
-
-
-sub newDataset {
-    carp "DEPRECATED! Please use the ProjectManager";
-
-    my $self = shift;
-    my $datasetName = shift;
-    my $datasetDescription = shift if @_ > 0;
-
-    my $factory = $self->Session()->Factory();
-    my $owner = $factory->loadAttribute("Experimenter",$self->owner_id());
-    my $group = $owner->Group();
-    my $dataset=$factory->newObject("OME::Dataset",{
-		name        => $datasetName,
-		description => $datasetDescription,
-		locked      => 'false',
-		owner_id    => $owner->id(),
-		group_id    => $group->id(),
-	} );
-
-	#my $dataset = OME::Dataset->create ( {
-	#	name        => $datasetName,
-	#	description => $datasetDescription,
-	#	locked      => 'false',
-	#	owner_id    => $owner->id(),
-	#	group_id    => $group->id(),
-	#} )
-	#	or die ref($self)."->newDataset:  Could not create a new dataset.\n";
-	
-	return $self->addDataset($dataset);
 }
 
 1;
