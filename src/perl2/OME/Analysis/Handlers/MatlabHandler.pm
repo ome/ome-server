d842c3973799fd1af8d0c4e4f8a6f9c8cec22a9f
Josiah Johnston <siah@nih.gov>
2005-10-06 15:35:06 +0000

    oops. fixed copy & paste error
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6661 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Analysis/Handlers/MatlabHandler.pm
+++ src/perl2/OME/Analysis/Handlers/MatlabHandler.pm
@@ -385,12 +385,12 @@ sub _getScalarFromMatlab {
 	                        "($name);") if defined $class;
 	
 	# Trimming required to avoid overflow and underflow problems with Postgress
-	$engine->eval("if (strcmp(class($name), 'double') && abs($name) < realmin('double')) $name = double(0); end; ".
-	              "if (strcmp(class($name), 'single') && abs($name) < realmin('single')) $name = single(0); end; ".
-	              "if (strcmp(class($name), 'double') && $name < -realmax('double')) $name = double(-inf); end; ".
-	              "if (strcmp(class($name), 'single') && $name < -realmax('single')) $name = single(-inf); end; ".
-	              "if (strcmp(class($name), 'double') && $name > realmax('double')) $name = double(inf); end; ".
-	              "if (strcmp(class($name), 'single') && $name > realmax('single')) $name = single(inf); end; "
+	$self->{__engine}->eval("if (strcmp(class($name), 'double') && abs($name) < realmin('double')) $name = double(0); end; ".
+	                        "if (strcmp(class($name), 'single') && abs($name) < realmin('single')) $name = single(0); end; ".
+	                        "if (strcmp(class($name), 'double') && $name < -realmax('double')) $name = double(-inf); end; ".
+	                        "if (strcmp(class($name), 'single') && $name < -realmax('single')) $name = single(-inf); end; ".
+	                        "if (strcmp(class($name), 'double') && $name > realmax('double')) $name = double(inf); end; ".
+	                        "if (strcmp(class($name), 'single') && $name > realmax('single')) $name = single(inf); end; "
 	);
 	
 	# Get value from matlab
