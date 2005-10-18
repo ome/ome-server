56b8a3a8580e2fc6a241f4848179cf241e1f9e7f
Josiah Johnston <siah@nih.gov>
2005-10-18 15:11:14 +0000

    Redirect to Login instead if Home.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6709 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/Logout.pm
+++ src/perl2/OME/Web/Logout.pm
@@ -59,7 +59,7 @@ sub getPageBody {
     $self->Manager()->logout($self->Session());
 	$self->setSessionCookie();
 
-	my $body = $self->redirect($self->pageURL('OME::Web::Home'));
+	my $body = $self->redirect($self->pageURL('OME::Web::Login'));
 
     return ('HTML',$body);
 }
