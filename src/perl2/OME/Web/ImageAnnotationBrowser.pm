f89ee33bd40d22481e4977e78cf4b9bf9c2507aa
Harry Hochheiser <hsh@nih.gov>
2005-10-20 20:15:40 +0000

    initial cut for browsing image annotations
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6719 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Web/ImageAnnotationBrowser.pm
@@ -0,0 +1,146 @@
+# OME/Web/ImageAnnotationBrowser.pm
+
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institue of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Harry Hochheiser <hsh@nih.gov>, based on code in
+#                CG_Annotator.pm by Arpun Nagaraja <arpun@mit.edu>
+#
+#-------------------------------------------------------------------------------
+
+
+package OME::Web::ImageAnnotationBrowser;
+
+use strict;
+use Carp;
+use Carp 'cluck';
+use vars qw($VERSION);
+use OME::SessionManager;
+
+use base qw(OME::Web);
+
+sub getPageTitle {
+    return "OME: Image Annotation Browser";
+}
+
+{
+    my $menu_text = "Image Annotation Browser";
+    sub getMenuText { return $menu_text }
+}
+
+sub getPageBody {
+    my $self = shift ;
+    my $q = $self->CGI() ;
+    my $session= $self->Session();
+    my $factory = $session->Factory();
+    my %tmpl_data;
+
+    # Load the correct template and make sure the URL still carries the template
+    # name.
+    
+    my $geneName = $q->param('GeneName');
+
+
+    return $self->getGeneChooser() unless (defined $geneName);
+
+    my $which_tmpl = $q->url_param('Template');
+    my $referer = $q->referer();
+    my $url = $self->pageURL('OME::Web::ImageAnnotationBrowser');
+    if ($referer =~ m/Template=(.+)$/ && !($which_tmpl)) {
+	$which_tmpl = $1;
+	$which_tmpl =~ s/%20/ /;
+	return ('REDIRECT', $self->redirect($url.'&Template='.$which_tmpl));
+    }
+    $which_tmpl =~ s/%20/ /;
+    my $tmplData = $factory->findObject( '@BrowseTemplate', Name => $which_tmpl );
+	
+    my $tmpl = HTML::Template->new(filename => $tmplData->Template(),
+				case_sensitive=>1);
+    $tmpl_data{'GeneName'} = $geneName;
+    $tmpl_data{'Template'} = $q->param('Template');
+    $tmpl->param(%tmpl_data);
+
+    my $gene = $factory->findObject('@Gene',  Name => $geneName);
+    print STDERR "Gene id is " .$gene->ID() . "\n";
+    my @probes = $factory->findObjects('@ProbeGeneMap', {Gene =>$gene});
+    print STDERR "Probes found : " . scalar(@probes) . "\n";
+
+    
+    my @loop_data;
+
+    while (@probes) {
+	my %row_data;	
+	my $probe = shift @probes;
+	print STDERR "seeeing probe " .$probe->id .".\n";
+	$row_data{'Probe'} = $probe->ID;
+	push(@loop_data,\%row_data);
+    }
+    $tmpl_data{'ProbeGenes'} = \@loop_data;
+
+    $tmpl->param(%tmpl_data);
+    my $html =
+	$q->startform( { -name => 'primary' } );
+    $html .= $tmpl->output() if ($tmpl);
+    $html .= $q->endform();
+
+    return ('HTML',$html);	
+
+}
+
+sub getGeneChooser {
+    my $self=shift;
+    my $q = $self->CGI();
+    my $session= $self->Session();
+    my $factory = $session->Factory();
+
+    my @genes = $factory->findObjects('@Gene');
+    my $url = $self->pageURL('OME::Web::ImageAnnotationBrowser');
+    my $directions = "Choose a gene ";
+    my $template = $q->param('Template');
+    my $popup =  $q->popup_menu(
+	-name => 'GeneChoice',
+	-values => [ map($_->Name,@genes)]);
+
+    if (defined $q->param('ChooseGene')) {
+	my $full = $url.'&Template='.$template.'&GeneName='.
+	    $q->param('GeneChoice');
+	return('REDIRECT',$full) 
+    }
+
+    my $button = $q->submit(-name=>'ChooseGene',-value=>'Submit');
+    my $html = $q->startform({ -name=>'primary'}) .
+        $q->hidden(-name=>'Template',-default=>$template)  .
+	$directions. $popup . $button. $q->endform();
+    return ('HTML',$html);
+}
+
+
+
+1;
