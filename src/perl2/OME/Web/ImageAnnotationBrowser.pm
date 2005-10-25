adb97f7e99c09412d43ef056e4d8cbef056f48bd
Harry Hochheiser <hsh@nih.gov>
2005-10-25 14:02:45 +0000

    generic handling of multiple levels
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@6733 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Web/ImageAnnotationBrowser.pm
+++ src/perl2/OME/Web/ImageAnnotationBrowser.pm
@@ -54,6 +54,42 @@ sub getPageTitle {
     sub getMenuText { return $menu_text }
 }
 
+
+=head1 getPageBody
+
+Get the body for a page that displays images associated with given
+    annotations. The annotations to be displayed will be given by the
+    content of a Template file - as specified in the Template
+    parameter of the requesting URL.
+
+  
+This template file will contain a TMPL_VAR of the name
+    "Path.load/types-[....]". Inside the brackets, this variable name
+    will include a list of types that will be used to define a path
+    leading from some root to sets of images.
+
+The list of types will alternate between objects and maps. An
+    event-numbered list of types will always be included - starting with
+    a root, followed by a map type, a subsequent type, etc., and
+    ending with a map that leads to OME::Image. Since OME::Image is
+    always the last element, it's presence in the list is implied.
+    
+Thus, for example, a list of the form 
+    [@Gene,@ProbeGeneMap,@Probe,@ImageProbe] will start with
+    an instance of the Gene ST, use ProbeGeneMap to find a set of
+    probes, and ImageProbe to find a set of images for each probe.
+
+It is assumed that this module is also called with a query parameter
+    Root indicating the value (name) of the first ST in the list. Thus,
+    Gene=Mga will find all of the probes and eventually images
+    associated with the name "Mga". The value given for root is
+    assumed to be the Name of the instance of the first ST.
+
+   
+    
+=cut
+
+
 sub getPageBody {
     my $self = shift ;
     my $q = $self->CGI() ;
@@ -61,106 +97,175 @@ sub getPageBody {
     my $factory = $session->Factory();
     my %tmpl_data;
 
-    # Load the correct template and make sure the URL still carries the template
-    # name.
-    
-    my $geneName = $q->param('GeneName');
-
-
-    return $self->getGeneChooser() unless (defined $geneName);
+    # Load the correct template and make sure the URL still carries
+    # the template  name.
+    # get template from url parameter, or referer
 
     my $which_tmpl = $q->url_param('Template');
     my $referer = $q->referer();
     my $url = $self->pageURL('OME::Web::ImageAnnotationBrowser');
-    if ($referer =~ m/Template=(.+)$/ && !($which_tmpl)) {
+    if ($referer && $referer =~ m/Template=(.+)$/ && !($which_tmpl)) {
 	$which_tmpl = $1;
 	$which_tmpl =~ s/%20/ /;
 	return ('REDIRECT', $self->redirect($url.'&Template='.$which_tmpl));
     }
     $which_tmpl =~ s/%20/ /;
-    my $tmplData = $factory->findObject( '@BrowseTemplate', Name => $which_tmpl );
+
+    # load the appropriate information for the named template.
+    my $tmplData = 
+	$factory->findObject( '@BrowseTemplate', Name => $which_tmpl );
 	
-    my $tmpl = HTML::Template->new(filename => $tmplData->Template(),
-				case_sensitive=>1);
-    $tmpl_data{'GeneName'} = $geneName;
-    $tmpl_data{'Template'} = $q->param('Template');
-    $tmpl->param(%tmpl_data);
-
-    my $gene = $factory->findObject('@Gene',  Name => $geneName);
-    print STDERR "Gene id is " .$gene->ID() . "\n";
-    my @pgmaps = $factory->findObjects('@ProbeGeneMap', {Gene =>$gene});
-    print STDERR "Probes found : " . scalar(@pgmaps) . "\n";
-
+    # instantiate the template
+    my $tmpl = 
+	HTML::Template->new(filename => $tmplData->Template(),
+			    case_sensitive=>1);
     
-    my @loop_data;
+   # get the root object
+    my $root = $q->param('Root');
 
-    while (@pgmaps) {
-	my %row_data;	
-	my $pgmap = shift @pgmaps;
-	my $probe = $pgmap->Probe;
-	$row_data{'Probe'} = $probe->Name;
-	my $images = $self->getImageDisplay($probe);
-	$row_data{'ProbeImages'} = $images;
-	push(@loop_data,\%row_data);
-    }
-    $tmpl_data{'ProbeGenes'} = \@loop_data;
+    # instantiate variables in the template
+    $tmpl_data{'Root'} = $root;
+    $tmpl_data{'Template'} = $q->param('Template');
 
+	
+    # get a parsed array of the types in the path variable.
+    my $pathTypes= $self->getPaths($tmpl);
+    
+    
+    # find the instances associated with root. 
+    # NOTE: We might want to change this if we revise to handle 
+    # _all_ instances of the first class
+    my $pathElt = shift @$pathTypes;
+    my $rootObj = $factory->findObject($pathElt, Name=>$root);
+
+    # get the associated layout code
+    # strip of preceding '@'
+    $pathElt =~ /@(.*)/;
+    my $data_html= $self->getLayoutCode($rootObj,$pathTypes,$1);
+
+    # populate the template..
     $tmpl->param(%tmpl_data);
+    # and the form.
     my $html =
 	$q->startform( { -name => 'primary' } );
+    $html .=  $data_html if ($data_html);
     $html .= $tmpl->output() if ($tmpl);
     $html .= $q->endform();
 
     return ('HTML',$html);	
 }
 
-sub getImageDisplay {
+=head1 getPaths
+
+    Find the template parameter named "Path.load/types-[...]",
+    parse out the list of types inside the brackets, and return an
+    array reference.
+    
+
+=cut
+sub getPaths {
+    my $self= shift;
+    my $tmpl = shift;
+
+    my @parameters = $tmpl->param();
+    my @found_params = grep (m/\.load/,@parameters);
+    my $path = $found_params[0];
+
+    $path =~ m/Path.load\/types-\[(.*)\]/;
+    my @paths = split(/,/,$1);
+    return \@paths;
+}
+
+=head1 getLayoutCode
+    
+    Get the layout code. Recursively walk down the list of types,
+    find all instances of the next type, and recurse down those, 
+    building resulting html into a list of nested <ul>..</ul> 
+    lists.
+    
+=cut
+sub getLayoutCode {
     my $self = shift;
-    my $probe = shift;
+    my ($root,$pathTypes,$parentType) = @_;
     my $session= $self->Session();
     my $factory = $session->Factory();
+    
+    my $html;
 
-    my (@probeImageMaps) = 
-	$factory->findObjects('@ImageProbe', {Probe=>$probe});
-    my @images;
+    # The map is the type that goes between $parentType
+    # and $type - the next entry in the list.
+    # type will be undef when the map maps directly to image.
+    # shifting walks down the list destructively,
+    # so we have to copy the list when we recurse.
 
-    while (@probeImageMaps) {
-	my $map = shift @probeImageMaps;
-	my %row_data;
-	my $imageID = $map->image_id;
-	my $image = $factory->loadObject("OME::Image",$imageID);
-	push(@images,$image);
+    my $map = shift @$pathTypes;
+    my $type = shift @$pathTypes || undef;
+
+    # strip off the "@" to get the field name.
+    $type =~ /@(.*)/;
+    # so, if map is ProbeGeneMap and type is "@Probe", target field
+    # will be probe
+    my $targetField = $1;
+
+    # find the maps that correspond to the root object.
+    my @maps = $factory->
+	findObjects($map, { $parentType  =>
+				$root});
+    
+    if (scalar(@maps) > 0) {
+	# if i have any maps
+
+	if (scalar(@$pathTypes) ==0 ) {
+	    # we're at the end of the list of types.
+
+	    my @images;
+	    # find the image associated with the maps.
+	    foreach my $map (@maps) {
+		my $imageID = $map->image_id;
+		my $image = $factory->loadObject('OME::Image',$imageID);
+		push(@images,$image);
+	    }
+	    # render them in an html array
+	    $html .=   return 
+		$self->Renderer->renderArray(\@images,'bare_ref_mass',
+					     { type => 'OME::Image'});
+	    
+	}
+	else  { # still more to go.
+	    #start a new list
+	    $html .= "<ul>";
+	    foreach my $map (@maps) {
+		# get target of map
+		# print a label for it
+		# recurse with it as root and it's type as parent
+
+		# target field is now the next type in the hierarchy.
+		my $target = $map->$targetField;
+		# get the item and build it as a list of item.
+		$html .= "<li> ". $targetField . "  ".
+		    $target->Name() .    "<br>\n";
+
+		# fresh copy of the list of types for the next recursion
+		my @localTypes;
+		for (my $i=0; $i < scalar(@$pathTypes); $i++) {
+		    $localTypes[$i]=$pathTypes->[$i];
+		}
+		# recurse to populate the next level.
+		$html .= $self->getLayoutCode($target,\@localTypes,
+					      $targetField);
+	    }
+	    # end the list.
+
+	    $html .= "</ul>";
+	}
     }
-    return $self->Renderer->renderArray(\@images,'bare_ref_mass',
-					{ type => 'OME::Image'});
+    else  {
+	# if I found no maps.
+	return "No ${targetField}s found";
+    }
+    return $html;
 }
 
-sub getGeneChooser {
-    my $self=shift;
-    my $q = $self->CGI();
-    my $session= $self->Session();
-    my $factory = $session->Factory();
-
-    my @genes = $factory->findObjects('@Gene');
-    my $url = $self->pageURL('OME::Web::ImageAnnotationBrowser');
-    my $directions = "Choose a gene ";
-    my $template = $q->param('Template');
-    my $popup =  $q->popup_menu(
-	-name => 'GeneChoice',
-	-values => [ map($_->Name,@genes)]);
-
-    if (defined $q->param('ChooseGene')) {
-	my $full = $url.'&Template='.$template.'&GeneName='.
-	    $q->param('GeneChoice');
-	return('REDIRECT',$full) 
-    }
-
-    my $button = $q->submit(-name=>'ChooseGene',-value=>'Submit');
-    my $html = $q->startform({ -name=>'primary'}) .
-        $q->hidden(-name=>'Template',-default=>$template)  .
-	$directions. $popup . $button. $q->endform();
-    return ('HTML',$html);
-}
 
 
 
