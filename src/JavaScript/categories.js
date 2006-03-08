86611d88ab882595857c7c59830ae88e9e011d4e
Harry Hochheiser <hsh@nih.gov>
2006-03-08 20:21:23 +0000

    simplify to work off of code that generates full html snippets
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7098 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/JavaScript/categories.js
+++ src/JavaScript/categories.js
@@ -12,8 +12,7 @@ var xmlhttp;
 function changeCategoryGroup(object) {
 	 var id = object.options[object.selectedIndex].value;
 	 var url = RenderPage+"&ID="+id;
-	 url += "&Mode=list_of_options&Type=@CategoryGroup";
-	 url += "&Accessor=Category&Output=XML";
+	 url += "&Mode=select&Type=@CategoryGroup";
 
 	 xmlhttp = new XMLHttpRequest;
 	 xmlhttp.onreadystatechange = updateCategory;
@@ -23,69 +22,9 @@ function changeCategoryGroup(object) {
 
 
 function updateCategory() {
-	 if (xmlhttp.readyState == 4) {
-	    var cats = document.getElementById("Category");
-	    var optCount = cats.length;
-
-	    for (i = optCount-1; i > 0; i--) {
-   		    cats.remove(i);
-	    }
-
-	    var serializer = new XMLSerializer();
 	    var txt = xmlhttp.responseText;
-
-	    // do the items
-            var select = buildSelect();
-	    var selectString = serializer.serializeToString(select);
 	    
 	    var span = document.getElementById("catSelect");
-	    span.innerHTML="";
-	    span.innerHTML=selectString;
-     }
+	    span.innerHTML=txt;
 }
 
-function buildSelect() {
-
-	 var select = document.createElement("select");
-	 // set name
-	 setNodeAttribute(select,"name","Category");
-	 // set id
-	 setNodeAttribute(select,"id","Category");
-
-	 // add "all"
-	 var opt = document.createElement("option");
-	 var node = document.createTextNode("All");
-	 opt.value = "All";
-	 opt.appendChild(node);
-	 select.appendChild(opt);
-	 
-	 
-
-	 // add the rest
-         var items = xmlhttp.responseXML.getElementsByTagName("OPTION");
-	 for (var i =0; i < items.length;i++) {
-	   var item = items[i];
-	   var valueNode = item.getAttributeNode("VALUE");
-	   if (valueNode) {
-	      var value = valueNode.nodeValue;
-	      var name = item.firstChild.nodeValue;	
-	      // create the text node
-	      node = document.createTextNode(name);
-	      opt = document.createElement("option");
-	      opt.value = value;
-	      opt.appendChild(node);
-	      select.appendChild(opt);
-	   }
-	}
-	return select;
-}
-
-function showCat(object) {
-    var id = object.options[object.selectedIndex].value;
-    alert("selected cat "+id);
-}
-
-function setNodeAttribute(node,attName,value) {
-	node.setAttributeNode(document.createAttribute(attName));
-	node.setAttribute(attName,value);
-}
