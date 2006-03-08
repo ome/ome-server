c9edbd4c9774bad42c329b71c4dfef8b5a124db6
Harry Hochheiser <hsh@nih.gov>
2006-03-08 21:12:30 +0000

    alternaative web page for browsing of ImageAnnotationTable output
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7102 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/perl2/OME/Web/TableBrowse.pm
@@ -0,0 +1,129 @@
+# OME/Web/TableBrowse.pm
+#
+#-------------------------------------------------------------------------------
+#
+# Copyright (C) 2003 Open Microscopy Environment
+#       Massachusetts Institue of Technology,
+#       National Institutes of Health,
+#       University of Dundee
+#
+#
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+#
+# Written by:    Harry Hochheiser <hsh@nih.gov>
+#-------------------------------------------------------------------------------
+
+package OME::Web::TableBrowse;
+
+use strict;
+use Carp 'cluck';
+use vars qw($VERSION);
+
+use base qw(OME::Web);
+use OME::Web::ImageAnnotationBrowser;
+use OME::Web::TableBrowse::HeaderBuilder;
+use OME::Web::ImageAnnotationTable;
+
+my $BROWSE_PAGE='OME::Web::ImageAnnotationBrowser';
+
+sub getPageTitle {
+    return "OME: Browsing Mouse Images";
+}
+
+{
+	my $menu_text = "Browse Mouse Images";
+	sub getMenuText { return $menu_text }
+}
+
+sub getPageBody {
+    my $self = shift;
+    my $q =  $self->CGI();
+    my $session = $self->Session();
+    my $factory = $session->Factory();
+    my %tmpl_data;
+
+    my $sub = $q->param('submit');
+
+    $tmpl_data{'Gene'} = $q->param('Gene') if (defined
+					       $q->param('Gene'));
+
+    # Gene and embryo Stage for rows and columns if not specified
+    $q->param(-name=>'Template',-value=>'GeneProbeTable') unless $q->param('Template');
+    $q->param(-name=>'Rows',-value=>'Gene') unless $q->param('Rows');
+    $q->param(-name=>'Columns',-value=>'EmbryoStage') unless $q->param('Columns');
+    
+    my $table = new OME::Web::ImageAnnotationTable();
+
+    # get the stage.
+    my @stages = $factory->findObjects('@EmbryoStage');
+
+    my %stageHash;
+    $stageHash{type}='@EmbryoStage';
+
+    # get the stage id  to use as defaults. handle possibility that
+    # stage came in as # or name.
+    my $stage;
+    if (defined $q->param('EmbryoStage')) {
+	$stage = $q->param('EmbryoStage');
+	if ($stage  =~ /\D+/) { # not numbers
+	    my @stageObjs = $factory->findObjects('@EmbryoStage',
+						 {Name=>$stage});
+	    if (scalar(@stageObjs) > 0) {
+		my $stageObj = $stageObjs[0];
+		$stage = $stageObj->ID();
+	    }
+	    else {
+		undef $stage;
+	    }
+	} # if it's not a number, it's an id. do nothing.
+    }
+    
+    $stageHash{default_value}= $stage;
+    $tmpl_data{'embryoStages/render-list_of_options'} 
+          = $self->Renderer->renderArray(\@stages,'list_of_options',\%stageHash);
+
+
+
+    my $output =
+	$table->getTableDetails($self,'GeneProbeTable','OME::Web::TableBrowse');
+
+    # and the form.
+    my $html =
+    $q->startform( { -name => 'primary' } );
+    $html .= $output;
+    $html .= $q->endform();
+
+    return ('HTML',$html);	
+}
+
+
+sub getMenuBuilder {
+    return undef;
+}
+
+sub getHeaderBuilder {
+    return new OME::Web::TableBrowse::HeaderBuilder();
+}
+
+
+1;
