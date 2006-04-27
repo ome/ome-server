1953bceaa07b258e22bc15c83f68e0cd51dc678e
Tomas Macura <tmacura@nih.gov>
2006-04-27 21:24:51 +0000

    fix a bug with float and CtoOMEISType
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7296 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/C/omeis-http/httpOMEISaux.c
+++ src/C/omeis-http/httpOMEISaux.c
@@ -10,7 +10,7 @@
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    version 2.1d of the License, or (at your option) any later version.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -99,7 +99,7 @@ void CtoOMEISDatatype (const char* data_type, pixHeader* head)
 		head->bp       = 4;
 		head->isSigned = 0;
 		head->isFloat  = 0;
-	} else if (strcmp (data_type, "float") || !strcmp (data_type, "single")) {
+	} else if (!strcmp (data_type, "float") || !strcmp (data_type, "single")) {
 		head->bp       = 4;
 		head->isSigned = 1;
 		head->isFloat  = 1;
