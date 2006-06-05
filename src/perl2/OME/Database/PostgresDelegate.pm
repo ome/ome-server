c3f6c202d354898bc176d80e29649f8fb9844a10
Curtis Rueden <ctrueden@wisc.edu>
2006-06-05 15:08:26 +0000

    Print Postgres restart information before actually dying.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7386 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Database/PostgresDelegate.pm
+++ src/perl2/OME/Database/PostgresDelegate.pm
@@ -118,10 +118,12 @@ sub createDatabase {
 		return 1;
 	}
 
-	$dbh->do(qq{CREATE DATABASE "$dbName"}) or die $dbh->errstr().
-		"\n\nThere was a problem creating the database. ".
-		"You could try restarting\nPostgresSQL ".
-		"(e.g., \"sudo /etc/init.d/postgresql-7.4 restart\").";
+	if (not defined $dbh->do(qq{CREATE DATABASE "$dbName"})) {
+		print "\n\nThere was a problem creating the database. ".
+			"You could try restarting\nPostgresSQL (e.g., ".
+			"\"sudo /etc/init.d/postgresql-7.4 restart\").\n";
+		die $dbh->errstr();
+	}
 
 	print "\t\033[1m[Done.]\033[0m\n";
 	$dbh->disconnect();
