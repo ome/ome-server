e6d78d6f2d2ad3b94c13342750fe17b06baeccd2
Tomas Macura <tmacura@nih.gov>
2006-07-11 15:38:20 +0000

    hahahaha
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7497 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ src/matlab/Tests/compute_sig_digits.m
@@ -0,0 +1,34 @@
+% returns the number of significant digits two numbers agree on
+% e.g.:
+% 	suppose vec_a is [123456] and matrix_b is [123457] then compute_sig_digits(vec_a, matrix_b)
+%	would return 5 as vec_a and matrix_b share 5 significant digits (1,2,3,4,5).
+
+function sig_digits = compute_sig_digits (matrix_a, matrix_b)
+[rows, columns] = size(matrix_a);
+sig_digits = zeros(rows, columns);
+
+for i=1:rows
+	for j=1:columns
+		% If both signatures are nan, then they agree exactly. 
+		% Perfect agreement is flagged as 16
+		if( isnan( matrix_a(i,j) ) && isnan( matrix_b(i,j) ) )
+			sig_digits(i,j) = 16;
+		% If only one signatures is nan, then they disagree in an extreme way.
+		% This is flagged as -1
+		elseif( isnan( matrix_a(i,j) ) || isnan( matrix_b(i,j) ) )
+			sig_digits(i,j) = -1;
+		% If both are normal numbers, then calculate the number of significant
+		% digits to which they agree.
+		else
+			
+			diff_vec = matrix_a(i,j) - matrix_b(i,j);
+			avg_vec  = (matrix_a(i,j) + matrix_b(i,j))/2;
+			
+			if (abs(diff_vec) < eps)
+				sig_digits(i,j) = 16;
+			else 
+				sig_digits(i,j) = uint8(-log10(diff_vec/avg_vec));
+			end
+		end
+	end
+end
