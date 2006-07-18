c42ccea829a9c9f2a13eb351fa5b93d05f57b5f0
Ilya Goldberg <igg@nih.gov>
2006-07-18 02:54:46 +0000

    Fixing FilenamePattern again
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7544 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ update/2.23/pre/000-FilenamePattern.sql
@@ -0,0 +1,14 @@
+-- Change the name and regex for the metamorph tiff filename pattern
+-- Add a time fragment to the pattern and specify that its for tiffs from Metamorph
+UPDATE filename_pattern
+    SET name='Metamorph Filename Pattern for TIFFs',
+    regex='^(.*?)((_w(\\d+[^_.]*?))|(_t(\\d+)))+',
+    the_c = 4,
+    the_t = 6
+WHERE attribute_id = (
+    SELECT fp.attribute_id
+    FROM filename_pattern fp, lsid_object_map lsid
+    WHERE fp.attribute_id = lsid.object_id
+        AND lsid.lsid = 'urn:lsid:openmicroscopy.org:FilenamePattern:0'
+        AND fp.regex='^(.+)(_w)(\d+.*)(_t)(\d+)(.*)'
+    );
