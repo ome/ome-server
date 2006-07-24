91ccd764d2a59c97c237ae1aed4c8765dd8de71a
Ilya Goldberg <igg@nih.gov>
2006-07-24 20:01:09 +0000

    Pre-loading os STs and other goodies
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7563 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME-startup.pl
+++ src/perl2/OME-startup.pl
@@ -5,8 +5,17 @@ print STDERR "Processing OME-startup.pl\n";
 use Carp;
 
 use CGI qw/-no_xhtml/;
-CGI->compile(':all');
 
+our @OME_WEB_CLASSES = qw/
+	OME::Web
+	OME::Web::Ping
+/;
+eval {
+	Apache::DBI->require();
+};
+if ($@) {
+	print STDERR "Not using Apache::DBI - no cached DB handles";
+}
 
 use OME;
 use OME::Session;
@@ -20,16 +29,79 @@ use OME::ImportExport::ChainImport;
 use OME::Tasks::OMEImport;
 use OME::Tasks::ModuleExecutionManager;
 
-use OME::Install::CoreDatabaseTablesTask;
-use OME::Web;
-use OME::Web::Ping;
+use OME::Util::cURL;
+
 
 use OME::Install::Environment;
-OME::Install::Environment->initialize();
+my $env = OME::Install::Environment->initialize()
+	or die "OME installation environment (/etc/ome-install.store) could not be initialized";
 
+use OME::Install::CoreDatabaseTablesTask;
 foreach my $class (@OME::Install::CoreDatabaseTablesTask::core_classes) {
 	$class->require();
 }
 
+
+use OME::Install::Util ();
+# Get a temporary factory
+my $old_euid;
+my $session;
+eval {
+	$session = bootstrap_session();
+};
+if ($@ or not $session) {
+	print STDERR "Tring to get a session as the Apache user...\n";
+	eval {
+		my $apacheUID = getpwnam($env->apache_user());
+		$old_euid = OME::Install::Util::euid ( $apacheUID );
+		$session = bootstrap_session();
+	};
+	if ($@) {
+		print STDERR "Could not get an OME::Session object:\n$@";
+	}
+	
+}
+
+use OME::SemanticType;
+if ($session) {
+	my $factory = OME::Session->instance()->Factory();
+	OME::Configuration->preload($factory);
+	my @STs = $factory->findObjects ('OME::SemanticType');
+	print STDERR "Pre-loading SemanticTypes...\n";
+	foreach my $ST (@STs) {
+		$ST->requireAttributeTypePackage($factory);
+	}
+
+	$factory = undef;
+	$session->deleteInstance();
+	$session = undef;
+}
+
+if (defined $old_euid) {
+	OME::Install::Util::euid ($old_euid);
+}
+
+my $class;
+
+# Import OME::Web stuff if we're using OME::Web
+if ($env->apache_conf()->{WEB}) {
+# recursively find all files with a matching package declaration
+# grep -r '^[^#]*package OME::Web' OME
+	print STDERR "Pre-loading classes for OME::Web...\n";
+	CGI->compile(':all');
+	foreach my $class (@OME_WEB_CLASSES) {
+		$class->require();
+	}
+}
+
+
+
+sub bootstrap_session {
+    my $factory = OME::Factory->new();
+	my $experimenterObj = $factory->
+		findObject('OME::SemanticType::BootstrapExperimenter',OMEName => $env->ome_exper()->{OMEName});
+    my $userState = $factory->findObject('OME::UserState',experimenter_id => $experimenterObj->id());
+    return OME::Session->instance($userState, $factory);
+}
 
 1;
