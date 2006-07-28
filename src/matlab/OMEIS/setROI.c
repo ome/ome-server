00f4a4b0eb5b7d88ffa5b540a73e6a1cfad4cc56
anagaraja <anagaraja@unknown>
2006-07-28 13:30:40 +0000

    Changed error messages to say "setROI" not "setPixels"
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7580 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/matlab/OMEIS/setROI.c
+++ src/matlab/OMEIS/setROI.c
@@ -23,22 +23,22 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
 	char* url, *sessionkey;
 	
 	if (nrhs != 13)
-		mexErrMsgTxt("\n [pix] = setPixels (is, ID, row0 (OME:y0), column0 (OME:x0), z0, c0, t0, row1 (OME:y1), column1 (OME:x1), z1, c1, t1, pixels)");
+		mexErrMsgTxt("\n [pix] = setROI (is, ID, row0 (OME:y0), column0 (OME:x0), z0, c0, t0, row1 (OME:y1), column1 (OME:x1), z1, c1, t1, pixels)");
 		
 	if (!mxIsStruct(prhs[0]))
-		mexErrMsgTxt("settPixels requires the first input to be the struct outputed"
+		mexErrMsgTxt("setROI requires the first input to be the struct outputed"
 					 " from openConnectionOMEIS\n");
 					 
 	if (!(m_url = mxGetField(prhs[0], 0, "url")))
-		mexErrMsgTxt("setPixels requires the first input, OMEIS struct, to have field: url");
+		mexErrMsgTxt("setROI requires the first input, OMEIS struct, to have field: url");
 	if (!(m_sessionkey = mxGetField(prhs[0], 0, "sessionkey")))
-		mexErrMsgTxt("setPixels requires the first input, OMEIS struct, to have field: sessionkey");
+		mexErrMsgTxt("setROI requires the first input, OMEIS struct, to have field: sessionkey");
 		
 	if (!mxIsChar(m_url) || !mxIsChar(m_sessionkey))
 		mexErrMsgTxt("OMEIS field aren't character array.\n");		
 	
 	if (!mxIsNumeric(prhs[1]))
-		mexErrMsgTxt("setPixels requires the second input to be the PixelsID\n") ;
+		mexErrMsgTxt("setROI requires the second input to be the PixelsID\n") ;
 		
 	ID = (OID) mxGetScalar(prhs[1]) ;
 	
