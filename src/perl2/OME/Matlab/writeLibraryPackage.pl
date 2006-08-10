2ce0ed2e4e3c7bf1269f8d35cc5bce807e6019e1
anagaraja <anagaraja@unknown>
2006-08-10 21:30:50 +0000

    Cache dir. is no longer hardcoded in resulting Perl packages. Also, when you specify a relative path, it is no longer hardcoded.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7709 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Matlab/writeLibraryPackage.pl
+++ src/perl2/OME/Matlab/writeLibraryPackage.pl
@@ -5,7 +5,7 @@
 
 # Usage:
 # perl writeLibraryPackage.pl libName relative/or/absolute/path/to/library
-use OME::SessionManager;
+use OME::Install::Environment;
 use Cwd;
 use File::Path;
 use strict;
@@ -14,10 +14,7 @@ die "Usage: perl writeLibraryPackage.pl libName relative/or/absolute/path/to/lib
 	unless ( @ARGV );
 
 my $environment = initialize OME::Install::Environment;
-
-my $session = OME::SessionManager->TTYlogin();
-my $ome_lib_root = $session->Configuration()->ome_lib_root();
-#my $ome_lib_root = '/OME/lib';
+my $ome_lib_root = $environment->base_dir().'/lib';
 
 my ( $packageName, $path ) = ( @ARGV );
 
@@ -36,13 +33,9 @@ my $libName = $packageName.'OME';
 # If you have a relative path, add the ome_lib_root to the beginning
 my $full_path = $path;
 $full_path = $ome_lib_root."/matlab_compiled/".$path unless ( $path =~ m/^(\/\S+)+/ );
-#mkpath($full_path, 0, 02775) unless ( -d $full_path );
 
 my $file = $full_path."/lib$libName\_mcc_component_data.c";
 print "The file is $file\n";
-
-my $cache_dir = $environment->base_dir().'/Inline';
-mkpath($cache_dir, 0, 02775) unless ( -d $cache_dir);
 
 # TODO: Figure this out dynamically, as it will be different once we have the
 # full compiler
@@ -52,7 +45,7 @@ my $include_dir = "/Applications/MATLAB72/extern/include";
 open FILE, "< $file"  or die "Cannot open file: $file\n";
 open PACKAGE, "> $src_dir\/$packageName.pm" or die "Cannot create file $packageName in directory $src_dir\n";
 
-print PACKAGE "# This file was automatically generated using a script.\n";
+print PACKAGE "# This file was automatically generated using a script.\n\n";
 
 print PACKAGE "package OME::Matlab::Lib::$packageName;
 use strict;
@@ -61,17 +54,47 @@ use Carp;
 
 use base qw(OME::Matlab::Compiled);
 
+my \$CACHE_DIRECTORY;
+my \$full_path;
+
+BEGIN {
+	# This gets reset if you specify a relative path - this value is only used when
+	# you specify an absolute path.
+	\$full_path = \"$full_path\";
+
+	use OME::Install::Environment;
+	my \$environment = initialize OME::Install::Environment;
+	if (\$environment and \$environment->base_dir()) {
+		\$CACHE_DIRECTORY = \$environment->base_dir().'/Inline';
+	} else {
+		croak \"OME::Matlab::Lib::$packageName was loaded without an OME installation environment!\";
+	}
+	if (not -d \$CACHE_DIRECTORY) {
+		mkpath \$CACHE_DIRECTORY
+			or croak \"Could not create cache directory for OME::Matlab::Lib::$packageName\";
+	}
+
+";
+
+ # If we have a relative path, get the ome lib root in the package itself
+unless ( $path =~ m/^(\/\S+)+/ ) {
+	print PACKAGE "	my \$ome_lib_root = \$environment->base_dir().'/lib';
+	\$full_path = \$ome_lib_root.\"/matlab_compiled/$path\";
+";
+}
+
+print PACKAGE "}
 # Changing the directory to the location of the ctf archive is an alternative
 # to setting dyld_library_path.
 die \"couldn't change directory\" 
-unless chdir('$full_path');
+unless chdir( \$full_path );
 
-use Inline ( Config => DIRECTORY => '$cache_dir' );
+use Inline ( Config => DIRECTORY => \$CACHE_DIRECTORY );
 
 use Inline (
 	C       => 'DATA',
 	INC		  => \"-I$include_dir\",
-	LIBS	  => [\"-L$full_path -l$libName\"],
+	LIBS	  => [\"-L\$full_path -l$libName\"],
 );
 
 Inline->init;
@@ -93,9 +116,9 @@ foreach my $line ( @contents ) {
 	print PACKAGE "$line";
 }
 print PACKAGE "SV *getComponentData(char* class) {
-SV *obj_ref = newSV(0);
-SV *obj = sv_setref_pv(obj_ref, NULL, (void*) &__MCC_lib$libName\_component_data);
-return obj;
+	SV *obj_ref = newSV(0);
+	SV *obj = sv_setref_pv(obj_ref, NULL, (void*) &__MCC_lib$libName\_component_data);
+	return obj;
 }";
 
 close FILE;
