2570450e559423c1771895b1d6731c122f59b2a9
Josiah Johnston <siah@nih.gov>
2006-08-16 21:52:57 +0000

    [Bug 683] Tweaked the way hostname is gathered during installation so that a non-interactive install will not be interupted.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@7735 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/perl2/OME/Install/CoreDatabaseTablesTask.pm
+++ src/perl2/OME/Install/CoreDatabaseTablesTask.pm
@@ -749,12 +749,11 @@ sub bootstrap_session {
 }
 
 sub verifyHostname {
-	my $hostname = $ENVIRONMENT->hostname ();
+	my $hostname = $ENVIRONMENT->hostname () || hostname();
 	return $hostname if
 		( $hostname && y_or_n ("Hostname set to '$hostname'.  OK ?",'y') );
 	
-	$hostname = hostname();
-	$hostname = confirm_default ("Hostname", $hostname);
+	$hostname = confirm_default ("Hostname: ", $hostname);
     $ENVIRONMENT->hostname ($hostname);
 	return $hostname;
 }
