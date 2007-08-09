09ac4c8fc911464263dc18f2bef0126384dfcdb8
Curtis Rueden <ctrueden@wisc.edu>
2007-08-09 16:47:23 +0000

    Refix initial skeleton schemaLocation attribute.
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@8699 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- src/java/trunk/src/org/openmicroscopy/xml/OMENode.java
+++ src/java/trunk/src/org/openmicroscopy/xml/OMENode.java
@@ -73,7 +73,10 @@ public class OMENode extends OMEXMLNode {
     "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
     "xsi:schemaLocation=\"" +
     "http://www.openmicroscopy.org/XMLschemas/OME/FC/ome.xsd " +
+    "http://www.openmicroscopy.org/XMLschemas/OME/FC/ome.xsd " +
     "http://www.openmicroscopy.org/XMLschemas/STD/RC2/STD.xsd " +
+    "http://www.openmicroscopy.org/XMLschemas/STD/RC2/STD.xsd " +
+    "http://www.openmicroscopy.org/XMLschemas/CA/RC1/CA.xsd " +
     "http://www.openmicroscopy.org/XMLschemas/CA/RC1/CA.xsd\"/>";
 
   /** Element name for storing extra path information. */
