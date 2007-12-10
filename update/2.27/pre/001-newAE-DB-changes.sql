f4296c1d4f9d110e0c09a58f51b1b7c232a3f635
Tomas Macura <tmacura@nih.gov>
2007-12-10 10:18:05 +0000

    new DAE: update scripts
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@8441 61f9eca8-113b-463c-a1a4-bab9b0a29f14

---
+++ update/2.27/pre/001-newAE-DB-changes.sql
@@ -0,0 +1,14 @@
+ALTER TABLE analysis_chain_executions ADD COLUMN task INTEGER;
+ALTER TABLE analysis_chain_executions ADD COLUMN results_reuse BOOLEAN;
+UPDATE analysis_chain_executions SET results_reuse=false;
+
+ALTER TABLE analysis_chain_executions ADD COLUMN additional_jobs BOOLEAN DEFAULT FALSE;
+
+ALTER TABLE tasks ADD COLUMN visible BOOLEAN DEFAULT true;
+
+ALTER TABLE analysis_chain_nodes ADD COLUMN dependence CHAR(1);
+
+-- the purpose of the analysis_workers table has changed from the old DAE
+-- in the new DAE the table is *not* edited by users/scripts but by the AE
+-- when workers register/unregister
+DELETE FROM analysis_workers;
