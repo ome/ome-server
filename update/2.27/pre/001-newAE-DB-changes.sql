41a823a1f0a35930d0314eac10d979c04024ad1b
Tomas Macura <tmacura@nih.gov>
2007-12-11 17:01:57 +0000

    don`t delete old analysis_workers, just set their status to legacy
    git-svn-id: svn+ssh://svn.openmicroscopy.org.uk/home/svn/ome/trunk@8448 61f9eca8-113b-463c-a1a4-bab9b0a29f14

--- update/2.27/pre/001-newAE-DB-changes.sql
+++ update/2.27/pre/001-newAE-DB-changes.sql
@@ -12,4 +12,4 @@ ALTER TABLE analysis_chain_nodes ADD COLUMN dependence CHAR(1);
 -- the purpose of the analysis_workers table has changed from the old DAE
 -- in the new DAE the table is *not* edited by users/scripts but by the AE
 -- when workers register/unregister
-DELETE FROM analysis_workers;
+UPDATE analysis_workers SET status='LEGACY';
